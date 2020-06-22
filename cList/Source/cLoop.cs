012ef0088308d17e87993d71f8efef23796167bd
Wei Ding <ding_web@yahoo.co.jp>
2020-06-21 20:02:33 -0400

    check loop

---
+++ cList/Source/cLoop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cList.Source
+{
+	public class cLoop
+	{
+		public void LoopStart()
+		{
+			Console.WriteLine("-- Loop");
+			for (int i = 2; i < 2; i++)
+				Console.WriteLine("  " + i);
+			Console.WriteLine("-- End");
+		}
+	}
+}
