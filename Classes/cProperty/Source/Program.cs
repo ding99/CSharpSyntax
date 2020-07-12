c81f73d6e42cca2fc549907c06a0cede4868d99a
Wei Ding <ding_web@yahoo.co.jp>
2020-07-11 23:59:35 -0400

    update Classes folder

--- Classes/cProperty/Source/Program.cs
+++ Classes/cProperty/Source/Program.cs
@@ -5,7 +5,7 @@ namespace cProperty {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("=== start");
-            //new PV2().test();
+            new PV2().test();
             new PV3().test();
             Console.WriteLine("=== end");
         }
