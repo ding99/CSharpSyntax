dc8e7a1b3e783a96ee65ec99c77baf89d7e95507
Wei Ding <ding_web@yahoo.co.jp>
2020-07-11 21:36:04 -0400

    update Files

---
+++ Files/cPath/Source/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cPath.Source
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("== strat");
+
+			TPath tp = new TPath();
+			tp.createFile(args, "\\test");
+			tp.currentdir();
+			tp.temppath();
+			tp.separators();
+
+			Console.WriteLine("== end");
+		}
+	}
+}
