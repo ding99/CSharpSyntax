bdc0c9c6aae5313283ceaa7f35c78b371fc1eac5
Wei Ding <ding_web@yahoo.co.jp>
2020-07-28 21:16:20 -0400

    simple console app

---
+++ CS60DN46/Ch02BuildingApp/SimpleConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "My Rocking App";
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.WriteLine("*************************************");
+			Console.WriteLine("***** Welcome to My Rocking App *****");
+			Console.WriteLine("*************************************");
+		}
+	}
+}
