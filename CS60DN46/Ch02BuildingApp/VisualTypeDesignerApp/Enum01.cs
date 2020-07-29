5c727335fb13e0d55991a47b35be6ee1e55b40fc
Wei Ding <ding_web@yahoo.co.jp>
2020-07-29 19:24:07 -0400

    use toolbox

---
+++ CS60DN46/Ch02BuildingApp/VisualTypeDesignerApp/Enum01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisualTypeDesignerApp
+{
+	public enum CarColor
+	{
+		White = 10,
+		Black,
+		Red,
+		Gray
+	}
+}
--- CS60DN46/Ch02BuildingApp/VisualTypeDesignerApp/Program.cs
+++ CS60DN46/Ch02BuildingApp/VisualTypeDesignerApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,19 @@ namespace VisualTypeDesignerApp
 {
 	class Program
 	{
-		static void Main(string[] args)
+		private static int number = 2;
+
+		public static void Main(string[] args)
 		{
+			number++;
+			
+			
+			Console.WriteLine("Hello Visual Type! " + number + "; Color {0} with value {1:D}", CarColor.Black,  CarColor.Black);
+
+			SportCar newcar = new SportCar();
+			newcar.color = CarColor.Gray;
+
+			Console.WriteLine("New Car : " + newcar.GetPetName() + ", Color : " + newcar.color);
 		}
 	}
 }
