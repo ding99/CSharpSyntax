ec52529a2f044294d538bcad80412a8df5804dd9
Wei Ding <ding_web@yahoo.co.jp>
2020-07-29 01:23:02 -0400

    add message box

--- CS60DN46/Ch02BuildingApp/SimpleConsoleApp/Program.cs
+++ CS60DN46/Ch02BuildingApp/SimpleConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace SimpleConsoleApp
 {
@@ -9,9 +10,12 @@ namespace SimpleConsoleApp
 			Console.Title = "My Rocking App";
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.BackgroundColor = ConsoleColor.Blue;
+
 			Console.WriteLine("*************************************");
 			Console.WriteLine("***** Welcome to My Rocking App *****");
 			Console.WriteLine("*************************************");
+
+			MessageBox.Show("All Done!");
 
 			Console.ResetColor();
 		}
