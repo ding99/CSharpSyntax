e437f05f1a117004f5eb89ce8e6762b27ea253a5
Wei Ding <ding_web@yahoo.co.jp>
2020-07-31 00:51:10 -0400

    console

--- CS60DN46/Ch03CoreConstructs1/SimpleCSharpApp/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/SimpleCSharpApp/Program.cs
@@ -6,15 +6,26 @@ namespace SimpleCSharpApp
 	{
 		static void Main(string[] args)
 		{
+			Console.Title = "Core C# Programming Constructs, Part I";
+
 			Console.WriteLine("**** My First C# App *****");
 			Console.WriteLine("Hello Core Constructure I!");
 			Console.WriteLine();
 
 			string[] theArgs = Environment.GetCommandLineArgs();
+			Console.WriteLine("Number of arguments: {0}", theArgs.Length);
 			foreach (string arg in theArgs)
 				Console.WriteLine("Arg: {0}", arg);
+			Console.WriteLine();
 
 			ShowEnvironmentDetails();
+
+			Console.WriteLine();
+			Console.Beep();
+			Console.WriteLine("Windows: Top {0} x Left {1} Size: {2} x {3}",
+				Console.WindowTop, Console.WindowLeft, Console.WindowWidth, Console.WindowHeight);
+			Console.WriteLine("Buffer area: {0} x {1}",
+				Console.BufferWidth, Console.BufferHeight);
 		}
 
 		static void ShowEnvironmentDetails()
@@ -29,6 +40,14 @@ namespace SimpleCSharpApp
 			Console.WriteLine("Number of processors: {0}",
 				Environment.ProcessorCount);
 			Console.WriteLine(".NET Version: {0}", Environment.Version);
+
+			Console.WriteLine();
+			Console.WriteLine("Is64BitOperationSystem: {0}", Environment.Is64BitOperatingSystem);
+			Console.WriteLine("Is64BitProcess: {0}", Environment.Is64BitProcess);
+			Console.WriteLine("MachineName: {0}", Environment.MachineName);
+			Console.WriteLine("SystemDirectory: {0}", Environment.SystemDirectory);
+			Console.WriteLine("UserName: {0}", Environment.UserName);
+			Console.WriteLine("ExitCode: {0}", Environment.ExitCode);
 
 			Console.ResetColor();
 		}
