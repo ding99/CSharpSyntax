416ac615b1f1925c0f389b0bdfc6ab02df3ca27e
Wei Ding <ding_web@yahoo.co.jp>
2020-07-31 00:21:32 -0400

    environments

--- CS60DN46/Ch03CoreConstructs1/SimpleCSharpApp/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/SimpleCSharpApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleCSharpApp
 {
@@ -18,6 +14,23 @@ namespace SimpleCSharpApp
 			foreach (string arg in theArgs)
 				Console.WriteLine("Arg: {0}", arg);
 
+			ShowEnvironmentDetails();
+		}
+
+		static void ShowEnvironmentDetails()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			Console.WriteLine("== Environment ==");
+			foreach (string drive in Environment.GetLogicalDrives())
+				Console.WriteLine("Drive: {0}", drive);
+
+			Console.WriteLine("OS: {0}", Environment.OSVersion);
+			Console.WriteLine("Number of processors: {0}",
+				Environment.ProcessorCount);
+			Console.WriteLine(".NET Version: {0}", Environment.Version);
+
+			Console.ResetColor();
 		}
 	}
 }
