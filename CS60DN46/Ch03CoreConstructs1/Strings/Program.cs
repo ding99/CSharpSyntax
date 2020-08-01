8333273454b3f2ce3ff45979b38be28c02638909
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 13:20:15 -0400

    string project

---
+++ CS60DN46/Ch03CoreConstructs1/Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BasicStringFunctionality();
+		}
+
+		private static void BasicStringFunctionality()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+
+			Console.WriteLine("=> Basic String Functionality");
+			string firstName = "Freddy";
+			Console.WriteLine("Value of firstName: {0}", firstName);
+			Console.WriteLine("firstName has {0} characters.", firstName.Length);
+			Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
+			Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
+			Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
+			Console.WriteLine("firstName after replace: {0}", firstName.Replace("dy", ""));
+			Console.WriteLine();
+
+			Console.ResetColor();
+		}
+	}
+}
