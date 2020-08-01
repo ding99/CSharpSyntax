3131284fb56f26c0b282ab766d914522efbd0035
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 00:19:40 -0400

    data type functionality

--- CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
@@ -10,8 +10,24 @@ namespace BasicDadaTypes
 			LocalVarDeclarations();
 			NewingDataType();
 			ObjectFunctionality();
+			DataTypeFunctionality();
 		}
 
+		static void DataTypeFunctionality()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			Console.WriteLine("=> Data type Functionality:");
+			Console.WriteLine("Max of int: {0}", int.MaxValue);
+			Console.WriteLine("Min of int: {0}", int.MinValue);
+			Console.WriteLine("Max of double: {0}", double.MaxValue);
+			Console.WriteLine("Min of double: {0}", double.MinValue);
+			Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
+			Console.WriteLine("double.PositiveInfinity: {0}", double.PositiveInfinity);
+			Console.WriteLine("double.NegativeInfinity: {0}", double.NegativeInfinity);
+
+			Console.ResetColor();
+		}
 		static void ObjectFunctionality()
 		{
 			Console.ForegroundColor = ConsoleColor.Blue;
