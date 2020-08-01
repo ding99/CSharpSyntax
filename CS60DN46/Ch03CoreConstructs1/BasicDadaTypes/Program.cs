5fbdeb3f50263b3662a58a3414a1a660e8e63b19
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 00:34:17 -0400

    char member

--- CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
@@ -11,6 +11,23 @@ namespace BasicDadaTypes
 			NewingDataType();
 			ObjectFunctionality();
 			DataTypeFunctionality();
+			CharFunctionality();
+		}
+
+		static void CharFunctionality()
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+
+			Console.WriteLine("=> char type functionality:");
+			char myChar = 'a';
+			Console.WriteLine("char.IsDigit('a'): {0}", char.IsDigit(myChar));
+			Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+			Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}", char.IsWhiteSpace("Hello There", 5));
+			Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}", char.IsWhiteSpace("Hello There", 6));
+			Console.WriteLine("char.IsPunctuation('?'): {0}", char.IsPunctuation('?'));
+			Console.WriteLine();
+
+			Console.ResetColor();
 		}
 
 		static void DataTypeFunctionality()
