732f93b6dc819ad4a59a116c18fdd5cb34a5aa4c
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 13:58:16 -0400

    string equality

--- CS60DN46/Ch03CoreConstructs1/Strings/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/Strings/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Strings
 {
@@ -8,8 +9,30 @@ namespace Strings
 		{
 			BasicStringFunctionality();
 			VerBatimStrings();
+			StringEqulity();
 		}
 
+		private static void StringEqulity()
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+
+			Console.WriteLine("=> String Equality");
+			string s1 = "Hello!", s2 = "Yo!";
+			Console.WriteLine("s1 = {0}", s1);
+			Console.WriteLine("s2 = {0}", s2);
+			Console.WriteLine();
+
+			Console.WriteLine("s1 == s2: {0}", s1 == s2);
+			Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!");
+			Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+			Console.WriteLine("s1 == hello!: {0}", s1 == "hello!");
+			Console.WriteLine("s1.Equals(s2): {0}", s1.Equals(s2));
+			Console.WriteLine("Yo!.Equals(s2): {0}", "Yo!".Equals(s2));
+			Console.WriteLine("Yo! == s2: {0}", "Yo!" == s2);
+			Console.WriteLine();
+
+			Console.ResetColor();
+		}
 		private static void VerBatimStrings()
 		{
 			Console.ForegroundColor = ConsoleColor.Yellow;
