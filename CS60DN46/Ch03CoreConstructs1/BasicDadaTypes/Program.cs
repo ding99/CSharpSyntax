0a0757093f76708455e3e0e0a86c39e0326acc33
Wei Ding <ding_web@yahoo.co.jp>
2020-07-31 20:37:52 -0400

    default constructor

--- CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/BasicDadaTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace BasicDadaTypes
 {
@@ -7,6 +8,23 @@ namespace BasicDadaTypes
 		static void Main(string[] args)
 		{
 			LocalVarDeclarations();
+			NewingDataType();
+		}
+
+		static void NewingDataType()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+
+			Console.WriteLine("=> Using new to create variables:");
+			bool b = new bool();
+			int i = new int();
+			double d = new double();
+			DateTime dt = new DateTime();
+			BigInteger bi = new BigInteger();
+			Console.WriteLine("{0}, {1}, {2}, {3}, {4}", b,i,d,dt, bi);
+			Console.WriteLine();
+
+			Console.ResetColor();
 		}
 
 		static void LocalVarDeclarations()
@@ -17,7 +35,7 @@ namespace BasicDadaTypes
 			string myString;
 			myString = "This is my character data";
 			bool b1 = true, b2 = false, b3 = b1;
-			System.Boolean b4 = false;
+			Boolean b4 = false;
 
 			Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}",
 				myInt, myString, b1, b2, b3, b4);
