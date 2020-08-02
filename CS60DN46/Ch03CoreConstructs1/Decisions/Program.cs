c096fef8aae7e13a172b828a5f356867054a32f4
Wei Ding <ding_web@yahoo.co.jp>
2020-08-02 03:04:07 -0400

    start decisions

---
+++ CS60DN46/Ch03CoreConstructs1/Decisions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Decisions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Decisions *****");
+			SwitchOnString();
+			Console.ResetColor();
+		}
+
+		static void SwitchOnString()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("C# or VB");
+			Console.Write("Please pick your language preference: ");
+
+			string lang = Console.ReadLine();
+			switch (lang)
+			{
+				case "#C":
+					Console.WriteLine("Good choice, C# is a fine language.");
+					break;
+				case "VB":
+					Console.WriteLine("VB: OOP, multithreading and more!");
+					break;
+				default:
+					Console.WriteLine("Well... good luck with that!");
+					break;
+			}
+		}
+	}
+}
