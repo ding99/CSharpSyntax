bee82d5885c7fca687cd44f9f31753cc7d52958f
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 20:30:59 -0400

    start data type conversion

---
+++ CS60DN46/Ch03CoreConstructs1/TypeConversions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Fun with type conversions *****");
+
+			short num1 = 9, num2 = 10;
+			Console.WriteLine("{0} + {1} = {2}", num1, num2, Add(num1, num2));
+		}
+
+		static int Add(int x, int y) { return x + y; }
+	}
+}
