c3e73e5bd99511049303cc6f065c9d6cf71f5b2a
Wei Ding <ding_web@yahoo.co.jp>
2020-08-02 00:49:52 -0400

    declare implicit vars

---
+++ CS60DN46/Ch03CoreConstructs1/ImplicitTypedLocalVars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImplicitTypedLocalVars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Fun with Implicit Typing *****");
+
+			DeclareImplicitVars();
+
+			Console.ResetColor();
+		}
+
+		private static void DeclareImplicitVars()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			var myInt = 0;
+			var myBool = true;
+			var myString = "Time, marches on...";
+
+			Console.WriteLine("myInt is a: {0}", myInt.GetType().Name);
+			Console.WriteLine("myBool is a: {0}", myBool.GetType().Name);
+			Console.WriteLine("myString is a: {0}", myString.GetType().Name);
+		}
+	}
+}
