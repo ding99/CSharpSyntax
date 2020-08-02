8a3c75f370a3c1c329e6ec8a7255d7683230b83b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-01 22:47:45 -0400

    explicit cast

--- CS60DN46/Ch03CoreConstructs1/TypeConversions/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/TypeConversions/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TypeConversions
 {
@@ -13,6 +8,36 @@ namespace TypeConversions
 		{
 			Console.WriteLine("***** Fun with type conversions *****");
 
+			NoError();
+			NarrowingAttempt();
+			ExplicitCast();
+
+			Console.ResetColor();
+		}
+
+		private static void ExplicitCast()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Explicit Casting with Lossing Data");
+			short n1 = 30000, n2 = 30000;
+			short answer = (short)Add(n1, n2);
+			Console.WriteLine($"{n1} + {n2} = {answer}");
+		}
+
+		private static void NarrowingAttempt()
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("=> Narrowing Attempt");
+			byte myByte = 0;
+			int myInt = 200;
+			myByte = (byte)myInt;
+			Console.WriteLine("Value of myByte: {0}", myByte);
+		}
+
+		private static void NoError()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> No Error");
 			short num1 = 9, num2 = 10;
 			Console.WriteLine("{0} + {1} = {2}", num1, num2, Add(num1, num2));
 		}
