20137b2c501f6e7fc9d1360a0109bd0bfc508468
Wei Ding <ding_web@yahoo.co.jp>
2020-08-02 01:52:55 -0400

    linq

--- CS60DN46/Ch03CoreConstructs1/ImplicitTypedLocalVars/Program.cs
+++ CS60DN46/Ch03CoreConstructs1/ImplicitTypedLocalVars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ImplicitTypedLocalVars
 {
@@ -9,8 +10,22 @@ namespace ImplicitTypedLocalVars
 			Console.WriteLine("***** Fun with Implicit Typing *****");
 
 			DeclareImplicitVars();
+			LinqQueryOverInts();
 
 			Console.ResetColor();
+		}
+
+		static void LinqQueryOverInts()
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+			var subset = from i in numbers where i < 10 select i;
+			Console.Write("Values in subset:");
+			foreach (var i in subset) Console.Write(" {0}", i);
+			Console.WriteLine();
+
+			Console.WriteLine("subset is a: {0}", subset.GetType().Name);
+			Console.WriteLine("subset is defiend in: {0}", subset.GetType().Namespace);
 		}
 
 		private static void DeclareImplicitVars()
