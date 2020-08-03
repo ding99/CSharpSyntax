54ce2119e5a72ecd9e700f69433f25ab55871fb0
Wei Ding <ding_web@yahoo.co.jp>
2020-08-03 13:06:44 -0400

    object array

--- CS60DN46/Ch03CoreConstructs2/FunWithArrays/Program.cs
+++ CS60DN46/Ch03CoreConstructs2/FunWithArrays/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FunWithArrays
 {
@@ -12,9 +7,35 @@ namespace FunWithArrays
 		static void Main(string[] args)
 		{
 			Console.WriteLine("***** Fun with Arrays *****");
+			
 			SimpleArrays();
+			DeclareImplicitArrays();
+			ArrayOfObjects();
 
 			Console.ResetColor();
+		}
+
+		static void ArrayOfObjects()
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			object[] myObjects = new object[4];
+			myObjects[0] = 10;
+			myObjects[1] = false;
+			myObjects[2] = new DateTime(1969, 3, 24);
+			myObjects[3] = "Form & Void";
+			foreach(object o in myObjects)
+				Console.WriteLine($"Type {o.GetType()}, Value {o}");
+			Console.WriteLine();
+		}
+
+		static void DeclareImplicitArrays()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Implicit Array Initializatoin.");
+			var a = new[] { 1, 10, 100, 1000 }; Console.WriteLine("a is a: {0} with size {1}", a.ToString(), a.Length);
+			var b = new[] { 1, 1.5, 2, 2.5 }; Console.WriteLine("b is a: {0}", b.ToString());
+			var c = new[] { "hello", null, "world" }; Console.WriteLine("c is a: {0}",c.ToString());
+			Console.WriteLine();
 		}
 
 		static void SimpleArrays()
@@ -36,7 +57,7 @@ namespace FunWithArrays
 			foreach (var i in boolArray) Console.Write($" {i}");
 			Console.WriteLine();
 
-			int[] intArray = new int[4] { 20, 222, 23, 0 };
+			int[] intArray = new int[] { 20, 222, 23, 0 };
 			Console.Write("intArray has {0} elements:", intArray.Length);
 			foreach (var i in intArray) Console.Write($" {i}");
 			Console.WriteLine();
