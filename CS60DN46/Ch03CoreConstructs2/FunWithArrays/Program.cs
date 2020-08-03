73297e0477698ee999e6ec764780e8fae00904aa
Wei Ding <ding_web@yahoo.co.jp>
2020-08-03 13:47:00 -0400

    multi dimentional array

--- CS60DN46/Ch03CoreConstructs2/FunWithArrays/Program.cs
+++ CS60DN46/Ch03CoreConstructs2/FunWithArrays/Program.cs
@@ -11,10 +11,25 @@ namespace FunWithArrays
 			SimpleArrays();
 			DeclareImplicitArrays();
 			ArrayOfObjects();
+			RectMultidimensionalArray();
 
 			Console.ResetColor();
 		}
 
+		static void RectMultidimensionalArray()
+		{
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("=> Rectangular multidimensional array.");
+			int[,] myMatrix;
+			myMatrix = new int[3, 4];
+			for (int i = 0; i < 3; i++) for (int j = 0; j < 4; j++) myMatrix[i, j] = i * j;
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 4; j++)
+					Console.Write(myMatrix[i, j] + "\t");
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
 		static void ArrayOfObjects()
 		{
 			Console.ForegroundColor = ConsoleColor.Cyan;
