28074934e39e1dd6c0faaae17fc4409474544584
Wei Ding <ding_web@yahoo.co.jp>
2020-08-04 22:22:58 -0400

    structs

---
+++ CS60DN46/Ch03CoreConstructs2/FunWithStructs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithStructs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Structs *****");
+
+			FirstLook();
+
+			Console.ResetColor();
+		}
+
+		static void FirstLook()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Point myPoint; myPoint.X = 349; myPoint.Y = 76; myPoint.Display();
+			myPoint.Increment(); myPoint.Display();
+
+			Point p1 = new Point(); p1.Display();
+		}
+
+		struct Point
+		{
+			public int X, Y;
+			public void Increment() { X++; Y++; }
+			public void Decrement() { X--; Y--; }
+			public void Display() { Console.WriteLine($"X = {X}, Y = {Y}"); }
+		}
+	}
+}
