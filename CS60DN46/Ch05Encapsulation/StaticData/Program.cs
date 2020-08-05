5830ac3977d64584ba1d925f6bbdaf499cecf61b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-05 19:19:50 -0400

    static point

---
+++ CS60DN46/Ch05Encapsulation/StaticData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Fun with Static Data *****");
+			StaticData();
+			Console.ResetColor();
+		}
+
+		static void StaticData()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			SavingsAccount s1 = new SavingsAccount(50);
+			Console.WriteLine($"Interest Rate is {SavingsAccount.GetInterestRate()}");
+
+			SavingsAccount.SetInterestRate(0.05);
+			SavingsAccount s2 = new SavingsAccount(100);
+			Console.WriteLine($"Interest Rate is {SavingsAccount.GetInterestRate()}");
+			
+			SavingsAccount s3 = new SavingsAccount(10000.75);
+			Console.WriteLine($"Interest Rate is {SavingsAccount.GetInterestRate()}");
+		}
+	}
+}
---
+++ CS60DN46/Ch05Encapsulation/StaticData/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticData
+{
+	class SavingsAccount
+	{
+		public double balance;
+		public static double interestRate = 0.04;
+		public SavingsAccount(double balance) { this.balance = balance; }
+
+		public static void SetInterestRate(double newRate) { interestRate = newRate; }
+		public static double GetInterestRate() { return interestRate; }
+	}
+}
