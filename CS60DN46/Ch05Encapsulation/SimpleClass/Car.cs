00099c0d3e91394a1870870a9bc62f6e1c3aacfe
Wei Ding <ding_web@yahoo.co.jp>
2020-08-05 15:03:55 -0400

    custom constructors

--- CS60DN46/Ch05Encapsulation/SimpleClass/Car.cs
+++ CS60DN46/Ch05Encapsulation/SimpleClass/Car.cs
@@ -8,6 +8,9 @@ namespace SimpleClass
 		public int speed;
 
 		public Car() { name = "Chuck"; speed = 10; }
+		public Car(string pn) { name = pn;  }
+		public Car(string pn, int cs) { name = pn; speed = cs; }
+
 		public void PrintState()
 		{
 			Console.WriteLine($"{name} is going {speed} MPH.");
--- CS60DN46/Ch05Encapsulation/SimpleClass/Program.cs
+++ CS60DN46/Ch05Encapsulation/SimpleClass/Program.cs
@@ -16,8 +16,14 @@ namespace SimpleClass
 		static void Speeding()
 		{
 			Console.ForegroundColor = ConsoleColor.Green;
-			Car car = new Car(); car.PrintState();
-			car.name = "Henry"; car.speed = 50;
+			
+			Car chunk = new Car(); chunk.PrintState();
+
+			Car mary = new Car("Mary"); mary.PrintState();
+
+			Car daisy = new Car("Daisy", 75); daisy.PrintState();
+
+			Car car = new Car();  car.name = "Henry"; car.speed = 50;
 			for(int i = 0; i< 3; i++) { car.SpeedUp(5); car.PrintState(); }
 		}
 	}
