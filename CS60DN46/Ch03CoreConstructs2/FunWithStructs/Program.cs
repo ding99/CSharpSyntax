d4c2525fb26cb807cf0dd5d5608d9641d3ca8de6
Wei Ding <ding_web@yahoo.co.jp>
2020-08-05 01:22:38 -0400

    value type and reference type assignment

--- CS60DN46/Ch03CoreConstructs2/FunWithStructs/Program.cs
+++ CS60DN46/Ch03CoreConstructs2/FunWithStructs/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace FunWithStructs
 {
@@ -13,8 +10,39 @@ namespace FunWithStructs
 			Console.WriteLine("***** Structs *****");
 
 			FirstLook();
+			ValueTypeAssignment();
+			ReferenceTypeAssignment();
 
 			Console.ResetColor();
+		}
+
+		static void ReferenceTypeAssignment()
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("Assigning reference types");
+
+			PointRef p1 = new PointRef(10, 10);
+			PointRef p2 = p1;
+
+			p1.Display(); p2.Display();
+			p1.X = 100; Console.WriteLine("-> Changed p1.X");
+			p1.Display(); p2.Display();
+		}
+
+
+		static void ValueTypeAssignment()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Assigning value types");
+
+			Point p1 = new Point(10, 10);
+			Point P2 = p1;
+
+			p1.Display(); P2.Display();
+
+			p1.X = 100;
+			Console.WriteLine("-> Changed p1.X");
+			p1.Display(); P2.Display();
 		}
 
 		static void FirstLook()
@@ -29,6 +57,16 @@ namespace FunWithStructs
 		struct Point
 		{
 			public int X, Y;
+			public Point(int XPos, int Ypos) { X = XPos; Y = Ypos; }
+			public void Increment() { X++; Y++; }
+			public void Decrement() { X--; Y--; }
+			public void Display() { Console.WriteLine($"X = {X}, Y = {Y}"); }
+		}
+
+		class PointRef
+		{
+			public int X, Y;
+			public PointRef(int XPos, int YPos) { X = XPos; Y = YPos; }
 			public void Increment() { X++; Y++; }
 			public void Decrement() { X--; Y--; }
 			public void Display() { Console.WriteLine($"X = {X}, Y = {Y}"); }
