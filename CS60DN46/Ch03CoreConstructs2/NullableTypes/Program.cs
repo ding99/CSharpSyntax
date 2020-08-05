e473d0ec830cc7eb5b1c94dc93e8723ff535ae4a
Wei Ding <ding_web@yahoo.co.jp>
2020-08-05 03:57:14 -0400

    nullable

---
+++ CS60DN46/Ch03CoreConstructs2/NullableTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace NullableTypes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Fun with Nullable Data *****");
+
+			NullableData();
+			NullConditionalOpe();
+
+			Console.ResetColor();
+		}
+
+		static void NullConditionalOpe()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			TesterMethod(new string[] { "First Row", "Second Row" });
+			TesterMethod(null);
+		}
+		static void TesterMethod(string[] args)
+		{
+			Console.WriteLine($"You sent me {args?.Length ?? 0} argument.");
+		}
+
+		static void NullableData()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			DatabaseReader dr = new DatabaseReader();
+
+			int? i = dr.GetIntFromDatabase();
+			if (i.HasValue) Console.WriteLine($"Value of 'i' is {i.Value}");
+			else Console.WriteLine("Value of 'i' is undefined.");
+
+			bool? b = dr.GetBoolFromDatabse();
+			if(b != null)
+				Console.WriteLine($"Value of 'b' is {b.Value}");
+			else Console.WriteLine("Value of 'b' is undefined.");
+		}
+
+		class DatabaseReader
+		{
+			public int? numericValue = null;
+			public bool? boolValue = true;
+			public int? GetIntFromDatabase() { return numericValue; }
+			public bool? GetBoolFromDatabse() { return boolValue; }
+	}
+}
+}
