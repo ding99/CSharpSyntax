305bce4e1208ee09ea3890fd088d35dd1a7db63a
Wei Ding <ding_web@yahoo.co.jp>
2020-08-06 00:18:33 -0400

    static class

---
+++ CS60DN46/Ch05Encapsulation/SimpleUtility/TimeUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleUtility
+{
+	static class TimeUtil
+	{
+		public static void PrintTime() {
+			Console.WriteLine(DateTime.Now.ToShortTimeString());
+		}
+		public static void PrintDate() {
+			Console.WriteLine(DateTime.Now.ToShortDateString());
+		}
+	}
+}
