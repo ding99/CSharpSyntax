b07604709003b775c239238e30b2dbdbaafe376e
Wei Ding <ding_web@yahoo.co.jp>
2020-08-07 10:28:21 -0400

    add encapsulation services project

---
+++ CS60DN46/Ch05Encapsulation/EncapsulationService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EncapsulationService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Encapsulotion Services");
+			Reading();
+			Console.ResetColor();
+		}
+
+		static void Reading()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Book miniNovel = new Book();
+			miniNovel.numberOfPages = 30000000;
+		}
+	}
+}
