24d95b6d0a7a3b9bf0fb1d4e32d0918e9033b08a
Wei Ding <ding_web@yahoo.co.jp>
2020-08-07 11:04:24 -0400

    traditional accessor and mutator

---
+++ CS60DN46/Ch05Encapsulation/EmployeeApp/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+	class Employee
+	{
+		private string name;
+		private int id;
+		private float pay;
+
+		public Employee() { }
+		public Employee(string name, int id, float pay)
+		{
+			this.name = name; this.id = id; this.pay = pay;
+		}
+
+		public string GetName() { return name; }
+		public void SetName(string name) {
+			if (name.Length > 15)
+				Console.WriteLine("Error! Name length exceeds 15 characters!");
+			else this.name = name;
+		}
+
+		public void GiveBonus(float amount)
+		{
+			pay += amount;
+		}
+
+		public void Display()
+		{
+			Console.WriteLine($"Name: {name}");
+			Console.WriteLine($"ID: {id}");
+			Console.WriteLine($"Pay: {pay}");
+		}
+	}
+
+}
---
+++ CS60DN46/Ch05Encapsulation/EmployeeApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Employee *****");
+			Traditional();
+			Console.ResetColor();
+		}
+
+		static void Traditional()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> Traditional accessors and mutators");
+
+			Employee emp = new Employee("Marvin", 456, 30000);
+			emp.GiveBonus(1000);
+			emp.Display();
+
+			Console.WriteLine("-> Set a new name");
+			emp.SetName("Marv");
+			emp.Display();
+		}
+	}
+}
