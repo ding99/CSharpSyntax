a2b6523c732f1997b9442200ecc206a6851fd417
Wei Ding <ding_web@yahoo.co.jp>
2020-08-09 17:42:14 -0400

    initializations

---
+++ CS60DN46/Ch05Encapsulation/ObjectInitializers/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ObjectInitializers
+{
+	class Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Point(int x, int y) { X = x; Y = y; }
+		public Point() { }
+
+		public void Display()
+		{
+			Console.WriteLine($"[{X}, {Y}]");
+		}
+	}
+}
---
+++ CS60DN46/Ch05Encapsulation/ObjectInitializers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ObjectInitializers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Object Init Syntax *****");
+			Manually();
+			Console.ResetColor();
+		}
+
+		static void Manually()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			
+			Point p1 = new Point();
+			p1.X = 10;
+			p1.Y = 10;
+			p1.Display();
+
+			Point p2 = new Point(20, 20);
+			p2.Display();
+
+			Point p3 = new Point { X = 30, Y = 30 };
+			p3.Display();
+		}
+	}
+}
