1e4760351d366cada503e8edebd0d04176756050
Wei Ding <ding_web@yahoo.co.jp>
2020-08-09 17:48:32 -0400

    more options

--- CS60DN46/Ch05Encapsulation/ObjectInitializers/Program.cs
+++ CS60DN46/Ch05Encapsulation/ObjectInitializers/Program.cs
@@ -15,16 +15,11 @@ namespace ObjectInitializers
 		{
 			Console.ForegroundColor = ConsoleColor.Green;
 			
-			Point p1 = new Point();
-			p1.X = 10;
-			p1.Y = 10;
-			p1.Display();
-
-			Point p2 = new Point(20, 20);
-			p2.Display();
-
-			Point p3 = new Point { X = 30, Y = 30 };
-			p3.Display();
+			Point p1 = new Point(); p1.X = 10; p1.Y = 10; p1.Display();
+			Point p2 = new Point(20, 20); p2.Display();
+			Point p3 = new Point { X = 30, Y = 30 }; p3.Display();
+			Point p5 = new Point { X = 50 }; p5.Display();
+			new Point { Y = 60 }.Display();
 		}
 	}
 }
