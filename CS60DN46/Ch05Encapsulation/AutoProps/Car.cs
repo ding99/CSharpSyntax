d0d8c10e8d73cfb91325c51332f262a3ce30258a
Wei Ding <ding_web@yahoo.co.jp>
2020-08-09 15:39:09 -0400

    status

---
+++ CS60DN46/Ch05Encapsulation/AutoProps/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AutoProps
+{
+	class Car
+	{
+		public string Name { get; set; }
+		public int Speed { get; set; }
+		public string Color { get; set; }
+
+		public void Display()
+		{
+			Console.WriteLine($"Car Name {Name}, Speed {Speed}, Color {Color}");
+		}
+	}
+}
---
+++ CS60DN46/Ch05Encapsulation/AutoProps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AutoProps
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** Automatic Properties *****");
+			Status();
+			Console.ResetColor();
+		}
+
+		static void Status()
+		{
+			Console.ForegroundColor = ConsoleColor.Green; ;
+			Console.WriteLine("=> Status");
+			Car c = new Car() { Name = "Frank", Color = "Red" };
+			Console.WriteLine($"Your car is named {c.Name}?");
+			c.Display();
+
+			c.Speed = 55; c.Display();
+		}
+	}
+}
