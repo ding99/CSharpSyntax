d0e6a6925b0dfffd8236a1c3e4a60795c9d906c5
Wei Ding <ding_web@yahoo.co.jp>
2020-08-09 18:39:01 -0400

    const data

---
+++ CS60DN46/Ch05Encapsulation/ConstData/MyMath.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstData
+{
+	class MyMath
+	{
+		public const double PI = 3.14;
+	}
+}
---
+++ CS60DN46/Ch05Encapsulation/ConstData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConstData
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Const Data *****");
+			PIValue();
+			Console.ResetColor();
+		}
+
+		static void PIValue()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine($"The value of PI is: {MyMath.PI}");
+		}
+	}
+}
