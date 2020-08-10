52cb1fccffd21bc02ffc557f841a73043338179b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-10 01:02:29 -0400

    employees architecture

---
+++ CS60DN46/Ch06InheritancePolymorphism/Employees/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Employees
+{
+	partial class Employee
+	{
+		public void GiveBonus(float amount) { pay += amount; }
+
+		public void Display()
+		{
+			Console.WriteLine($"Name: {Name}");
+			Console.WriteLine($"ID: {ID}");
+			Console.WriteLine($"Age: {Age}");
+			Console.WriteLine($"Pay: {Pay}");
+			Console.WriteLine($"SSN: {SSN}");
+		}
+	}
+}
