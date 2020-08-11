461e8e47a98ff38c250f7acd9a9c2304be77795f
Wei Ding <ding_web@yahoo.co.jp>
2020-08-11 00:44:13 -0400

    start interface

---
+++ CS60DN46/Ch06InheritancePolymorphism/Shapes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shapes
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** Polymorphic Interface *****");
+			Console.ResetColor();
+		}
+	}
+}
