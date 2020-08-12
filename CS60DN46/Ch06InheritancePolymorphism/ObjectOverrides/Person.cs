00365eaffaf0736de038b2538e79b69ba3f74a03
Wei Ding <ding_web@yahoo.co.jp>
2020-08-11 23:38:07 -0400

    system object

---
+++ CS60DN46/Ch06InheritancePolymorphism/ObjectOverrides/Person.cs
@@ -0,0 +1,6 @@
+﻿namespace ObjectOverrides
+{
+	class Person
+	{
+	}
+}
---
+++ CS60DN46/Ch06InheritancePolymorphism/ObjectOverrides/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ObjectOverrides
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** System.Object *****");
+			OrigMethods();
+			Console.ResetColor();
+		}
+
+		static void OrigMethods()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> Original Methods");
+
+			Person p1 = new Person();
+			Console.WriteLine($"ToString: {p1.ToString()}");
+			Console.WriteLine($"Hash Code: {p1.GetHashCode()}");
+			Console.WriteLine($"Type: {p1.GetType()}");
+
+			Person p2 = p1;
+			object o = p2;
+			if(o.Equals(p1) && p2.Equals(o))
+				Console.WriteLine("Same instance!");
+		}
+	}
+}
