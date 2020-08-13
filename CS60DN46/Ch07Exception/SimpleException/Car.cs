96e0c156b1a23ed2d3f2872e63fe8ac1252b83bd
Wei Ding <ding_web@yahoo.co.jp>
2020-08-12 23:24:25 -0400

    start simple exception

---
+++ CS60DN46/Ch07Exception/SimpleException/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+	class Car
+	{
+		public const int MaxSpeed = 100;
+		public int CurrSpeed { get; set; } = 0;
+		public string PetName { get; set; } = "";
+		private bool carIsDead;
+
+		private Radio theMusicBox = new Radio();
+
+		public Car() { }
+		public Car(string name, int speed) { CurrSpeed = speed; PetName = name; }
+
+		public void CrankTunes(bool state)
+		{
+			theMusicBox.TrunOn(state);
+		}
+
+		public void Accelerate(int delta)
+		{
+			if(carIsDead)
+				Console.WriteLine($"{PetName} is out of order...");
+			else
+			{
+				CurrSpeed += delta;
+				if(CurrSpeed > MaxSpeed)
+				{
+					Console.WriteLine($"{PetName} has overheated!");
+					CurrSpeed = 0;
+					carIsDead = true;
+				}
+				else
+					Console.WriteLine($"=> CurrentSpeed = {CurrSpeed}");
+			}
+		}
+	}
+}
---
+++ CS60DN46/Ch07Exception/SimpleException/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleException
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Simple Exception *****");
+			CreatingCar();
+			Console.ResetColor();
+		}
+
+		static void CreatingCar()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> Creating a car and stepping on it!");
+			Car car = new Car("Zippy", 20);
+			car.CrankTunes(true);
+
+			for (int i = 0; i < 10; i++)
+				car.Accelerate(10);
+		}
+	}
+}
