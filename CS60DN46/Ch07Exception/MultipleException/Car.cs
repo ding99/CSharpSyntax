ad20e3d9df6e39c9340bd97caec274a75b326974
Wei Ding <ding_web@yahoo.co.jp>
2020-08-13 16:00:06 -0400

    project for processing multiple exceptions

---
+++ CS60DN46/Ch07Exception/MultipleException/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MultipleException
+{
+	class Car
+	{
+		public const int MaxSpeed = 100;
+		public int CurrSpeed { get; set; } = 0;
+		public string PetName { get; set; } = "";
+		private bool carIsDead;
+
+		private Radio theMusicBox = new Radio();
+
+		public Car() { }
+		public Car(string name, int speed) { CurrSpeed = speed; PetName = name; }
+
+		public void CrankTunes(bool state)
+		{
+			theMusicBox.TrunOn(state);
+		}
+
+		public void Accelerate2(int delta)
+		{
+			if (carIsDead)
+				Console.WriteLine($"{PetName} is out of order...");
+			else
+			{
+				CurrSpeed += delta;
+				if (CurrSpeed >= MaxSpeed)
+				{
+					CurrSpeed = 0;
+					carIsDead = true;
+					CarIsDeadException2 ex = new CarIsDeadException2(string.Format($"{PetName} has overheated!"), "You have a lead foot", DateTime.Now);
+					ex.HelpLink = @"http://www.carsrus.com";
+					throw ex;
+				}
+				else
+					Console.WriteLine($"=> CurrentSpeed = {CurrSpeed}");
+			}
+		}
+	}
+}
---
+++ CS60DN46/Ch07Exception/MultipleException/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultipleException
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***** Handling Multiple Exceptions *****");
+			Console.ResetColor();
+		}
+	}
+}
