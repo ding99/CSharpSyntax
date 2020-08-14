ef314bd61715a796a55cc0aa0fde0bc4549b6086
Wei Ding <ding_web@yahoo.co.jp>
2020-08-14 10:39:22 -0400

    using as and is keywords

--- CS60DN46/Ch08Interface/CustomInterface/Program.cs
+++ CS60DN46/Ch08Interface/CustomInterface/Program.cs
@@ -4,14 +4,44 @@ namespace CustomInterface
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
 			Console.WriteLine("***** Interfaces *****");
 			InvokeInterface();
 			DetermineInterface();
+			ByAs();
+			ByIs();
 			Console.ResetColor();
 		}
 
+		static void ByIs()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine("=> Using Is keyword");
+
+			Shape[] shapes = { new Hexagon(), new Circle(), new Triangle("Joe"), new Circle("JoJo") };
+			for (int i = 0; i < shapes.Length; i++)
+			{
+				shapes[i].Draw();
+				if(shapes[i] is IPointy)
+					Console.WriteLine($"-> Points: {((IPointy)shapes[i]).Points}");
+				else Console.WriteLine($"-> {shapes[i].PetName}\'s not pointy!");
+			}
+		}
+
+		static void ByAs()
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Using As keyword");
+
+			Hexagon h2 = new Hexagon("Peter");
+			IPointy ip2 = h2 as IPointy;
+
+			if(ip2 != null)
+				Console.WriteLine($"Points: {ip2.Points}");
+			else
+				Console.WriteLine("OOPS! Not pointy...");
+		}
 		static void DetermineInterface()
 		{
 			Console.ForegroundColor = ConsoleColor.Yellow;
