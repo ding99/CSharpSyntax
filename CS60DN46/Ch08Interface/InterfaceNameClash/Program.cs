3624ad53ca1d3350e5216658a3b50315a5bab380
Wei Ding <ding_web@yahoo.co.jp>
2020-08-14 18:36:23 -0400

    explicit interface implementation

---
+++ CS60DN46/Ch08Interface/InterfaceNameClash/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InterfaceNameClash
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** Interface Name Clashes *****");
+			WhichInterface();
+			Console.ResetColor();
+		}
+
+		static void WhichInterface()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> which interface");
+
+			Octagon oct = new Octagon();
+			((IDrawToForm)oct).Draw();
+			((IDrawToMemory)oct).Draw();
+			((IDrawToPrinter)oct).Draw();
+		}
+	}
+}
