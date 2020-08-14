a97b1295291021bcfd086136324b0f7714182af2
Wei Ding <ding_web@yahoo.co.jp>
2020-08-14 10:28:06 -0400

    determine whether an interface is supported

--- CS60DN46/Ch08Interface/CustomInterface/Program.cs
+++ CS60DN46/Ch08Interface/CustomInterface/Program.cs
@@ -8,7 +8,30 @@ namespace CustomInterface
 		{
 			Console.WriteLine("***** Interfaces *****");
 			InvokeInterface();
+			DetermineInterface();
 			Console.ResetColor();
+		}
+
+		static void DetermineInterface()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Determine whether a type supports a specific interface");
+
+			Circle c = new Circle();
+
+			Console.WriteLine($"Circle supports IPointy: <{c is IPointy}>");
+			Console.WriteLine($"Triangle supports IPointy: <{new Triangle() is IPointy}>");
+
+			IPointy ip = null;
+			try
+			{
+				ip = (IPointy)c;
+				Console.WriteLine($"The points is {ip.Points}");
+			}
+			catch(InvalidCastException e)
+			{
+				Console.WriteLine($"Invalid cast error: {e.Message}");
+			}
 		}
 
 		static void InvokeInterface()
