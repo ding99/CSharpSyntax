5c2b85cfefccfe1a056e5c8683ebb219b955ac5b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-15 18:56:48 -0400

    IComparer

---
+++ CS60DN46/Ch08Interface/ComparableCar/PetNameComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace ComparableCar
+{
+	class PetNameComparer : IComparer
+	{
+		int IComparer.Compare(object x, object y)
+		{
+			Car c1 = x as Car;
+			Car c2 = y as Car;
+			if (c1 != null && c2 != null)
+				return String.Compare(c1.PetName, c2.PetName);
+			else throw new ArgumentException("Parameter is not a Car!");
+		}
+	}
+}
--- CS60DN46/Ch08Interface/ComparableCar/Program.cs
+++ CS60DN46/Ch08Interface/ComparableCar/Program.cs
@@ -8,7 +8,32 @@ namespace ComparableCar
 		{
 			Console.WriteLine("***** Comparable Interface *****");
 			ObjectSorting();
+			MultiSort();
 			Console.ResetColor();
+		}
+
+		static void MultiSort()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Multi Sort");
+
+			Car[] cars = new Car[5];
+			cars[0] = new Car("Rusty", 80, 1);
+			cars[1] = new Car("Mary", 70, 32);
+			cars[2] = new Car("Aiper", 60, 32);
+			cars[3] = new Car("Mel", 50, 4);
+			cars[4] = new Car("Chucky", 30, 5);
+
+			Console.WriteLine("Here is the unordered set of cars:");
+			foreach (Car c in cars)
+				Console.Write($" ({c.CarID},{c.PetName})");
+			Console.WriteLine();
+
+			Array.Sort(cars, new PetNameComparer());
+			Console.WriteLine("Here is the ordered set of cars:");
+			foreach (Car c in cars)
+				Console.Write($" ({c.CarID},{c.PetName})");
+			Console.WriteLine();
 		}
 
 		static void ObjectSorting()
