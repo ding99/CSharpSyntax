3e77711e6169b7edd6d78b3a5aa9bceafea18f23
Wei Ding <ding_web@yahoo.co.jp>
2020-08-15 01:35:56 -0400

    ienumerator

---
+++ CS60DN46/Ch08Interface/CustomEnumerator/Garage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace CustomEnumerator
+{
+	class Garage : IEnumerable
+	{
+		private Car[] cars = new Car[4];
+		public Garage()
+		{
+			cars[0] = new Car("Rusty", 30);
+			cars[1] = new Car("Clunker", 55);
+			cars[2] = new Car("Zippy", 30);
+			cars[3] = new Car("Fred", 30);
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return cars.GetEnumerator();
+		}
+	}
+}
---
+++ CS60DN46/Ch08Interface/CustomEnumerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomEnumerator
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** IEnumerable and IEnumerator *****");
+			Enumerators();
+			Console.ResetColor();
+		}
+
+		static void Enumerators()
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("=> custom enumerator");
+
+			Garage carLot = new Garage();
+			foreach(Car c in carLot)
+				Console.WriteLine($"{c.PetName} is going {c.CurrSpeed} MPH");
+		}
+	}
+}
