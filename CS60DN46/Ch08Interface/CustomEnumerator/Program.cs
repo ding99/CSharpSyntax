28100511b7c4d772b5eebd06550072ed863d842b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-25 13:33:28 -0400

    add comment

--- CS60DN46/Ch08Interface/CustomEnumerator/Program.cs
+++ CS60DN46/Ch08Interface/CustomEnumerator/Program.cs
@@ -26,9 +26,23 @@ namespace CustomEnumerator
 			IEnumerator enumerator = carLot.GetEnumerator();
 			while (enumerator.MoveNext())
 			{
-				Car car = (Car)enumerator.Current;
-				Console.WriteLine($"{car.PetName} is going {car.CurrSpeed} MPH (M)");
+				Car c = (Car)enumerator.Current;
+				Console.WriteLine($"{c.PetName} is going {c.CurrSpeed} MPH (M)");
 			}
 		}
+
+		/** result:
+		***** IEnumerable and IEnumerator *****
+		=> custom enumerator
+		Rusty is going 30 MPH
+		Clunker is going 55 MPH
+		Zippy is going 30 MPH
+		Fred is going 30 MPH
+		-> use custom enumerator manually
+		Rusty is going 30 MPH (M)
+		Clunker is going 55 MPH (M)
+		Zippy is going 30 MPH (M)
+		Fred is going 30 MPH (M)
+		**/
 	}
 }
