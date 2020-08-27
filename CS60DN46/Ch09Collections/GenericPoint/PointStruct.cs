9698d7202994037f0e6a0f641a167b1992702884
Wei Ding <ding_web@yahoo.co.jp>
2020-08-27 00:32:59 -0400

    custom generic struct

---
+++ CS60DN46/Ch09Collections/GenericPoint/PointStruct.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericPoint
+{
+	public struct Point<T>
+	{
+		private T x, y;
+
+		public Point(T xVal, T yVal) { x = xVal; y = yVal; }
+		public T X { get { return x; } set { x = value; } }
+		public T Y { get { return y; } set { y = value; } }
+		public override string ToString() { return string.Format("[{0}, {1}]", x, y); }
+		public void ResetPoint() { x = default(T); y = default(T); }
+	}
+}
---
+++ CS60DN46/Ch09Collections/GenericPoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericPoint
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** Custom Generic Structures and Classes *****");
+			PracticeStruct();
+			Console.ResetColor();
+		}
+
+		static void PracticeStruct()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> invoke custom generic struct");
+
+			Point<int> p = new Point<int>(10, 20);
+			Console.WriteLine($"int type: <{p}>");
+			p.ResetPoint();
+			Console.WriteLine($"int type (reset): <{p}>");
+
+			Point<double> p2 = new Point<double>(5.3, 7.88);
+			Console.WriteLine($"double type: <{p2}>");
+			p2.ResetPoint();
+			Console.WriteLine($"double type (reset): <{p2}>");
+		}
+	}
+}
