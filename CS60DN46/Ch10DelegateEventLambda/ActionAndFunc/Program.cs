dc593e97783fbc239dc05d79b5f7c7a04193ddbc
Wei Ding <ding_web@yahoo.co.jp>
2020-08-28 13:51:50 -0400

    action delegate

---
+++ CS60DN46/Ch10DelegateEventLambda/ActionAndFunc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ActionAndFunc
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("***** Action and Func Telegates *****");
+			PracticeAction();
+			Console.ResetColor();
+		}
+
+		static void PracticeAction()
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Practice Action Delegate");
+
+			Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
+			actionTarget("Action Message!", ConsoleColor.Yellow, 3);
+		}
+
+		static void DisplayMessage(string msg, ConsoleColor color, int count)
+		{
+			ConsoleColor prev = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			for (int i = 0; i < count; i++) Console.WriteLine(msg);
+			Console.ForegroundColor = prev;
+		}
+	}
+}
