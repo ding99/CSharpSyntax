86f38331eecf9bf1881fb59e4ca0db9196ddc177
Wei Ding <ding_web@yahoo.co.jp>
2020-08-29 19:35:23 -0400

    examine insert

--- CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/Program.cs
+++ CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/Program.cs
@@ -15,12 +15,12 @@ namespace SimpleIndexer {
 			PersonCollection people = new PersonCollection();
 			people[0] = new Person("Homer", "Simpson", 40);
 			people[1] = new Person("Marge", "Simpson", 38);
-			people[2] = new Person("Lisa", "Simpson", 9);
+			people[1] = new Person("Lisa", "Simpson", 9);
 			people[3] = new Person("Bart", "Simpson", 7);
 			people[4] = new Person("Maggie", "Simpson", 2);
 
 			for(int i = 0; i < people.Count; i++)
-				Console.WriteLine($"Person Number {i}, Name: {people[i].FirstName} {people[i].LastName}, Age: {people[i].Age}");
+				Console.WriteLine($"Person Number: {i}, Name: {people[i].FirstName} {people[i].LastName}, Age: {people[i].Age}");
 		}
 	}
 }
