43dac9bfd20f44720de7bade5aebe93a5bfb9b1b
Wei Ding <ding_web@yahoo.co.jp>
2020-08-28 21:45:16 -0400

    event

---
+++ CS60DN46/Ch10DelegateEventLambda/CarEvents/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CarEvents {
+	class Car {
+		public int Speed { get; set; }
+		public int MaxSpeed { get; set; }
+		public string Name { get; set; }
+
+		private bool carIsDead;
+
+		public Car() { }
+		public Car(string name, int max, int speed) {
+			Speed = speed; MaxSpeed = max; Name = name;
+		}
+
+		public delegate void CarEngineHandler(string msg);
+
+		public event CarEngineHandler Exploaded;
+		public event CarEngineHandler AboutToBlow;
+
+		public void Accelerate(int delta) {
+			if (carIsDead) {
+				if (Exploaded != null)
+					Exploaded("Sorry, this car is dead...");
+			} else {
+				Speed += delta;
+
+				if (10 >= (MaxSpeed - Speed) && AboutToBlow != null)
+					AboutToBlow("Careful buddy! Gonna blow!");
+			}
+
+			if (Speed >= MaxSpeed) carIsDead = true;
+			else Console.WriteLine($"Current Speed = {Speed}");
+		}
+
+		public void Reset() {
+			carIsDead = false;
+			Speed = 10;
+		}
+	}
+}
---
+++ CS60DN46/Ch10DelegateEventLambda/CarEvents/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CarEvents {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Car Events *****");
+			EventKeyword();
+			Console.ResetColor();
+		}
+
+		static void EventKeyword() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Events");
+
+			Car c1 = new Car("SlugBug", 100, 10);
+			//c1.AboutToBlow += CarIsAlmostDoomed;
+			//c1.AboutToBlow += CarAboutToBlow;
+			c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+			c1.AboutToBlow += new Car.CarEngineHandler(CarAboutToBlow);
+
+			Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
+			c1.Exploaded += d;
+
+			Console.WriteLine("===== Speeding up");
+			for (int i = 0; i < 7; i++) c1.Accelerate(20);
+
+			c1.Exploaded -= d;
+
+			c1.Reset();
+			Console.WriteLine("----- Speeding up");
+			for (int i = 0; i < 7; i++) c1.Accelerate(20);
+		}
+
+		public static void CarExploded(string msg) {
+			ConsoleColor fore = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine($"   {msg}");
+			Console.ForegroundColor = fore;
+		}
+		public static void CarAboutToBlow(string msg) { Console.WriteLine($"   {msg}"); }
+		public static void CarIsAlmostDoomed(string msg) {
+			ConsoleColor fore = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine($"=> Critical Message from Car: {msg}");
+			Console.ForegroundColor = fore;
+		}
+	}
+}
