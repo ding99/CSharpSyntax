7f37f4760838e2d117e7452f2600b40c7265a602
Wei Ding <ding_web@yahoo.co.jp>
2020-08-29 03:48:28 -0400

    simple problem

---
+++ CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace SimpleIndexer {
+	class PersonCollection : IEnumerable
+	{
+		private ArrayList people = new ArrayList();
+
+		public Person GetPerson(int pos) { return (Person)people[pos]; }
+		public void AddPerson(Person p) { people.Add(p); }
+		public void ClearPeople() { people.Clear(); }
+		public int Count { get { return people.Count; } }
+
+		IEnumerator IEnumerable.GetEnumerator() { return people.GetEnumerator(); }
+	}
+}
---
+++ CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace SimpleIndexer {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Indexer Methods *****");
+			SimpleIndexing();
+			Console.ResetColor();
+		}
+
+		static void SimpleIndexing() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Simple Indexer");
+
+			PersonCollection people = new PersonCollection();
+			people[0] = new Person("Homer", "Simpson", 40);
+			people[1] = new Person("Marge", "Simpson", 38);
+			people[2] = new Person("Lisa", "Simpson", 9);
+			people[3] = new Person("Bart", "Simpson", 7);
+			people[4] = new Person("Maggie", "Simpson", 2);
+
+			for(int i = 0; i < people.Count; i++)
+				Console.WriteLine($"Person: Number {i}, Name {people[i].FirstName} {people[i].LastName}, Age {people[i].Age}");
+		}
+	}
+}
