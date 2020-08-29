9435a13c9da2184d31df182a9d540878ee211e91
Wei Ding <ding_web@yahoo.co.jp>
2020-08-29 02:19:52 -0400

    simple lambda

--- CS60DN46/Ch10DelegateEventLambda/SimpleLambdaExpressions/Program.cs
+++ CS60DN46/Ch10DelegateEventLambda/SimpleLambdaExpressions/Program.cs
@@ -7,7 +7,25 @@ namespace SimpleLambdaExpressions {
 			Console.WriteLine("***** Lambdas *****");
 			TraditionalDelegateSyntax();
 			AnonymousMethodSyntax();
+			LambdaExpressionSyntax();
 			Console.ResetColor();
+		}
+
+		static void LambdaExpressionSyntax() {
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine("=> Lambda Expression Syntax");
+
+			List<int> list = new List<int>();
+			list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+			Console.Write("Here are original numbers :");
+			foreach (int e in list) Console.Write($" {e}");
+			Console.WriteLine();
+
+			List<int> evens = list.FindAll(i => (i % 2) == 0);
+
+			Console.Write("Here are your even numbers:");
+			foreach (int e in evens) Console.Write($" {e}");
+			Console.WriteLine();
 		}
 
 		static void AnonymousMethodSyntax() {
