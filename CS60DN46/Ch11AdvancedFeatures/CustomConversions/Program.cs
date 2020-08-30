2c541cca338befd1bc47238820af4743055191f7
Wei Ding <ding_web@yahoo.co.jp>
2020-08-30 19:21:16 -0400

    custom conversion routine

---
+++ CS60DN46/Ch11AdvancedFeatures/CustomConversions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomConversions {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Custom Conversion *****");
+			StructConversion();
+			Console.ResetColor();
+		}
+
+		static void StructConversion() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Convert a rectangle to a square");
+
+			Rectangle r = new Rectangle(15, 4);
+			Console.Write($"Rectangle: {r}. "); r.Draw();
+
+			Square s = (Square)r;
+			Console.Write($"Square: {s}. "); s.Draw();
+		}
+	}
+}
