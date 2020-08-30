8972be1a26c1e247a97d2f7c1375075c06d14f92
Wei Ding <ding_web@yahoo.co.jp>
2020-08-30 19:30:26 -0400

    explicit conversion

--- CS60DN46/Ch11AdvancedFeatures/CustomConversions/Program.cs
+++ CS60DN46/Ch11AdvancedFeatures/CustomConversions/Program.cs
@@ -5,7 +5,19 @@ namespace CustomConversions {
 		static void Main() {
 			Console.WriteLine("***** Custom Conversion *****");
 			StructConversion();
+			ConvertRectangle();
 			Console.ResetColor();
+		}
+
+		static void ConvertRectangle() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Explicit conversion operatoin");
+			Rectangle rect = new Rectangle(10, 5);
+			Console.WriteLine($"Original rectangle: {rect}");
+			DrawSquare((Square)rect);
+		}
+		static void DrawSquare(Square sq) {
+			Console.Write($"Square: {sq}. "); sq.Draw();
 		}
 
 		static void StructConversion() {
