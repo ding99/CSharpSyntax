f19974451859a135afb10d3d4d4437e4cf94db05
Wei Ding <ding_web@yahoo.co.jp>
2020-08-30 01:50:38 -0400

    operator keyword

---
+++ CS60DN46/Ch11AdvancedFeatures/OverloadedOps/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace OverloadedOps {
+	class Point {
+		public int X { set; get; }
+		public int Y { set; get; }
+		public Point(int x, int y) { X = x; Y = y; }
+		public override string ToString() { return $"[{X}, {Y}]"; }
+
+		public static Point operator + (Point p1, Point p2) {
+			return new Point(p1.X + p2.X, p1.Y + p2.Y);
+		}
+		public static Point operator - (Point p1, Point p2) {
+			return new Point(p1.X - p2.X, p1.Y - p2.Y);
+		}
+	}
+}
---
+++ CS60DN46/Ch11AdvancedFeatures/OverloadedOps/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadedOps {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Overloading Binary Operators");
+			TwoPoints();
+			Console.ResetColor();
+		}
+
+		static void TwoPoints() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Two Points");
+
+			Point ptOne = new Point(100, 100), ptTwo = new Point(30, 30);
+			Console.WriteLine($"ptOne = {ptOne}; ptTwo = {ptTwo}");
+
+			Console.WriteLine($"phOne + ptTwo: {ptOne + ptTwo}");
+			Console.WriteLine($"phOne + ptTwo: {ptOne - ptTwo}");
+		}
+	}
+}
