c3be1687cf4178ab08be24e88dd3168a7d00e640
Wei Ding <ding_web@yahoo.co.jp>
2020-08-29 20:31:39 -0400

    use generic list

--- CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/Program.cs
+++ CS60DN46/Ch11AdvancedFeatures/SimpleIndexer/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SimpleIndexer {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Indexer Methods *****");
 			SimpleIndexing();
+			UseGenericList();
 			Console.ResetColor();
+		}
+
+		static void UseGenericList() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Use Generic List of Person");
+
+			List<Person> people = new List<Person>();
+
+			people.Add(new Person("Lisa", "Simpson", 9));
+			people.Add(new Person("Bart", "Simpson", 7));
+
+			people[0] = new Person("Maggie", "Simpson", 2);
+
+			for(int i = 0; i < people.Count; i++)
+				Console.WriteLine($"Person Number: {i}, Name: {people[i].FirstName} {people[i].LastName}, Age: {people[i].Age}");
 		}
 
 		static void SimpleIndexing() {
