130034152b64af301214fcf35927f141db0110f3
Wei Ding <ding_web@yahoo.co.jp>
2020-08-30 02:10:00 -0400

    shorthand assignment

--- CS60DN46/Ch11AdvancedFeatures/OverloadedOps/Program.cs
+++ CS60DN46/Ch11AdvancedFeatures/OverloadedOps/Program.cs
@@ -5,7 +5,21 @@ namespace OverloadedOps {
 		static void Main() {
 			Console.WriteLine("***** Overloading Binary Operators");
 			TwoPoints();
+			ShortHandAssign();
 			Console.ResetColor();
+		}
+
+		static void ShortHandAssign() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Shorthand assignment operators");
+
+			Point ptOne = new Point(100, 100), ptTwo = new Point(30, 30);
+			Point ptThree = new Point(90,5), ptFour = new Point(0, 500);
+			Console.WriteLine($"ptOne = {ptOne}; ptTwo = {ptTwo}");
+			Console.WriteLine($"ptThree = {ptThree}; ptFour = {ptFour}");
+
+			Console.WriteLine($"phThree += ptTwo: {ptThree += ptTwo}");
+			Console.WriteLine($"phFour -= ptThree: {ptFour -= ptThree}");
 		}
 
 		static void TwoPoints() {
