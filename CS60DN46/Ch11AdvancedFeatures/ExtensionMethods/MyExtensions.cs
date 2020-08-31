4e03aa641159afc866f3503d34915dbb4f9c3fbe
Wei Ding <ding_web@yahoo.co.jp>
2020-08-30 22:42:47 -0400

    invoke extension

--- CS60DN46/Ch11AdvancedFeatures/ExtensionMethods/MyExtensions.cs
+++ CS60DN46/Ch11AdvancedFeatures/ExtensionMethods/MyExtensions.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 
 namespace ExtensionMethods {
-	class MyExtensions {
+	static class MyExtensions {
+		//display he assembly
+		public static void DisplayDefiningAssembly(this object obj) {
+			Console.WriteLine($"{obj.GetType().Name} lives here: => {Assembly.GetAssembly(obj.GetType()).GetName().Name}");
+		}
+
+		//Reverse digits of any integer
+		public static int ReverseDigits(this int i) {
+			char[] digits = i.ToString().ToCharArray();
+			Array.Reverse(digits);
+			return int.Parse(new string(digits));
+		}
 	}
 }
