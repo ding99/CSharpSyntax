a61ff5dcfd22b436739e5b84c17575f5ee8d4ea6
Wei Ding <ding_web@yahoo.co.jp>
2020-08-31 14:18:49 -0400

    unsafe scope

--- CS60DN46/Ch11AdvancedFeatures/UnsafeCode/Program.cs
+++ CS60DN46/Ch11AdvancedFeatures/UnsafeCode/Program.cs
@@ -4,8 +4,28 @@ namespace UnsafeCode {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Pointer Types *****");
-
+			UnsafeScope();
 			Console.ResetColor();
+		}
+
+		static void UnsafeScope() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Unsage scope");
+
+			unsafe {	//work with pointer types here
+				int myInt = 5;
+				SquareIntPointer(&myInt);
+				Console.WriteLine($"myInt: {myInt}");
+			}
+
+			//can't work with pointers here
+			int myInt2 = 10;
+			//SquareIntPointer(&myInt2);	//Compile error
+			Console.WriteLine($"myInt2: {myInt2}");
+		}
+
+		unsafe static void SquareIntPointer(int* myIntPointer) {
+			*myIntPointer *= *myIntPointer;
 		}
 	}
 }
