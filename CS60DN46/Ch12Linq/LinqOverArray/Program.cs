e58890bf2d306501fbfae7bbcca8e9bd2604f73d
Wei Ding <ding_web@yahoo.co.jp>
2020-09-01 00:28:07 -0400

    first project frame

---
+++ CS60DN46/Ch12Linq/LinqOverArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqOverArray {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Linq to Object *****");
+			QueryOverStrings();
+			Console.ResetColor();
+		}
+
+		static void QueryOverStrings() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Query over strings");
+			string[] games = { "Morrowwind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+		}
+	}
+}
