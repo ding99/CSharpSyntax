33b6ff21d4ce396e45a5edfef8fbab1a3539cfeb
Wei Ding <ding_web@yahoo.co.jp>
2020-09-01 16:47:33 -0400

    update display

--- CS60DN46/Ch12Linq/LinqOverArray/Program.cs
+++ CS60DN46/Ch12Linq/LinqOverArray/Program.cs
@@ -15,9 +15,15 @@ namespace LinqOverArray {
 			Console.WriteLine("=> Query over strings");
 			string[] games = { "Morrowwind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
 
+			Console.WriteLine($"-> original array (size {games.Length}):");
+			foreach (string s in games) Console.Write($" <{s}>");
+			Console.WriteLine();
+
 			//query expression to find the items having an embedded space
 			IEnumerable<string> subset = from g in games where g.Contains(" ") orderby g select g;
-			foreach (string s in subset) Console.WriteLine($"Item: {s}");
+			Console.WriteLine($"-> subset(containing an embedded space) (size {subset.Count()}):");
+			foreach (string s in subset) Console.Write($" <{s}>");
+			Console.WriteLine();
 
 			ReflectOverQueryResults(subset);
 		}
