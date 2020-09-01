bfc7447f3c679f26ddbfb1d4f68025ad7dcbce92
Wei Ding <ding_web@yahoo.co.jp>
2020-09-01 17:00:53 -0400

    query over integer

--- CS60DN46/Ch12Linq/LinqOverArray/Program.cs
+++ CS60DN46/Ch12Linq/LinqOverArray/Program.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace LinqOverArray {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Linq to Object *****");
 			QueryOverStrings();
+			QueryOverInts();
 			Console.ResetColor();
+		}
+
+		static void QueryOverInts() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Query over ints");
+			int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+
+			Console.Write($"-> original array (size {numbers.Length}):");
+			foreach (int a in numbers) Console.Write($" {a}");
+			Console.WriteLine();
+
+			IEnumerable<int> subset = from i in numbers where i < 10 select i;
+			Console.Write($"-> subset (size {subset.Count()}):");
+			foreach (int a in subset) Console.Write($" {a}");
+			Console.WriteLine();
 		}
 
 		static void QueryOverStrings() {
