0fdbc7f44bb01d126d4f62e72187279a9d020736
Wei Ding <ding_web@yahoo.co.jp>
2020-09-02 11:21:41 -0400

    fast bmw

--- CS60DN46/Ch12Linq/LinqOverCollections/Program.cs
+++ CS60DN46/Ch12Linq/LinqOverCollections/Program.cs
@@ -14,27 +14,34 @@ namespace LinqOverCollections {
 			Console.ForegroundColor = ConsoleColor.Cyan;
 			Console.WriteLine("=> Linq Over Generic Collections");
 			List<Car> cars = new List<Car> {
-				new Car{ Name = "Henry", Color = "Silver", Speed = 100, Make = "BWM" },
-				new Car{ Name = "Daisy", Color = "Tan", Speed = 90, Make = "BWM" },
+				new Car{ Name = "Henry", Color = "Silver", Speed = 100, Make = "BMW" },
+				new Car{ Name = "Daisy", Color = "Tan", Speed = 90, Make = "BMW" },
 				new Car{ Name = "Mary", Color = "Black", Speed = 55, Make = "VM" },
 				new Car{ Name = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo" },
 				new Car{ Name = "Melvin", Color = "White", Speed = 56, Make = "Ford" }
 			};
 
-			GetFastCars(cars);
-		}
-		static void GetFastCars(List<Car> cars) {
 			Console.Write($"-> Cars (size {cars.Count()}):");
 			foreach (var car in cars)
-				Console.Write($" <{car.Name}>");
+				Console.Write($" <{car.Name}/{car.Make}/{car.Speed}>");
 			Console.WriteLine();
 
+			GetFastCars(cars);
+			GetFastBMWs(cars);
+		}
+		static void GetFastCars(List<Car> cars) {
 			var fastCars = from c in cars where c.Speed > 55 select c;
-			Console.Write($"-> Fast Cars (size {fastCars.Count()}):");
+			Console.Write($"-> Fast (>55) Cars (size {fastCars.Count()}):");
 			foreach(var car in fastCars)
 				Console.Write($" <{car.Name}>");
 			Console.WriteLine();
 		}
-
+		static void GetFastBMWs(List<Car> cars) {
+			var fastCars = from c in cars where c.Speed > 90 && c.Make == "BMW" select c;
+			Console.Write($"-> Too Fast (>90.BMW) Cars (size {fastCars.Count()}):");
+			foreach (var car in fastCars)
+				Console.Write($" <{car.Name}>");
+			Console.WriteLine();
+		}
 	}
 }
