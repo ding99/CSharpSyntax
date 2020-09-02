c5fd3bd5a7af55bbb98d3a685012236207e0224c
Wei Ding <ding_web@yahoo.co.jp>
2020-09-01 23:54:46 -0400

    return values

---
+++ CS60DN46/Ch12Linq/LinqOverArray/LinqBasedFieldsAreClunky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqOverArray {
+	class LinqBasedFieldsAreClunky {
+		private static string[] games = { "Morrowwind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+		private IEnumerable<string> subset = from g in games where g.Contains(" ") orderby g select g;
+
+		public void PrintGames() {
+			foreach(var item in subset)
+				Console.WriteLine(item);
+		}
+	}
+}
