6b5007d22d9516231979fc0b5ca331e5e7da3fec
Wei Ding <ding_web@yahoo.co.jp>
2020-09-02 10:28:21 -0400

    fast cars

--- CS60DN46/Ch12Linq/LinqOverCollections/Program.cs
+++ CS60DN46/Ch12Linq/LinqOverCollections/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace LinqOverCollections {
 	class Program {
-		static void Main(string[] args) {
+		static void Main() {
 			Console.WriteLine("***** Linq Queries to Collection Objects *****");
 			GenericCollections();
 			Console.ResetColor();
@@ -17,8 +18,22 @@ namespace LinqOverCollections {
 				new Car{ Name = "Daisy", Color = "Tan", Speed = 90, Make = "BWM" },
 				new Car{ Name = "Mary", Color = "Black", Speed = 55, Make = "VM" },
 				new Car{ Name = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo" },
-				new Car{ Name = "Melvin", Color = "White", Speed = 54, Make = "Ford" }
+				new Car{ Name = "Melvin", Color = "White", Speed = 56, Make = "Ford" }
 			};
+
+			GetFastCars(cars);
+		}
+		static void GetFastCars(List<Car> cars) {
+			Console.Write($"-> Cars (size {cars.Count()}):");
+			foreach (var car in cars)
+				Console.Write($" <{car.Name}>");
+			Console.WriteLine();
+
+			var fastCars = from c in cars where c.Speed > 55 select c;
+			Console.Write($"-> Fast Cars (size {fastCars.Count()}):");
+			foreach(var car in fastCars)
+				Console.Write($" <{car.Name}>");
+			Console.WriteLine();
 		}
 
 	}
