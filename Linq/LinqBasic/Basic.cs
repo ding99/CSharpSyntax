112a36aa4b66c9e59635b7b5bfd88e8cbc8b8465
Wei Ding <ding_web@yahoo.co.jp>
2020-09-03 13:02:21 -0400

    linq basic

---
+++ Linq/LinqBasic/Basic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqBasic {
+	class Basic {
+		int[] scores;
+
+		public Basic() {
+			scores = new int[] { 97, 55, 61, 81, 92 };
+			Console.Write($"Original  (size {scores.Count()}):");
+			foreach (var a in scores) Console.Write($" <{a}>");
+			Console.WriteLine();
+		}
+
+		public void Start() {
+			Console.ForegroundColor = ConsoleColor.Yellow; Select();
+		}
+
+		public void Select() {
+			Console.WriteLine("- Select");
+			IEnumerable<int> excellence = from score in scores where score > 80 select score;
+			Console.Write($"Excellent (size {excellence.Count()}):");
+			foreach (var a in excellence) Console.Write($" <{a}>");
+			Console.WriteLine();
+
+			IEnumerable<string> excellence2 = from score in scores where score > 80 select $"The score is {score}";
+			Console.Write($"Excellent (size {excellence2.Count()}):");
+			foreach (var a in excellence2) Console.Write($" <{a}>");
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Linq/LinqBasic/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LinqBasic {
+	class Program {
+		static void Main() {
+			Console.WriteLine("== Linq Basic");
+			new Basic().Start();
+			Console.WriteLine("== End");
+		}
+	}
+}
