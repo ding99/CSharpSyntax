51ea114b7685d8b3aee5cf7e20ff8d437fa82739
Wei Ding <ding_web@yahoo.co.jp>
2020-09-04 02:01:35 -0400

    comment

--- CS60DN46/Ch13ObjectLifetime/SimpleGC/Program.cs
+++ CS60DN46/Ch13ObjectLifetime/SimpleGC/Program.cs
@@ -12,7 +12,10 @@ namespace SimpleGC {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> GC Basics");
 
+			//Create a Car object on the managed heap
+			//A reference to this object is returned ("refToMyCar")
 			Car refToMyCar = new Car("Zippy", 50);
+			//dot operator is use to invoke member on the object
 			Console.WriteLine(refToMyCar.ToString());
 		}
 	}
