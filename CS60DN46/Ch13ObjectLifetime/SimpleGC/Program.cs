bdac386b3a7f4973404ad088388df988cec6b702
Wei Ding <ding_web@yahoo.co.jp>
2020-09-04 01:55:15 -0400

    GC basics

---
+++ CS60DN46/Ch13ObjectLifetime/SimpleGC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleGC {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Classs, Objects and References *****");
+			GCBasics();
+			Console.ResetColor();
+		}
+
+		static void GCBasics() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> GC Basics");
+
+			Car refToMyCar = new Car("Zippy", 50);
+			Console.WriteLine(refToMyCar.ToString());
+		}
+	}
+}
