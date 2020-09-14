69fce378062d52d9af11c4fdc0cf9aeeafcdd51c
Wei Ding <ding_web@yahoo.co.jp>
2020-09-14 02:37:55 -0400

    use dispose

---
+++ CS60DN46/Ch13ObjectLifetime/SimpleDispose/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleDispose {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Dispose *****");
+			UseDispose();
+			Console.ResetColor();
+		}
+
+		static void UseDispose() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			MyResourceWrapper rw = new MyResourceWrapper();
+			rw.Dispose();
+		}
+	}
+}
--- CS60DN46/Ch13ObjectLifetime/SimpleFinalize/MyResourceWrapper.cs
+++ CS60DN46/Ch13ObjectLifetime/SimpleFinalize/MyResourceWrapper.cs
@@ -2,7 +2,6 @@
 
 namespace SimpleFinalize {
 	class MyResourceWrapper {
-		// Compile-time error!
 		~MyResourceWrapper() {
 			//clean up unmanaged resources here
 			Console.Beep();
