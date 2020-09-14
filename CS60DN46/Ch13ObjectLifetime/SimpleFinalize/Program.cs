befeaa9e9d0591e4554e84fdb9604d8fdd916780
Wei Ding <ding_web@yahoo.co.jp>
2020-09-14 01:27:50 -0400

    finalizer

--- CS60DN46/Ch13ObjectLifetime/SimpleFinalize/Program.cs
+++ CS60DN46/Ch13ObjectLifetime/SimpleFinalize/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleFinalize {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Simple Finalize *****");
-
+			Finalization();
 			Console.ResetColor();
+		}
+
+		static void Finalization() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> finalizers");
+
+			Console.WriteLine("Hit the return key to shut down this app");
+			Console.WriteLine("and force the GC to invoke Finalize()");
+			Console.WriteLine("for finalizable objects created in this AppDomain.");
+			Console.ReadLine();
+			MyResourceWrapper rw = new MyResourceWrapper();
 		}
 	}
 }
