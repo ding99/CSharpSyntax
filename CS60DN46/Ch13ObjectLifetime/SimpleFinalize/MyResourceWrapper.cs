7128560a3178d651eabda5ea96d7afab01074dd7
Wei Ding <ding_web@yahoo.co.jp>
2020-09-14 01:05:22 -0400

    object for finalize

---
+++ CS60DN46/Ch13ObjectLifetime/SimpleFinalize/MyResourceWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFinalize {
+	class MyResourceWrapper {
+		// Compile-time error!
+		~MyResourceWrapper() {
+			//clean up unmanaged resources here
+			Console.Beep();
+		}
+	}
+}
