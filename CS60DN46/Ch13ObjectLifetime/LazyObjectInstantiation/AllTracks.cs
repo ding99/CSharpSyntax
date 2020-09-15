21119f17c79a3f4ff8156f2166bc5fcfa96dcaea
Wei Ding <ding_web@yahoo.co.jp>
2020-09-14 21:46:55 -0400

    customized lazy

--- CS60DN46/Ch13ObjectLifetime/LazyObjectInstantiation/AllTracks.cs
+++ CS60DN46/Ch13ObjectLifetime/LazyObjectInstantiation/AllTracks.cs
@@ -7,12 +7,12 @@ namespace LazyObjectInstantiation {
 
 		public AllTracks() {
 			// Assume we fill up the array of Song objects here.
-			allSongs[0] = new Song { Artist = "A", Trackname = "Song 001", TrackLength = 10 };
-			allSongs[1] = new Song { Artist = "B", Trackname = "Song 002", TrackLength = 20 };
+			allSongs[0] = new Song { Artist = "A", Trackname = "Song1", TrackLength = 10 };
+			allSongs[1] = new Song { Artist = "B", Trackname = "Song2", TrackLength = 20 };
 			Console.WriteLine("Filling up the songs!");
 		}
 
-		public void Choose(int n) { Console.WriteLine($"[{allSongs[n].Artist} / {allSongs[n].Trackname}]"); }
+		public void Choose(int n) { Console.WriteLine($"[{allSongs[n].Artist} / {allSongs[n].Trackname} / {allSongs[n].TrackLength}]"); }
 
 		public int Size() { return allSongs.Length; }
 	}
