f09c2b83b13ad90ec84e25315f70f64a74d07818
Wei Ding <ding_web@yahoo.co.jp>
2020-09-16 11:37:08 -0400

    VB client

---
+++ CS60DN46/Ch14ClassLibraries/VBCarClient/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports CarLibrary
+
+Module Module1
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine("***** VB CarLibrary Client APP *****")
+        Dim van As New MiniVan()
+        van.TurboBoost()
+
+        Dim sports As New SportsCar()
+        sports.TurboBoost()
+    End Sub
+
+End Module
