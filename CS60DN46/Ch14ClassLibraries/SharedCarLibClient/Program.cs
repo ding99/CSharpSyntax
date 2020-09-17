ac7f083ed2b4f70c534a58e30fdbe0ce0f1f0fe8
Wei Ding <ding_web@yahoo.co.jp>
2020-09-17 13:59:12 -0400

    consume shared assembly

---
+++ CS60DN46/Ch14ClassLibraries/SharedCarLibClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace SharedCarLibClient {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** C# Shared CarLibrary Client App *****");
+			UseSharedAssembly();
+			Console.ResetColor();
+		}
+
+		static void UseSharedAssembly() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> use shared assembly");
+
+			SportsCar viper = new SportsCar("Shared", 150, 60);
+			viper.TurboBoost();
+
+			MiniVan van = new MiniVan();
+			van.TurboBoost();
+
+			Console.WriteLine("Done");
+		}
+	}
+}
