1b99b486007fe06d03403d00cc51bcb218df8bd6
Wei Ding <ding_web@yahoo.co.jp>
2020-09-18 01:32:39 -0400

    consume shared assembly setting CopyLocal as false

---
+++ CS60DN46/Ch14ClassLibraries/ConsumeSharedAssembly/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CarLibrary;
+
+namespace ConsumeSharedAssembly {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Consume a Shared Assembly *****");
+			Consuming();
+			Console.ResetColor();
+		}
+
+		//Notice: CarLibrary - Properties - CopyLocal: False
+		static void Consuming() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			SportsCar car = new SportsCar("Shared", 200, 100);
+			car.TurboBoost();
+
+			Console.WriteLine("Consumed");
+		}
+	}
+}
