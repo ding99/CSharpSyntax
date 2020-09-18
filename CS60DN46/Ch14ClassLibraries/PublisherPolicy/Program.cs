864696254da38438e3a788e3e405a3e98352c39c
Wei Ding <ding_web@yahoo.co.jp>
2020-09-17 23:42:22 -0400

    disable publisher policy

---
+++ CS60DN46/Ch14ClassLibraries/PublisherPolicy/Program.cs
@@ -0,0 +1,21 @@
+﻿using CarLibrary;
+using System;
+
+namespace PublisherPolicy {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Disabling Publisher Policy *****");
+			UseSharedAssembly();
+			Console.ResetColor();
+		}
+		static void UseSharedAssembly() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> use shared assembly");
+
+			SportsCar sport = new SportsCar("Shared", 160, 60);
+			sport.TurboBoost();
+
+			Console.WriteLine("- Done");
+		}
+	}
+}
