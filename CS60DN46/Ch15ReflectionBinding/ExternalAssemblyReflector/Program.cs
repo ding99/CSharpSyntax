34c28f0f7e0c4ecf60600dd840b345226baed344
Wei Ding <ding_web@yahoo.co.jp>
2020-09-25 10:24:17 -0400

    load assembly

---
+++ CS60DN46/Ch15ReflectionBinding/ExternalAssemblyReflector/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ExternalAssemblyReflector {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** External Assembly Viewer *****");
+			DisplayTypes();
+			Console.ResetColor();
+		}
+
+		static void DisplayTypes() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			string asmName = "";
+			do {
+				Console.Write("Enter an assembly to evaluate or Q to quit:");
+				asmName = Console.ReadLine();
+
+				if (asmName.ToUpper().Equals("Q")) break;
+
+				try {
+					DisplayTypesInAsm(Assembly.Load(asmName));
+				}
+				catch(Exception e) {
+					Console.WriteLine($"Sorry, can't find assembly. {e.Message}");
+				}
+			} while (true);
+		}
+
+		static void DisplayTypesInAsm(Assembly asm) {
+			Console.WriteLine($"-> Types in Assembly <{asm.FullName}>");
+			Type[] types = asm.GetTypes();
+			Console.Write($"Types:");
+			foreach (Type t in types)
+				Console.Write($" <{t}>");
+			Console.WriteLine();
+		}
+	}
+}
