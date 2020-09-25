9c233ba1371e7199ad5df3fef009abf5d718eb61
Wei Ding <ding_web@yahoo.co.jp>
2020-09-25 10:38:15 -0400

    loadfrom

--- CS60DN46/Ch15ReflectionBinding/ExternalAssemblyReflector/Program.cs
+++ CS60DN46/Ch15ReflectionBinding/ExternalAssemblyReflector/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace ExternalAssemblyReflector {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** External Assembly Viewer *****");
-			DisplayTypes();
+			ByLoad();
+			ByLoadFrom();
 			Console.ResetColor();
 		}
 
-		static void DisplayTypes() {
+		//test data: d:\workFolder\CarLibs\version2.0\CarLibrary.dll
+		static void ByLoadFrom() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Disply Types using Assembly.LoadFrom()");
+
+			string asmName = "";
+			do {
+				Console.Write("Enter an assembly to evaluate or Q to quit:");
+				asmName = Console.ReadLine();
+
+				if (asmName.ToUpper().Equals("Q")) break;
+
+				try {
+					DisplayTypesInAsm(Assembly.LoadFrom(asmName));
+				}
+				catch (Exception e) {
+					Console.WriteLine($"Sorry, can't find assembly. {e.Message}");
+				}
+			} while (true);
+		}
+
+		static void ByLoad() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Disply Types using Assembly.Load()");
 
 			string asmName = "";
 			do {
