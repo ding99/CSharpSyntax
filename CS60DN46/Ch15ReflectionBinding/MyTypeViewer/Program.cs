db0ddf5a64fdf580e8ac16415118f2588b314cf8
Wei Ding <ding_web@yahoo.co.jp>
2020-09-24 23:11:42 -0400

    list fields and properties

--- CS60DN46/Ch15ReflectionBinding/MyTypeViewer/Program.cs
+++ CS60DN46/Ch15ReflectionBinding/MyTypeViewer/Program.cs
@@ -6,15 +6,41 @@ namespace MyTypeViewer {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Custom Metadata Viewer *****");
+//			ListMethods();
 			Console.ResetColor();
 		}
 
 		static void ListMethods(Type t) {
 			Console.WriteLine("=> Methods");
+			
+			MethodInfo[] mi = t.GetMethods();
+			foreach(MethodInfo m in mi)
+				Console.Write($" {m.Name}");
+			Console.WriteLine();
+
 			var methodNames = from n in t.GetMethods() select n.Name;
 			foreach(var name in methodNames)
-				Console.WriteLine($"-> {name}");
+				Console.WriteLine($" {name}");
+			Console.WriteLine();
+		}
+
+		static void ListFields(Type t) {
+			Console.WriteLine("=> Fields");
+
+			var fieldNames = from n in t.GetFields() select n.Name;
+			foreach (var name in fieldNames)
+				Console.WriteLine($" {name}");
+			Console.WriteLine();
+		}
+
+		static void ListProps(Type t) {
+			Console.WriteLine("=> Properties");
+
+			var propNames = from n in t.GetFields() select n.Name;
+			foreach (var name in propNames)
+				Console.WriteLine($" {name}");
 			Console.WriteLine();
 		}
 	}
 }
+
