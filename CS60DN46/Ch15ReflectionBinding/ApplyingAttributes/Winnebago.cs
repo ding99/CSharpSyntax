1afa9f0eaa749e1eb4b3be66b7bc34ccdb0a5b54
Wei Ding <ding_web@yahoo.co.jp>
2020-10-02 00:44:10 -0400

    apply to method

--- CS60DN46/Ch15ReflectionBinding/ApplyingAttributes/Winnebago.cs
+++ CS60DN46/Ch15ReflectionBinding/ApplyingAttributes/Winnebago.cs
@@ -3,5 +3,10 @@
 namespace ApplyingAttributes {
 	[VehicleDescription("A very long, slow, but feature-rich auto")]
 	class Winnebago {
+
+		[VehicleDescription(Description = "Setup radio")]
+		public void PlayMusic(bool On) {
+			System.Console.WriteLine($"Radio {On}");
+		}
 	}
 }
