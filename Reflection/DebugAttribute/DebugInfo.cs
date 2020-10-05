c1b138b6b504ab1b787793355bbd4f726fa2832a
Wei Ding <ding_web@yahoo.co.jp>
2020-10-05 19:06:14 -0400

    auto property

--- Reflection/DebugAttribute/DebugInfo.cs
+++ Reflection/DebugAttribute/DebugInfo.cs
@@ -4,19 +4,35 @@ namespace DebugAttribute {
 
 	[AttributeUsage(AttributeTargets.Class|AttributeTargets.Constructor|AttributeTargets.Field|AttributeTargets.Method|AttributeTargets.Property,AllowMultiple =true)]
 	class DebugInfoAttribute : Attribute {
-		private int bugNo;
-		private string developer;
-		private string lastReview;
+		//private int bugNo;
+		//private string developer;
+		//private string lastReview;
 
 		public string message;
 
 		public DebugInfoAttribute(int bg, string dev, string review) {
-			bugNo = bg; developer = dev; lastReview = review;
+			BugNo = bg; Developer = dev; LastReview = review;
 		}
 
-		public int BugNo { get { return bugNo; } }
-		public string Developer { get { return developer; } }
-		public string LastReview { get { return lastReview; } }
+		public int BugNo { get; }
+		public string Developer { get; }
+		public string LastReview { get; }
 		public string Message { get { return message; } set { message = value; } }
 	}
+	//class DebugInfoAttribute : Attribute {
+	//	private int bugNo;
+	//	private string developer;
+	//	private string lastReview;
+
+	//	public string message;
+
+	//	public DebugInfoAttribute(int bg, string dev, string review) {
+	//		bugNo = bg; developer = dev; lastReview = review;
+	//	}
+
+	//	public int BugNo { get { return bugNo; } }
+	//	public string Developer { get { return developer; } }
+	//	public string LastReview { get { return lastReview; } }
+	//	public string Message { get { return message; } set { message = value; } }
+	//}
 }
--- Reflection/DebugAttribute/Rectangle.cs
+++ Reflection/DebugAttribute/Rectangle.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
 namespace DebugAttribute {
-	[DebugInfo(45,"Zara Ali","12/18/2012")]
-	[DebugInfo(49, "Nuha Ali", "10/10/2012")]
+	[DebugInfo(45,"Zara Ali","12/18/2012", Message = "Return tyep mismatch")]
+	[DebugInfo(49, "Nuha Ali", "10/10/2012", Message = "Unused variable")]
 	class Rectangle {
 		protected double length, width;
 		public Rectangle(double l, double w) { length = l; width = w; }
@@ -10,7 +10,7 @@ namespace DebugAttribute {
 		[DebugInfo(55,"Zara Ali","19/10/2013", Message = "Return type mismatch")]
 		public double GetArea() { return length * width; }
 
-		[DebugInfo(55, "Zara Ali", "19/10/2013", Message = "Unused variable")]
+		[DebugInfo(56, "Zara Ali", "17/10/2013")]
 		public void Display() {
 			Console.WriteLine($"Length: {length}, Width: {width}, Area: {GetArea()}");
 		}
