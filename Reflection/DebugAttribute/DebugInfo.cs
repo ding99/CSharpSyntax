9ec8095272584001375ed13fe9d52a916a2105f6
Wei Ding <ding_web@yahoo.co.jp>
2020-10-05 17:49:15 -0400

    add attribute

---
+++ Reflection/DebugAttribute/DebugInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugAttribute {
+	class DebugInfo : Attribute {
+		private int bugNo;
+		private string developer;
+		private string lastReview;
+
+		public string message;
+
+		public DebugInfo(int bg, string dev, string review) {
+			bugNo = bg; developer = dev; lastReview = review;
+		}
+	}
+}
