12cb5cb31462d519674667c938c5e06bcdaeaff4
Wei Ding <ding_web@yahoo.co.jp>
2020-10-05 17:30:37 -0400

    new project for debug info attribute

---
+++ Reflection/DebugAttribute/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DebugAttribute {
+	class Program {
+		static void Main() {
+			Console.WriteLine("== Use Attributes");
+		}
+	}
+}
