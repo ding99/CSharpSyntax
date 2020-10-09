878897f90d33d7181b289c90788655c0c7921f36
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 15:54:48 -0400

    arraylist

---
+++ Collection/ArrayListApp/ExamineArrayList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListApp {
+	class ExamineArrayList {
+		public void Start() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("-- Examining ArrayList (integer)");
+
+			ArrayList list = new ArrayList();
+			list.AddRange(new int[] {45,78,33,56,12,23,9});
+
+			Console.WriteLine($"Capacity: {list.Capacity}");
+			Console.WriteLine($"Count: {list.Count}");
+
+			Console.Write("Content:");
+			foreach (var i in list) Console.Write(" " + i);
+			Console.WriteLine();
+
+			list.Sort();
+			Console.Write("Sorted:");
+			foreach (var i in list) Console.Write(" " + i);
+			Console.WriteLine();
+
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("-- Examining ArrayList (string)");
+
+			ArrayList lista = new ArrayList();
+			lista.AddRange(new string[] { "Zhang","Wang","Li","Zhao","Liu" });
+
+			Console.WriteLine($"Capacity: {lista.Capacity}");
+			Console.WriteLine($"Count: {lista.Count}");
+
+			Console.Write("Content:");
+			foreach (var i in lista) Console.Write(" " + i);
+			Console.WriteLine();
+
+			lista.Sort();
+			Console.Write("Sorted:");
+			foreach (var i in lista) Console.Write(" " + i);
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Collection/ArrayListApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArrayListApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("== Start");
+
+			new ExamineArrayList().Start();
+			Console.ResetColor();
+
+			Console.WriteLine("== End");
+		}
+	}
+}
