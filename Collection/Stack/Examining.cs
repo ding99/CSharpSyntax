64c19ea55a76d5d90f7e08541bc673adff75f503
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 19:17:02 -0400

    start stack

---
+++ Collection/Stack/Examining.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack {
+	class Examining {
+		public void Start() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("-- Examine Stack");
+		}
+	}
+}
---
+++ Collection/Stack/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stack {
+	class Program {
+		static void Main() {
+			Console.WriteLine("== Start");
+			Examining exam = new Examining();
+			exam.Start();
+			Console.ResetColor();
+			Console.WriteLine("== End");
+		}
+	}
+}
