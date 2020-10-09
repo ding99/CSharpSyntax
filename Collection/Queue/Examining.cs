8538939b62a9616696dd5f860b394d74107e5c05
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 19:52:18 -0400

    enqueue and dequeue

--- Collection/Queue/Examining.cs
+++ Collection/Queue/Examining.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Queue {
 	class Examining {
@@ -10,7 +7,27 @@ namespace Queue {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("-- Examine Queue");
 
+			Queue<char> q = new Queue<char>();
 
+			q.Enqueue('A'); q.Enqueue('M'); q.Enqueue('G'); q.Enqueue('M');
+			Console.Write("Current Stack:");
+			foreach (var c in q) Console.Write(" " + c);
+			Console.WriteLine();
+
+			q.Enqueue('V'); q.Enqueue('H');
+			Console.Write("Current Stack:");
+			foreach (var c in q) Console.Write(" " + c);
+			Console.WriteLine();
+
+			Console.WriteLine("Removing some values");
+			char ch = (char)q.Dequeue();
+			Console.WriteLine($"The removed value {ch}");
+			ch = (char)q.Dequeue();
+			Console.WriteLine($"The removed value {ch}");
+
+			Console.Write("Current Stack:");
+			foreach (var c in q) Console.Write(" " + c);
+			Console.WriteLine();
 		}
 	}
 }
