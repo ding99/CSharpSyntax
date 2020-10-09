b6eebe19f53937def81fae3965b2fa4ca8fe6dbb
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 17:16:22 -0400

    start sortedlist

---
+++ Collection/SortedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SortedList {
+	class Program {
+		static void Main() {
+			Console.WriteLine("== Start");
+			ExamineSortedList exam = new ExamineSortedList();
+			exam.Start();
+			Console.ResetColor();
+			Console.WriteLine("== End");
+		}
+	}
+}
