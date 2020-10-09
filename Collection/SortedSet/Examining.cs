3a176aba5977ab6905f7a9a8a3eca0ca23a4a789
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 17:38:42 -0400

    integer sortedset

--- Collection/SortedSet/Examining.cs
+++ Collection/SortedSet/Examining.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortedSet {
 	class Examining {
 		public void Start() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("-- Examine SortedSet");
+			Console.WriteLine("-- Examine SortedSet with integer");
+
+			SortedSet<int> set1 = new SortedSet<int>();
+			set1.Add(101);
+			set1.Add(10001);
+			set1.Add(1001);
+			set1.Add(100001);
+			Console.Write("set1 Elements:");
+			foreach (var v in set1) Console.Write(" " + v);
+			Console.WriteLine();
+
+			SortedSet<int> set2 = new SortedSet<int> { 2002, 200002, 20002, 202 };
+			Console.Write("set2 Elements:");
+			foreach (var v in set2) Console.Write(" " + v);
+			Console.WriteLine();
+
+			if (set1.Contains(1001)) Console.WriteLine("set1 contains 1001");
+			if (!set2.Contains(1001)) Console.WriteLine("set2 does not contain 1001");
 		}
 	}
 }
