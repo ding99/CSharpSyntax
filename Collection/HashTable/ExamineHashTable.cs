c55a72e78af269b168d7a26e54e6fee2bf88ec53
Wei Ding <ding_web@yahoo.co.jp>
2020-10-09 17:10:00 -0400

    hash table with integer keys, rename arraylist project

--- Collection/HashTable/ExamineHashTable.cs
+++ Collection/HashTable/ExamineHashTable.cs
@@ -23,8 +23,37 @@ namespace HashTable {
 			if (t1.ContainsValue(name)) Console.WriteLine($"{name} is existing!");
 			else t1.Add("006", name);
 
-			ICollection keys = t1.Keys;
-			foreach (var key in keys) Console.Write(" " + key);
+			ICollection vs = t1.Keys;
+			Console.Write("Keys:");
+			foreach (var v in vs) Console.Write(" " + v);
+			Console.WriteLine();
+
+			vs = t1.Values;
+			Console.Write("Values:");
+			foreach (var v in vs) Console.Write(" <" + v + ">");
+			Console.WriteLine();
+
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("-- HashTable with integer key");
+
+			Hashtable t2 = new Hashtable();
+			t2.Add(1, "Zara Ali");
+			t2.Add(2, "Abida Rehman");
+			t2.Add(3, "Joe Holzner");
+			t2.Add(4, "Mausam Benazir Nur");
+			t2.Add(5, "M. Arif");
+
+			if (t2.ContainsValue(name)) Console.WriteLine($"{name} is existing!");
+			else t2.Add(6, name);
+
+			vs = t2.Keys;
+			Console.Write("Keys:");
+			foreach (var v in vs) Console.Write(" " + v);
+			Console.WriteLine();
+
+			vs = t2.Values;
+			Console.Write("Values:");
+			foreach (var v in vs) Console.Write(" <" + v + ">");
 			Console.WriteLine();
 		}
 	}
