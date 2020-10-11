f680ec720d4348f81df332a636c24de81ab140d1
Wei Ding <ding_web@yahoo.co.jp>
2020-10-10 21:25:37 -0400

    class attributes and method attributes

--- CS60DN46/Ch15ReflectionBinding/ApplyingAttributes/Program.cs
+++ CS60DN46/Ch15ReflectionBinding/ApplyingAttributes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AttributedCarLibrary;
+using System;
 
 namespace ApplyingAttributes {
 	class Program {
@@ -12,9 +13,39 @@ namespace ApplyingAttributes {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> Apply Attributes to classes");
 
-			Motorcycle motor = new Motorcycle();
+			Console.WriteLine($"-> class attributes");
 
+			Motorcycle motor = new Motorcycle();
+			Type type = typeof(Motorcycle);
+			foreach(var attr in type.GetCustomAttributes(true)) {
+				VehicleDescriptionAttribute v = attr as VehicleDescriptionAttribute;
+				if (v != null)
+					Console.WriteLine($"Class <{type.Name}>, Description <{v.Description}>");
+			}
+			
 			HorseAndBuggy mule = new HorseAndBuggy();
+			type = typeof(HorseAndBuggy);
+			foreach (var attr in type.GetCustomAttributes(true)) {
+				VehicleDescriptionAttribute v = attr as VehicleDescriptionAttribute;
+				if (v != null)
+					Console.WriteLine($"Class <{type.Name}>, Description <{v.Description}>");
+			}
+
+			Winnebago wb = new Winnebago();
+			type = typeof(Winnebago);
+			foreach (var attr in type.GetCustomAttributes(true)) {
+				VehicleDescriptionAttribute v = attr as VehicleDescriptionAttribute;
+				if (v != null)
+					Console.WriteLine($"Class <{type.Name}>, Description <{v.Description}>");
+			}
+
+			Console.WriteLine($"-> method attributes");
+			foreach (var m in type.GetMethods())
+				foreach(var a in m.GetCustomAttributes(true)) {
+				VehicleDescriptionAttribute v = a as VehicleDescriptionAttribute;
+				if (v != null)
+					Console.WriteLine($"Class <{type.Name}>, Method <{m.Name}>, Description <{v.Description}>");
+				}
 		}
 	}
 }
