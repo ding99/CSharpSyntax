cd1d6c01efe65a56f5df01751ac514238ccbbb9e
Wei Ding <ding_web@yahoo.co.jp>
2020-10-11 04:22:41 -0400

    late binding

---
+++ CS60DN46/Ch15ReflectionBinding/VehicleDescriptionAttributeReaderLateBinding/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+namespace VehicleDescriptionAttributeReaderLateBinding {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Value of Vehicle Description Attribute *****");
+			ReflectAttributesUsingLateBinding();
+			Console.ResetColor();
+		}
+
+		private static void ReflectAttributesUsingLateBinding() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Reflect Attributes Using Late Binding");
+
+			try {
+				Assembly asm = Assembly.Load("AttributedCarLibrary");
+				Type vehicleDesc = asm.GetType("AttributedCarLibrary.VehicleDescriptionAttribute");
+
+				PropertyInfo propDesc = vehicleDesc.GetProperty("Description");
+
+				Type[] types = asm.GetTypes();
+
+				foreach(Type t in types) {
+					object[] objs = t.GetCustomAttributes(vehicleDesc, false);
+					foreach(object o in objs)
+						Console.WriteLine($"-> {t.Name}: {propDesc.GetValue(o, null)})");
+				}
+			}
+			catch(Exception e) {
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
+}
