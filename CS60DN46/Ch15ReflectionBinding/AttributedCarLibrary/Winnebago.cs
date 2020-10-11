fbb4f0d2a16566d4257d25f9a18dc86a3107a62c
Wei Ding <ding_web@yahoo.co.jp>
2020-10-10 23:45:09 -0400

    early binding

---
+++ CS60DN46/Ch15ReflectionBinding/AttributedCarLibrary/Winnebago.cs
@@ -0,0 +1,10 @@
+﻿namespace AttributedCarLibrary {
+	[VehicleDescription("A very long, slow, but feature-rich auto")]
+	public class Winnebago {
+
+		[VehicleDescription("My rocking CD player")]
+		public void PlayMusic(bool On) {
+			System.Console.WriteLine($"Radio {On}");
+		}
+	}
+}
--- CS60DN46/Ch15ReflectionBinding/VehicleDescriptionAttributeReader/Program.cs
+++ CS60DN46/Ch15ReflectionBinding/VehicleDescriptionAttributeReader/Program.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AttributedCarLibrary;
+using System;
 
 namespace VehicleDescriptionAttributeReader {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Value of Vehicle Description Attribute *****");
-
-
-
+			ReflectOnAttributesUsingEarlyBinding();
 			Console.ResetColor();
+		}
+
+		private static void ReflectOnAttributesUsingEarlyBinding() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Reflect on Attributes Using Early Binding");
+
+			Type t = typeof(Winnebago);
+			object[] attrs = t.GetCustomAttributes(false);
+			 foreach(VehicleDescriptionAttribute v in attrs)
+				Console.WriteLine($"-> {v.Description}");
 		}
 	}
 }
