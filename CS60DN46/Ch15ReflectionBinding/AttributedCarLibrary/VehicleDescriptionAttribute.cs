eca729405add6bc94f5afb73edd261a26ba60a36
Wei Ding <ding_web@yahoo.co.jp>
2020-10-10 21:08:07 -0400

    attribute usage

--- CS60DN46/Ch15ReflectionBinding/AttributedCarLibrary/VehicleDescriptionAttribute.cs
+++ CS60DN46/Ch15ReflectionBinding/AttributedCarLibrary/VehicleDescriptionAttribute.cs
@@ -1,4 +1,7 @@
-﻿namespace AttributedCarLibrary {
+﻿using System;
+
+namespace AttributedCarLibrary {
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Struct|AttributeTargets.Method,Inherited =false)]
 	public sealed class VehicleDescriptionAttribute : System.Attribute
     {
         public string Description { get; set; }
