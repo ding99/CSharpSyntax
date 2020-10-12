5bcc1a12089806867114dfda5c1317a7b994bd5a
Wei Ding <ding_web@yahoo.co.jp>
2020-10-11 23:30:58 -0400

    dynamic keyword

---
+++ CS60DN46/Ch16Dynamic/DynamicKeyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Dynamic Keyword *****");
+			ImplicitlyTypeVariable();
+			Console.ResetColor();
+		}
+
+		private static void ImplicitlyTypeVariable() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Implicitly Type Variable");
+
+			var a = new List<int>();
+			a.Add(90);
+			//This would be a compile-time error!
+			//a = "Hello";
+		}
+	}
+}
