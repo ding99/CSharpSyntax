a85b16c094b1a080da503251c1f39e40c76c3fa4
Wei Ding <ding_web@yahoo.co.jp>
2020-10-12 23:47:54 -0400

    console project for late binding

---
+++ CS60DN46/Ch16Dynamic/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateBindingWithDynamic {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Late Binding with Dynamic *****");
+
+
+			Console.ResetColor();
+		}
+	}
+}
