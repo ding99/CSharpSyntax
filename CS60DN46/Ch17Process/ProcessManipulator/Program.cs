888390c4ec959700982f9cbf9d72d18591c9227c
Wei Ding <ding_web@yahoo.co.jp>
2020-10-14 03:16:54 -0400

    start chapter 17

---
+++ CS60DN46/Ch17Process/ProcessManipulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManipulator {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Processes *****");
+			ListAllRunningProcesses();
+
+			Console.ResetColor();
+		}
+
+		private static void ListAllRunningProcesses() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> List All Running Processes");
+		}
+	}
+}
