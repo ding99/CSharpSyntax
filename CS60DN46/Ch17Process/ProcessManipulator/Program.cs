83bbc7c65fad88988dd0c9f573343787f8141ea3
Wei Ding <ding_web@yahoo.co.jp>
2020-10-14 19:10:21 -0400

    specific process

--- CS60DN46/Ch17Process/ProcessManipulator/Program.cs
+++ CS60DN46/Ch17Process/ProcessManipulator/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ProcessManipulator {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Processes *****");
 			ListAllRunningProcesses();
-
+			SpecificProcess();
 			Console.ResetColor();
 		}
 
@@ -21,6 +22,30 @@ namespace ProcessManipulator {
 			foreach(var p in runningProcs)
 				Console.Write($" [{p.Id}:{p.ProcessName}]");
 			Console.WriteLine();
+		}
+
+		private static void SpecificProcess() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Specific Process");
+
+			IEnumerable<Process> runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
+			Process theProc = null;
+
+			try {
+				Console.WriteLine($"{runningProcs.Count()} processes");
+				int n = 2;
+				if (runningProcs.Count() > n) {
+					int pid = runningProcs.ToList()[n].Id;
+					theProc = Process.GetProcessById(pid);
+					Console.WriteLine($"{theProc.Id} / {theProc.ProcessName}");
+				}
+			}
+			catch(ArgumentException e) {
+				Console.WriteLine(e.Message);
+			}
+			catch(Exception e) {
+				Console.WriteLine(e.Message);
+			}
 		}
 	}
 }
