d78c98a5b7f0085ee8908b28c4926b7ddbd45ca4
Wei Ding <ding_web@yahoo.co.jp>
2020-10-13 23:13:32 -0400

    add new car dialog

--- CS60DN46/Ch16Dynamic/ExportDataToOfficeApp/ExportData.cs
+++ CS60DN46/Ch16Dynamic/ExportDataToOfficeApp/ExportData.cs
@@ -10,8 +10,35 @@ using System.Windows.Forms;
 
 namespace ExportDataToOfficeApp {
 	public partial class ExportData : Form {
+		List<Car> carsInStock = null;
+
 		public ExportData() {
 			InitializeComponent();
+		}
+
+		private void ExportData_Load(object sender, EventArgs e) {
+			carsInStock = new List<Car> {
+				new Car { Color = "Green", Make = "VW", Name = "Mary" },
+				new Car { Color = "Red", Make = "Saab", Name = "Mel" },
+				new Car { Color = "Black", Make = "Ford", Name = "Hank" },
+				new Car { Color = "Yellow", Make = "BMW", Name = "Davie" }
+			};
+			UpdateGrid();
+		}
+
+		private void UpdateGrid() {
+			//reset the source of data
+			dataGridCars.DataSource = null;
+			dataGridCars.DataSource = carsInStock;
+		}
+
+		private void btnAddNewCar_Click(object sender, EventArgs e) {
+			NewCarDialog d = new NewCarDialog();
+			if(d.ShowDialog() == DialogResult.OK) {
+				carsInStock.Add(d.theCar);
+				UpdateGrid();
+
+			}
 		}
 	}
 }
