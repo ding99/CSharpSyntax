ac0ae09fa9d5ed14faaad2f2ad2b556896c8861b
Wei Ding <ding_web@yahoo.co.jp>
2020-10-14 20:19:26 -0400

    seperate a method for thread set investigation

--- CS60DN46/Ch17Process/ProcessManipulator/Program.cs
+++ CS60DN46/Ch17Process/ProcessManipulator/Program.cs
@@ -9,19 +9,49 @@ namespace ProcessManipulator {
 			Console.WriteLine("***** Processes *****");
 			ListAllRunningProcesses();
 			SpecificProcess();
+			ThreadSet();
 			Console.ResetColor();
 		}
 
-		private static void ListAllRunningProcesses() {
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("=> List All Running Processes");
+		private static void ThreadSet() {
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine("=> Investigate a Process's Thread Set");
 
-			//get all the processes on the local machine, ordered by PID
 			var runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
-			Console.WriteLine($"-> {runningProcs.Count()} running processes totally");
-			foreach(var p in runningProcs)
-				Console.Write($" [{p.Id}:{p.ProcessName}]");
-			Console.WriteLine();
+
+			try {
+				List<Process> chromes = (from p in runningProcs where p.ProcessName.ToLower().Equals("chrome") select p).ToList();
+
+				Console.WriteLine($"{chromes.Count} chrome processes totally");
+				if (chromes.Count > 0)
+					EnumThreadsForPid(chromes[0].Id);
+			}
+			catch (ArgumentException e) {
+				Console.WriteLine(e.Message);
+			}
+			catch (Exception e) {
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		private static void EnumThreadsForPid(int pID) {
+			Process theProc = null;
+			try { theProc = Process.GetProcessById(pID); }
+			catch (ArgumentException e) { Console.WriteLine(e.Message); return; }
+
+			Console.WriteLine($"-> Here are the threads used by {theProc.ProcessName} with pID {pID}");
+			ProcessThreadCollection theThds = theProc.Threads;
+
+			foreach (ProcessThread t in theThds) {
+				string info = $"Thread ID {t.Id},{Spaces(t.Id)}\tTime {t.StartTime.ToShortTimeString()},\tState {t.ThreadState},\tPriority {t.PriorityLevel}";
+				Console.WriteLine(info);
+			}
+		}
+		private static string Spaces(int id) {
+			string s = string.Empty;
+			for (int i = id.ToString().Length; i < 5; i++)
+				s += " ";
+			return s;
 		}
 
 		private static void SpecificProcess() {
@@ -41,42 +71,25 @@ namespace ProcessManipulator {
 					theProc = Process.GetProcessById(pid);
 					Console.WriteLine($"{theProc.Id} / {theProc.ProcessName}");
 				}
-
-				List<Process> chromes = (from p in runningProcs where p.ProcessName.ToLower().Equals("chrome") select p).ToList();
-
-				Console.WriteLine($"{chromes.Count} chrome processes totally");
-				if (chromes.Count > 0)
-					EnumThreadsForPid(chromes[0].Id);
 			}
-			catch(ArgumentException e) {
+			catch (ArgumentException e) {
 				Console.WriteLine(e.Message);
 			}
-			catch(Exception e) {
+			catch (Exception e) {
 				Console.WriteLine(e.Message);
 			}
 		}
 
-		private static void EnumThreadsForPid(int pID) {
-			Console.ForegroundColor = ConsoleColor.DarkYellow;
+		private static void ListAllRunningProcesses() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> List All Running Processes");
 
-			Process theProc = null;
-			try { theProc = Process.GetProcessById(pID); }
-			catch (ArgumentException e)
-			{ Console.WriteLine(e.Message); return; }
-
-			Console.WriteLine($"-> Here are the threads used by {theProc.ProcessName} with pID {pID}");
-			ProcessThreadCollection theThds = theProc.Threads;
-
-			foreach(ProcessThread t in theThds) {
-				string info = $"Thread ID {t.Id},{Spaces(t.Id)}\tTime {t.StartTime.ToShortTimeString()},\tState {t.ThreadState},\tPriority {t.PriorityLevel}";
-				Console.WriteLine(info);
-			}
-		}
-		private static string Spaces(int id) {
-			string s = string.Empty;
-			for (int i = id.ToString().Length; i < 5; i++)
-				s += " ";
-			return s;
+			//get all the processes on the local machine, ordered by PID
+			var runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
+			Console.WriteLine($"-> {runningProcs.Count()} running processes totally");
+			foreach(var p in runningProcs)
+				Console.Write($" [{p.Id}:{p.ProcessName}]");
+			Console.WriteLine();
 		}
 	}
 }
