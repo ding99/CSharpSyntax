1226b962f1745ea24eb3d51f075da450194c6514
Wei Ding <ding_web@yahoo.co.jp>
2020-10-14 23:42:16 -0400

    default app domain

--- CS60DN46/Ch17Process/DefaultAppDomainApp/Program.cs
+++ CS60DN46/Ch17Process/DefaultAppDomainApp/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DefaultAppDomainApp {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Default AppDomain *****");
-
+			DisplayDADStats();
 
 			Console.ResetColor();
+		}
+
+		private static void DisplayDADStats() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Display DAD Stats");
+
+			AppDomain defaultAD = AppDomain.CurrentDomain;
+
+			Console.WriteLine($"Name of this domain: <{defaultAD.FriendlyName}>");
+			Console.WriteLine($"ID of domain in this process: <{defaultAD.Id}>");
+			Console.WriteLine($"Is this the default domain: <{defaultAD.IsDefaultAppDomain()}>");
+			Console.WriteLine($"Basic directory of this domain: <{defaultAD.BaseDirectory}>");
 		}
 	}
 }
