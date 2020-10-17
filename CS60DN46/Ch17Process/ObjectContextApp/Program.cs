25fd0bf30353192c161aaf137e6dbeceb8e440fa
Wei Ding <ding_web@yahoo.co.jp>
2020-10-17 00:43:10 -0400

    object contest project start

---
+++ CS60DN46/Ch17Process/ObjectContextApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectContextApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Inspecting an Object's Context *****");
+			Console.ResetColor();
+		}
+
+		private static void ObjectContext() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Object Context");
+		}
+	}
+}
