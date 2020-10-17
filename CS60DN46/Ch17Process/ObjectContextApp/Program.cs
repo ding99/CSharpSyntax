82bc4eab81d21a77b5ce7dda9959f74d310aed5a
Wei Ding <ding_web@yahoo.co.jp>
2020-10-17 00:57:20 -0400

    finish object context

--- CS60DN46/Ch17Process/ObjectContextApp/Program.cs
+++ CS60DN46/Ch17Process/ObjectContextApp/Program.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ObjectContextApp {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Inspecting an Object's Context *****");
+			ObjectContext();
 			Console.ResetColor();
 		}
 
 		private static void ObjectContext() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> Object Context");
+
+			SportsCar car1 = new SportsCar();
+			Console.WriteLine();
+
+			SportsCar car2 = new SportsCar();
+			Console.WriteLine();
+
+			SportsCarTS syncCar = new SportsCarTS();
+			Console.WriteLine();
 		}
 	}
 }
--- CS60DN46/Ch17Process/ObjectContextApp/SportsCarTS.cs
+++ CS60DN46/Ch17Process/ObjectContextApp/SportsCarTS.cs
@@ -3,12 +3,13 @@ using System.Runtime.Remoting.Contexts;
 using System.Threading;
 
 namespace ObjectContextApp {
+
 	[Synchronization]
-	class SportsCarTS : SportsCar {
+	class SportsCarTS : ContextBoundObject {
 		public SportsCarTS() {
 			Context ctx = Thread.CurrentContext;
 			Console.WriteLine($"{this.ToString()} object in context {ctx.ContextID}");
-			foreach(IContextProperty p in ctx.ContextProperties)
+			foreach (IContextProperty p in ctx.ContextProperties)
 				Console.WriteLine($"   Ctx Prop: {p.Name}");
 		}
 	}
