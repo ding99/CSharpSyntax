3c68728b24185a2ea8e2455b2c21d61dfa745b2e
Wei Ding <ding_web@yahoo.co.jp>
2020-10-18 22:30:01 -0400

    hello cil

---
+++ CS60DN46/Ch18CIL/RoundTrip/HelloProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundTrip {
+	class Program {
+		static void Main() {
+			Console.WriteLine("Hello CIL code!");
+			Console.ReadLine();
+		}
+	}
+}
