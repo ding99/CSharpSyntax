564587fa25ba8131b53e69d602c5d7a866d1b644
Wei Ding <ding_web@yahoo.co.jp>
2020-10-21 15:46:14 -0400

    start an assembly builder project

---
+++ CS60DN46/Ch18CIL/DynamicAsmBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace DynamicAsmBuilder {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Dynamic Assembly Builder *****");
+			AppDomain domain = Thread.GetDomain();
+			CreateMyAsm(domain);
+			Console.ResetColor();
+		}
+
+		private static void CreateMyAsm(AppDomain curAppDomain) {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Create an assembly");
+		}
+	}
+}
