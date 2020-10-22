fad8e6586576cfaf08a485062e9bfd63e210cbf3
Wei Ding <ding_web@yahoo.co.jp>
2020-10-22 18:48:52 -0400

    background thread

---
+++ CS60DN46/Ch19Threads/BackgroundThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace BackgroundThread {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Foreground Thread and Background Thread *****");
+
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("=> Background(Daemon) Thread");
+			Thread.CurrentThread.Name = "MainThread";
+
+			Printer p = new Printer();
+			Thread bg = new Thread(new ThreadStart(p.PrintNumbers));
+			bg.Name = "SecondaryThread";
+			
+			//each number will be printed if comment out the line
+			bg.IsBackground = true;
+			
+			bg.Start();
+
+			Thread.Sleep(1500);
+			Console.ResetColor();
+		}
+	}
+}
