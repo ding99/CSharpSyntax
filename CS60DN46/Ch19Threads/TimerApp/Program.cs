c6f1d0a5cb05ca232379f1249d3aa1eea62b46c6
Wei Ding <ding_web@yahoo.co.jp>
2020-10-23 17:38:44 -0400

    switch timer

--- CS60DN46/Ch19Threads/TimerApp/Program.cs
+++ CS60DN46/Ch19Threads/TimerApp/Program.cs
@@ -11,7 +11,12 @@ namespace TimerApp {
 			TimerCallback timeCB = new TimerCallback(PrintTime);
 
 			Timer t = new Timer(timeCB, null, 0, 1000);
+			Console.WriteLine("Hit key to switch to another timer");
+			Console.ReadLine();
 
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			t.Dispose();
+			t = new Timer(timeCB, "Second Timer", 0, 1000);
 			Console.WriteLine("Hit key to terminate...");
 			Console.ReadLine();
 
@@ -19,7 +24,10 @@ namespace TimerApp {
 		}
 
 		static void PrintTime(object state) {
-			Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()}");
+			if(state == null)
+				Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()}");
+			else
+				Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()}, Param is: <{state.ToString()}>");
 		}
 	}
 }
