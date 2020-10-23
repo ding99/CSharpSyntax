6eca693e18f4ab4ffd67d7e1188ab451e50bca05
Wei Ding <ding_web@yahoo.co.jp>
2020-10-23 17:33:23 -0400

    start timer

---
+++ CS60DN46/Ch19Threads/TimerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Working with Timer Type *****");
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			//create the delegate for the timer type
+			TimerCallback timeCB = new TimerCallback(PrintTime);
+
+			Timer t = new Timer(timeCB, null, 0, 1000);
+
+			Console.WriteLine("Hit key to terminate...");
+			Console.ReadLine();
+
+			Console.ResetColor();
+		}
+
+		static void PrintTime(object state) {
+			Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()}");
+		}
+	}
+}
