600b8fd8359a4347a9057524cb8bc53c2da2af06
Wei Ding <ding_web@yahoo.co.jp>
2020-10-26 01:21:18 -0400

    start a winform project for plinq

---
+++ CS60DN46/Ch19Threads/PLINQDataProcessingWithCancellation/MainForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace PLINQDataProcessingWithCancellation {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.btnExecute = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnExecute
+			// 
+			this.btnExecute.Location = new System.Drawing.Point(28, 31);
+			this.btnExecute.Name = "btnExecute";
+			this.btnExecute.Size = new System.Drawing.Size(221, 35);
+			this.btnExecute.TabIndex = 0;
+			this.btnExecute.Text = "Execute";
+			this.btnExecute.UseVisualStyleBackColor = true;
+			this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(28, 100);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(221, 35);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(280, 240);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnExecute);
+			this.Name = "MainForm";
+			this.Text = "PLINQ Data Processing";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnExecute;
+		private System.Windows.Forms.Button btnCancel;
+	}
+}
+
---
+++ CS60DN46/Ch19Threads/PLINQDataProcessingWithCancellation/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PLINQDataProcessingWithCancellation {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void btnExecute_Click(object sender, EventArgs e) {
+			Task.Factory.StartNew(() => { ProcessIntData(); });
+		}
+
+		private void ProcessIntData() {
+			int[] source = Enumerable.Range(1, 10000000).ToArray();
+
+			//Find the numbers where num%3 == 1 is true, returned in descending order.
+			int[] modThreaIsZero = (from num in source where num % 3 == 0 orderby num descending select num).ToArray();
+
+			MessageBox.Show($"Found {modThreaIsZero.Count()} numbers that match query!");
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e) {
+
+		}
+	}
+}
