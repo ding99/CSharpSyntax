071a0202d58d73ba947d075ab804f5ac0234e122
Wei Ding <ding_web@yahoo.co.jp>
2020-10-27 00:37:38 -0400

    start dirinfo

---
+++ CS60DN46/Ch20IO/DirectoryApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectoryApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Fun with Directory(Info) *****");
+			ShowWindowsDirectoryInfo();
+			Console.ResetColor();
+		}
+
+		private static void ShowWindowsDirectoryInfo() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			string path = @"E:\workFolder\cs60\ch20\mouse";
+			Console.WriteLine($"=> Info of directory <{path}>");
+		}
+	}
+}
