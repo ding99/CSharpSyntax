69b51c6804ad29359d0342f6b5e623daaf743d79
Wei Ding <ding_web@yahoo.co.jp>
2020-10-27 00:49:29 -0400

    dir info

--- CS60DN46/Ch20IO/DirectoryApp/Program.cs
+++ CS60DN46/Ch20IO/DirectoryApp/Program.cs
@@ -16,7 +16,15 @@ namespace DirectoryApp {
 		private static void ShowWindowsDirectoryInfo() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			string path = @"E:\workFolder\cs60\ch20\mouse";
-			Console.WriteLine($"=> Info of directory <{path}>");
+			Console.WriteLine($"=> Directory Info : <{path}>");
+
+			DirectoryInfo dir = new DirectoryInfo(path);
+			Console.WriteLine($"FullName  : {dir.FullName}");
+			Console.WriteLine($"Name      : {dir.Name}");
+			Console.WriteLine($"Parent    : {dir.Parent}");
+			Console.WriteLine($"Root      : {dir.Root}");
+			Console.WriteLine($"Creation  : {dir.CreationTime}");
+			Console.WriteLine($"Attributes: {dir.Attributes}");
 		}
 	}
 }
