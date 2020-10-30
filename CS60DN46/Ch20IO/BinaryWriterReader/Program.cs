9cafe34d1e7938f78b4cf10964c383158e3fe348
Wei Ding <ding_web@yahoo.co.jp>
2020-10-30 18:49:38 -0400

    binarywriter

--- CS60DN46/Ch20IO/BinaryWriterReader/Program.cs
+++ CS60DN46/Ch20IO/BinaryWriterReader/Program.cs
@@ -9,13 +9,31 @@ namespace BinaryWriterReader {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** BinaryWriter / BinaryReader *****");
-			WriteRead();
+			Writing();
 			Console.ResetColor();
 		}
 
-		private static void WriteRead() {
+		private static void Writing() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("=>");
+			string path = @"E:\workFolder\cs60\ch20\files\binfile.dat";
+
+			Console.WriteLine($"=> BinaryWrite ({path})");
+
+			FileInfo f = new FileInfo(path);
+			using(BinaryWriter writer = new BinaryWriter(f.OpenWrite())) {
+				Console.WriteLine($"Base stream is: <{writer.BaseStream}>");
+
+				double aDouble = 1234.67;
+				int anInt = 34567;
+				string aString = "A, B, C";
+				Console.WriteLine($"variables: {aDouble}, {anInt}, {aString}");
+
+				writer.Write(aDouble);
+				writer.Write(anInt);
+				writer.Write(aString);
+			}
+
+			Console.WriteLine("Done");
 		}
 	}
 }
