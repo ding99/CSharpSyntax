3ae62180fc320918f3fdfc1df91c90d3009ae170
Wei Ding <ding_web@yahoo.co.jp>
2020-10-30 13:17:28 -0400

    stringWriter and stringReader project

---
+++ CS60DN46/Ch20IO/StringReaderWriterApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringReaderWriterApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** StringWriter / StringReader *****");
+			Writing();
+			Reading();
+			Console.ResetColor();
+		}
+
+		private static void Reading() {
+			//string path = @"E:\workFolder\cs60\ch20\files\reminders.txt";
+			//string input = null;
+
+			//Console.ForegroundColor = ConsoleColor.Cyan;
+			//Console.WriteLine($"Using File Type to read file <{path}>");
+			//using (StreamReader sr = File.OpenText(path))
+			//	while ((input = sr.ReadLine()) != null)
+			//		Console.WriteLine(input);
+
+			//Console.ForegroundColor = ConsoleColor.DarkCyan;
+			//Console.WriteLine("-> Create StreamReader Directly");
+			//using (StreamReader sr = new StreamReader(path))
+			//	while ((input = sr.ReadLine()) != null)
+			//		Console.WriteLine(input);
+		}
+
+		private static void Writing() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			string path = @"E:\workFolder\cs60\ch20\files\reminders.txt";
+			Console.WriteLine($"Write some message to file <{path}>");
+
+			using (StringWriter writer = new StringWriter()) {
+				writer.WriteLine("Don't forget Mother's Day this year...");
+				writer.WriteLine("Don't forget Father's Day this year...");
+				writer.WriteLine("Don't forget these numbers:");
+				for (int i = 0; i < 10; i++) writer.Write(i + " ");
+				writer.Write(writer.NewLine);
+
+				Console.WriteLine($"Contents of StringWriter:");
+				Console.WriteLine(writer);
+			}
+		}
+	}
+}
