2895bfa6cd7847195e26c0bee26fa2bea411a555
Wei Ding <ding_web@yahoo.co.jp>
2020-10-30 00:28:06 -0400

    streamread

--- CS60DN46/Ch20IO/StreamWriterReaderApp/Program.cs
+++ CS60DN46/Ch20IO/StreamWriterReaderApp/Program.cs
@@ -10,13 +10,29 @@ namespace StreamWriterReaderApp {
 		static void Main() {
 			Console.WriteLine("***** StreanWriter / StreamReader *****");
 			Writing();
+			Reading();
 			Console.ResetColor();
+		}
+
+		private static void Reading() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+
+			string path = @"E:\workFolder\cs60\ch20\files\reminders.txt";
+			Console.WriteLine($"Here are your thoughts: (from file <{path}>)");
+
+			using (StreamReader sr = File.OpenText(path)) {
+				string input = null;
+
+				while ((input = sr.ReadLine()) != null)
+					Console.WriteLine(input);
+			}
 		}
 
 		private static void Writing() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 
 			string path = @"E:\workFolder\cs60\ch20\files\reminders.txt";
+			Console.WriteLine($"Write some message to file <{path}>");
 
 			using(StreamWriter wr = File.CreateText(path)) {
 				wr.WriteLine("Don't forget Mother's Day this year...");
