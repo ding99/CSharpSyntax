564c62b61dafcd1d0e5f07cb75a6f97f08ed93ac
Wei Ding <ding_web@yahoo.co.jp>
2020-10-29 22:55:41 -0400

    project for examing FileStreams

---
+++ CS60DN46/Ch20IO/FileStreamApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileStreamApp {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Working with FileStream *****");
+			WriteSample();
+			Console.ResetColor();
+		}
+
+		private static void WriteSample() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			string path = @"E:\workFolder\cs60\ch20\files\StreamWriting.txt";
+		}
+	}
+}
