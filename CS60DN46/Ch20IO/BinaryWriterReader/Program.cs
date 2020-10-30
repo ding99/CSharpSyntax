9ba21d2954d842107093267a6ac3aa0db58c2c6c
Wei Ding <ding_web@yahoo.co.jp>
2020-10-30 16:51:39 -0400

    create project for binarywriter and binaryreader

---
+++ CS60DN46/Ch20IO/BinaryWriterReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryWriterReader {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** BinaryWriter / BinaryReader *****");
+			WriteRead();
+			Console.ResetColor();
+		}
+
+		private static void WriteRead() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=>");
+		}
+	}
+}
