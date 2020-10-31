91b577900873856cdfba199ff494f7c75dccf139
Wei Ding <ding_web@yahoo.co.jp>
2020-10-30 23:49:09 -0400

    watcher project start

---
+++ CS60DN46/Ch20IO/DirectoryWatcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DirectoryWatcher {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** The Amazing File Watcher App *****");
+			Watching();
+			Console.ResetColor();
+		}
+
+		private static void Watching() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Use FileSystemWatcher");
+		}
+	}
+}
