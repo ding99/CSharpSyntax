a8f692a2738befcce6649a5c4cd3328f9013e112
Wei Ding <ding_web@yahoo.co.jp>
2020-10-31 00:39:51 -0400

    watcher works

--- CS60DN46/Ch20IO/DirectoryWatcher/Program.cs
+++ CS60DN46/Ch20IO/DirectoryWatcher/Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 namespace DirectoryWatcher {
 	class Program {
 		static void Main() {
-			Console.WriteLine("***** The Amazing File Watcher App *****");
+			Console.WriteLine("***** The Amazing File Watcher *****");
 			Watching();
 			Console.ResetColor();
 		}
@@ -16,6 +16,37 @@ namespace DirectoryWatcher {
 		private static void Watching() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> Use FileSystemWatcher");
+
+			string folder = @"E:\workFolder\cs60\ch20\watchFolder";
+
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			try {
+				watcher.Path = folder;
+			} catch(ArgumentException e) {
+				Console.WriteLine(e.Message);
+				return;
+			}
+
+			watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+			watcher.Filter = "*.txt";
+
+			watcher.Changed += new FileSystemEventHandler(OnChanged);
+			watcher.Created += new FileSystemEventHandler(OnChanged);
+			watcher.Deleted += new FileSystemEventHandler(OnChanged);
+			watcher.Renamed += new RenamedEventHandler(OnRemaned);
+
+			watcher.EnableRaisingEvents = true;
+
+			Console.WriteLine(@"Press 'q' to quit app.");
+			while (Console.Read() != 'q') ;
+		}
+
+		private static void OnChanged(object source, FileSystemEventArgs e) {
+			Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+		}
+
+		private static void OnRemaned(object source, RenamedEventArgs e) {
+			Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
 		}
 	}
 }
