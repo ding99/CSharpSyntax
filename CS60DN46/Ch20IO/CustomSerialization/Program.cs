f17491ff7afdd41b85f52e091218fb6099dd71f1
Wei Ding <ding_web@yahoo.co.jp>
2020-11-01 04:14:34 -0500

    ISerializable

---
+++ CS60DN46/Ch20IO/CustomSerialization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace CustomSerialization {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Custom Serialization *****");
+			Customizing("MyData.soap");
+			Console.ResetColor();
+		}
+
+		private static void Customizing(string file) {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine($"=> Serialize {file} as Soap");
+
+			StringData myData = new StringData();
+
+			SoapFormatter soap = new SoapFormatter();
+			using(Stream s = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None)) {
+				soap.Serialize(s, myData);
+			}
+		}
+	}
+}
