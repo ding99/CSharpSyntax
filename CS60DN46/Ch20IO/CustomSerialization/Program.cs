db1827fd577c157bf63a0773d8b81a3e4562f6cd
Wei Ding <ding_web@yahoo.co.jp>
2020-11-01 04:32:07 -0500

    custom serialization by attributes

--- CS60DN46/Ch20IO/CustomSerialization/Program.cs
+++ CS60DN46/Ch20IO/CustomSerialization/Program.cs
@@ -6,13 +6,26 @@ namespace CustomSerialization {
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Custom Serialization *****");
-			Customizing("MyData.soap");
+			CustomizeByISerializable("MyDataISerializable.soap");
+			CustomizeByAttributes("MyDataAttributes.soap");
 			Console.ResetColor();
 		}
 
-		private static void Customizing(string file) {
+		private static void CustomizeByAttributes(string file) {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine($"=> Serialize {file} as Soap Using Attribute");
+
+			MoreData myData = new MoreData();
+
+			SoapFormatter soap = new SoapFormatter();
+			using (Stream s = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None)) {
+				soap.Serialize(s, myData);
+			}
+		}
+
+		private static void CustomizeByISerializable(string file) {
 			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine($"=> Serialize {file} as Soap");
+			Console.WriteLine($"=> Serialize {file} as Soap Using ISerializable");
 
 			StringData myData = new StringData();
 
--- CS60DN46/Ch20IO/CustomSerialization/StringData.cs
+++ CS60DN46/Ch20IO/CustomSerialization/StringData.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Soap;
 
 namespace CustomSerialization {
 	[Serializable]
