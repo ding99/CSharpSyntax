73dab44cb8b149c67d6d3b64bea1f23e60191315
Wei Ding <ding_web@yahoo.co.jp>
2020-11-01 23:31:52 -0500

    public method

--- Variables/cVariable/Source/Program.cs
+++ Variables/cVariable/Source/Program.cs
@@ -10,20 +10,21 @@ namespace CVariable {
 			Vari v = new Vari();
 			ret = v.bytedef();
 			ret = v.rows();
-			ret = v.toFloat();
-			v.sel();
-			v.valuable();
+			//ret = v.toFloat();
+			ret = Vari.toFloat();
+			//v.sel();
+			//v.valuable();
 
-			Console.ForegroundColor = ConsoleColor.Cyan;
-			ret = new Marks().TestMark();
+			//Console.ForegroundColor = ConsoleColor.Cyan;
+			//ret = new Marks().TestMark();
 
-			Console.ForegroundColor = ConsoleColor.DarkYellow;
-			ret = new NullAble().TestValue();
+			//Console.ForegroundColor = ConsoleColor.DarkYellow;
+			//ret = new NullAble().TestValue();
 
-			Console.ForegroundColor = ConsoleColor.DarkCyan;
-			Examine exam = new Examine();
-            exam.Interpolation();
-            exam.ExamineStruct();
+			//Console.ForegroundColor = ConsoleColor.DarkCyan;
+			//Examine exam = new Examine();
+			//         exam.Interpolation();
+			//         exam.ExamineStruct();
 
 			Console.ResetColor();
             Console.WriteLine("== end (" + (ret ? "success)" : "failuer)"));
