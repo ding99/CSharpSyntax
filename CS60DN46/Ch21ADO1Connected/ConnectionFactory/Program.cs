5e6fbbeeec5199fd4e353ba3b41fb9ad4b43c8e1
Wei Ding <ding_web@yahoo.co.jp>
2020-11-04 23:08:59 -0500

    first project of chapter 21

---
+++ CS60DN46/Ch21ADO1Connected/ConnectionFactory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionFactory {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Connection Factory *****");
+
+			Console.ResetColor();
+		}
+	}
+}
