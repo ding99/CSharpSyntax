b91019f283b2f31f202d11b44ebbe42b6b039c4c
Wei Ding <ding_web@yahoo.co.jp>
2020-11-04 23:19:08 -0500

    simple connected factory start

--- CS60DN46/Ch21ADO1Connected/ConnectionFactory/Program.cs
+++ CS60DN46/Ch21ADO1Connected/ConnectionFactory/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.OleDb;
 
 namespace ConnectionFactory {
 	class Program {
 		static void Main() {
-			Console.WriteLine("***** Connection Factory *****");
-
+			Console.WriteLine("***** Very Simple Connection Factory *****");
+			SimpleFactory();
 			Console.ResetColor();
+		}
+
+		private static void SimpleFactory() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Connection Factory");
 		}
 	}
 }
