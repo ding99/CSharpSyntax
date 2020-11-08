4764add5ede4cf33eaa675a0a65ded30eb36a230
Wei Ding <ding_web@yahoo.co.jp>
2020-11-08 11:30:18 -0500

    strongly typed window

---
+++ CS60DN46/Ch26IntroduceWPF/WpfAppAllCodeStronglyTypedWindow/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfAppAllCodeStronglyTypedWindow {
+	class MainWindow : Window {
+		public MainWindow(string windowTitle, int height, int width) {
+			this.Title = windowTitle;
+			this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			this.Height = height;
+			this.Width = width;
+		}
+	}
+}
