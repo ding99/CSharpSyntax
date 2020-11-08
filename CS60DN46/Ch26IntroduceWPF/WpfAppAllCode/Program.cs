b375042ae3b524c9dcfd8a13c03210843be2842e
Wei Ding <ding_web@yahoo.co.jp>
2020-11-08 02:43:53 -0500

    start wpf app without axml

---
+++ CS60DN46/Ch26IntroduceWPF/WpfAppAllCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppAllCode {
+	class Program : Application {
+		[STAThread]
+		static void Main(string[] args) {
+			//Handle the Startup and Exit events, and then run the application.
+			Program app = new Program();
+			app.Startup += AppStartUp;
+			app.Exit += AppExit;
+			app.Run();
+		}
+
+		static void AppExit(object sender, ExitEventArgs e) {
+			MessageBox.Show("App has exited");
+		}
+
+		static void AppStartUp(object sender, StartupEventArgs e) {
+			Window mainWindow = new Window();
+			mainWindow.Title = "My First WPF App!";
+			mainWindow.Height = 200;
+			mainWindow.Width = 300;
+			mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+			mainWindow.Show();
+		}
+	}
+}
