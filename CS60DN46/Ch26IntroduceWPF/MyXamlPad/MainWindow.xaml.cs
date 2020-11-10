a601c72bbba1b9bc2b225cf404f5e378afba0b4a
Wei Ding <ding_web@yahoo.co.jp>
2020-11-10 13:23:40 -0500

    closed event

--- CS60DN46/Ch26IntroduceWPF/MyXamlPad/MainWindow.xaml.cs
+++ CS60DN46/Ch26IntroduceWPF/MyXamlPad/MainWindow.xaml.cs
@@ -43,6 +43,9 @@ namespace MyXamlPad {
 		}
 
 		private void Window_Closed(object sender, EventArgs e) {
+			//write out the data in the text block to a local *.xaml file.
+			File.WriteAllText(fileName, txtXamlData.Text);
+			Application.Current.Shutdown();
 		}
 
 		private void btnViewXaml_Click(object sender, RoutedEventArgs e) {
