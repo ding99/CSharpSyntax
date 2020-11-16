a34fcc39812f866c9375172ce11945540dc6f26b
Wei Ding <ding_web@yahoo.co.jp>
2020-11-16 00:45:42 -0500

    simple connection factory

--- CS60DN46/Ch21ADO1Connected/ConnectionFactory/Program.cs
+++ CS60DN46/Ch21ADO1Connected/ConnectionFactory/Program.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.Odbc;
 using System.Data.OleDb;
 
 namespace ConnectionFactory {
+	enum DataProvider { SqlServer, OleDb, Odbc, None }
+
 	class Program {
 		static void Main() {
 			Console.WriteLine("***** Very Simple Connection Factory *****");
@@ -19,6 +22,26 @@ namespace ConnectionFactory {
 		private static void SimpleFactory() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> Connection Factory");
+
+			IDbConnection connection = GetConnection(DataProvider.SqlServer);
+			Console.WriteLine($"Your connnection is a {connection.GetType().Name}");
+
+		}
+
+		static IDbConnection GetConnection(DataProvider provider) {
+			IDbConnection connection = null;
+			switch (provider) {
+				case DataProvider.SqlServer:
+					connection = new SqlConnection();
+					break;
+				case DataProvider.OleDb:
+					connection = new OleDbConnection();
+					break;
+				case DataProvider.Odbc:
+					connection = new OdbcConnection();
+					break;
+			}
+			return connection;
 		}
 	}
 }
