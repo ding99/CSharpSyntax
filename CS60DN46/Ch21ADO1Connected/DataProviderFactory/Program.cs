e586546d32bd3d18ce3bce6a3fde9d2edb841661
Wei Ding <ding_web@yahoo.co.jp>
2020-11-16 23:51:21 -0500

    complete data peovider factory

---
+++ CS60DN46/Ch21ADO1Connected/DataProviderFactory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using static System.Console;
+
+namespace DataProviderFactory {
+	class Program {
+		static void Main() {
+			WriteLine("***** Data Provider Factories *****");
+			GetFactory();
+			ResetColor();
+		}
+
+		private static void GetFactory() {
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> Provider Factory");
+		}
+	}
+}
