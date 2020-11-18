493a3c468ab65fc9dd00f77cc932b873bc014513
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 13:51:17 -0500

    connection logic

--- CS60DN46/Ch21ADO1Connected/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ CS60DN46/Ch21ADO1Connected/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -8,5 +8,15 @@ using System.Data.SqlClient;
 
 namespace AutoLotDAL.ConnectedLayer {
 	public class InventoryDAL {
+		private SqlConnection _sqlConnection = null;
+
+		public void OpenConnection(string connectionString) {
+			_sqlConnection = new SqlConnection { ConnectionString = connectionString };
+			_sqlConnection.Open();
+		}
+
+		public void CloseConnection() {
+			_sqlConnection.Close();
+		}
 	}
 }
