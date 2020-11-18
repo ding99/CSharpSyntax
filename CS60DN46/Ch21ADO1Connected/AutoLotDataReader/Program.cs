27d3a7d2f0ceddf15da05b5c06b3f5c28fd33a21
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 00:59:06 -0500

    connected layer

---
+++ CS60DN46/Ch21ADO1Connected/AutoLotDataReader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace AutoLotDataReader {
+	class Program {
+		static void Main() {
+			WriteLine("***** Data Readers *****");
+			DataReader();
+			ResetColor();
+		}
+
+		private static void DataReader() {
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> SqlClient");
+
+			using(SqlConnection connection = new SqlConnection()) {
+				string connectionString = @"Data Source=(local);Integrated Security=SSPI;" + "Initial Catalog=AutoLot";
+				connection.ConnectionString = connectionString;
+				connection.Open();
+
+				string sql = "Select * From Inventory";
+				SqlCommand command = new SqlCommand(sql, connection);
+
+				using(SqlDataReader reader = command.ExecuteReader()) {
+					while (reader.Read())
+						WriteLine($"-> Make: {reader["Make"]}, PetName: {reader["PetName"]}.");
+				}
+			}
+		}
+	}
+}
