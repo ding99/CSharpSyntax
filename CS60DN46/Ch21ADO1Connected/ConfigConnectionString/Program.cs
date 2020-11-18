de1acd83f9350c570e829335bab5465e626340ad
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 00:37:56 -0500

    clean code

--- CS60DN46/Ch21ADO1Connected/ConfigConnectionString/Program.cs
+++ CS60DN46/Ch21ADO1Connected/ConfigConnectionString/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
-using System.Data;
 using System.Data.Common;
 using static System.Console;
 
