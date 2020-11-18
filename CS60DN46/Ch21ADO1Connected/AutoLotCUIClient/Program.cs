c6cf132c1aa96e557cd57ce1a2e4030fa1764970
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 15:38:12 -0500

    data access cui

---
+++ CS60DN46/Ch21ADO1Connected/AutoLotCUIClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using static System.Console;
+using AutoLotDAL.Models;
+using AutoLotDAL.ConnectedLayer;
+
+namespace AutoLotCUIClient {
+	class Program {
+		static void Main() {
+		}
+	}
+}
