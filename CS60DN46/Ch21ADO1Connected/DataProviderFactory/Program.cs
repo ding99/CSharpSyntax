5097f59b7c9074c10198363073ce62b2fed8bebc
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 00:15:29 -0500

    sql

--- CS60DN46/Ch21ADO1Connected/DataProviderFactory/Program.cs
+++ CS60DN46/Ch21ADO1Connected/DataProviderFactory/Program.cs
@@ -23,8 +23,7 @@ namespace DataProviderFactory {
 			WriteLine("=> Use Configuration");
 
 			string dataProvider = ConfigurationManager.AppSettings["provider"];
-			//string connectionString = ConfigurationManager.AppSettings["connectionString"];
-			string connectionString = ConfigurationManager.AppSettings["cnStr"];
+			string connectionString = ConfigurationManager.AppSettings["connectionString"];
 
 			WriteLine($"dataProvider <{dataProvider}>, connectionString <{connectionString}>");
 
@@ -39,6 +38,11 @@ namespace DataProviderFactory {
 
 				connection.ConnectionString = connectionString;
 				connection.Open();
+
+				var sqlConnection = connection as System.Data.SqlClient.SqlConnection;
+				if (sqlConnection != null)
+					WriteLine($"Sql Connnection version is {sqlConnection.ServerVersion}");
+				else WriteLine("This is not a Sql Connection");
 
 				DbCommand command = factory.CreateCommand();
 				if(command == null) {
@@ -66,10 +70,29 @@ namespace DataProviderFactory {
 }
 
 /**
+== SQL Server ==
+***** Data Provider Factories *****
+=> Use Configuration
+dataProvider <System.Data.SqlClient>, connectionString <Data Source=(local);Initial Catalog=AutoLot;Integrated Security=True>
+Your Connection object is a SqlConnection
+Sql Connnection version is 15.00.2070
+Your command object is a SqlCommand
+Your data reader object is a SqlDataReader
+----- Current Inventory -----
+-> Car #1 is a VW with a color Black
+-> Car #2 is a Ford with a color Rust
+-> Car #3 is a Saab with a color Black
+-> Car #4 is a Yugo with a color Yellow
+-> Car #5 is a BMW with a color Black
+-> Car #6 is a BMW with a color Green
+-> Car #7 is a BMW with a color Pink
+
+== OLEDB ==
 ***** Data Provider Factories *****
 => Use Configuration
 dataProvider <System.Data.OleDb>, connectionString <Provider=SQLOLEDB;Data Source=(local);Initial Catalog=AutoLot;Integrated Security=SSPI>
 Your Connection object is a OleDbConnection
+This is not a Sql Connection
 Your command object is a OleDbCommand
 Your data reader object is a OleDbDataReader
 ----- Current Inventory -----
