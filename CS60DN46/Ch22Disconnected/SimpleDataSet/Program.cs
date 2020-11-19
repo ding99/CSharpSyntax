694d339d5c1ff0e99fbe6287f0d13dcc5ee1710b
Wei Ding <ding_web@yahoo.co.jp>
2020-11-19 00:46:50 -0500

    define dataColumn

--- CS60DN46/Ch22Disconnected/SimpleDataSet/Program.cs
+++ CS60DN46/Ch22Disconnected/SimpleDataSet/Program.cs
@@ -3,21 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using System.Data;
+using static System.Console;
 
 namespace SimpleDataSet {
 	class Program {
 		static void Main() {
-			Console.WriteLine("***** Fun with DataSets *****");
+			WriteLine("***** Fun with DataSets *****");
 
 			CreateData();
 
-			Console.ResetColor();
+			ResetColor();
 		}
 
 		private static void CreateData() {
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("=> Create DataSet");
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> Create DataSet");
+
+			var ds = new DataSet("Car Inventory");
+
+			ds.ExtendedProperties["TimeStamp"] = DateTime.Now;
+			ds.ExtendedProperties["DataSetID"] = Guid.NewGuid();
+			ds.ExtendedProperties["Company"] = "Hot Tub Super Store";
+
+			FillDataSet(ds);
+			PrintDataSet(ds);
+		}
+
+		private static void FillDataSet(DataSet s) {
+			WriteLine($"-> Fill DataSet, <{s.ExtendedProperties["Company"]}>");
+		}
+
+		private static void PrintDataSet(DataSet s) {
+			WriteLine("-> Print DataSet");
+			var props = s.ExtendedProperties;
+			WriteLine($"Properties (size {props.Count}):");
+			foreach (var a in props)
+				if(a is DictionaryEntry)
+					WriteLine($"  <{((DictionaryEntry)a).Key}> : <{((DictionaryEntry)a).Value}> ");
 		}
 	}
 }
