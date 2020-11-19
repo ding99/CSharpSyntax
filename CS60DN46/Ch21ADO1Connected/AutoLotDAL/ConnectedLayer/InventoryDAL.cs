a27235f41a81eafdf2cde2107ca15b93de6260f7
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 19:53:17 -0500

    remove unused references

--- CS60DN46/Ch21ADO1Connected/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ CS60DN46/Ch21ADO1Connected/AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using AutoLotDAL.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
-using AutoLotDAL.Models;
 using static System.Console;
 
 namespace AutoLotDAL.ConnectedLayer {
