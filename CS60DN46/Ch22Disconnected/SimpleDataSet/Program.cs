012cbe1f86679350e57e86feb09afcd1d4111758
Wei Ding <ding_web@yahoo.co.jp>
2020-11-19 00:12:44 -0500

    simple dataset start

---
+++ CS60DN46/Ch22Disconnected/SimpleDataSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SimpleDataSet {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Fun with DataSets *****");
+
+			CreateData();
+
+			Console.ResetColor();
+		}
+
+		private static void CreateData() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Create DataSet");
+		}
+	}
+}
