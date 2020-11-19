86f32eac1efd568fb6aa2dfa52974c63c66bae4e
Wei Ding <ding_web@yahoo.co.jp>
2020-11-18 20:04:52 -0500

    test transaction

---
+++ CS60DN46/Ch21ADO1Connected/AdoNetTransaction/Program.cs
@@ -0,0 +1,28 @@
+﻿using AutoLotDAL.ConnectedLayer;
+using System;
+using static System.Console;
+
+namespace AdoNetTransaction {
+	class Program {
+		static void Main() {
+			WriteLine("***** Simple Transaction Example *****");
+			ForegroundColor = ConsoleColor.Yellow;
+
+			bool throwEx = true;
+
+			Write("Do you want to throw an exception (Y or N): ");
+			var userAnswer = ReadLine();
+			if (userAnswer?.ToLower() == "n")
+				throwEx = false;
+
+			var dal = new InventoryDAL();
+			string connectString = @"Data Source=(local);Integrated Security=SSPI;Initial Catalog=AutoLot";
+			dal.OpenConnection(connectString);
+
+			dal.ProcessCreditRisk(throwEx, 5);
+			WriteLine("Check CreditRisk table for results");
+
+			ResetColor();
+		}
+	}
+}
