1546f6a0c91a3f10ac6b1a8806de24f3f039874a
Wei Ding <ding_web@yahoo.co.jp>
2020-11-20 21:42:07 -0500

    clear

--- CS60DN46/Ch22Disconnected/FillDataSetUsingDataAdapter/Program.cs
+++ CS60DN46/Ch22Disconnected/FillDataSetUsingDataAdapter/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.Data.SqlClient;
 using System.Collections;
+using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
 using static System.Console;
 
 namespace FillDataSetUsingDataAdapter {
