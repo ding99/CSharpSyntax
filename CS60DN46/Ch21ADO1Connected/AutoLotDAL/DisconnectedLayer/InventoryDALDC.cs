e3ef280a93bc96bd4d51206b41986ca73da34e15
Wei Ding <ding_web@yahoo.co.jp>
2020-11-20 23:20:10 -0500

    get all inventory

--- CS60DN46/Ch21ADO1Connected/AutoLotDAL/DisconnectedLayer/InventoryDALDC.cs
+++ CS60DN46/Ch21ADO1Connected/AutoLotDAL/DisconnectedLayer/InventoryDALDC.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace AutoLotDAL.DisconnectedLayer {
-	class InventoryDALDC {
+	public class InventoryDALDC {
+		private string _connectionString;
+		private SqlDataAdapter _adapter = null;
+
+		public InventoryDALDC(string connectionString) {
+			_connectionString = connectionString;
+			ConfigureAdapter(out _adapter);
+		}
+
+		private void ConfigureAdapter(out SqlDataAdapter adapter) {
+			adapter = new SqlDataAdapter("Select * From Inventory", _connectionString);
+
+			var builder = new SqlCommandBuilder(adapter);
+		}
+
+		public DataTable GetAllInventory() {
+			DataTable inv = new DataTable("Inventory");
+			_adapter.Fill(inv);
+			return inv;
+		}
 	}
 }
