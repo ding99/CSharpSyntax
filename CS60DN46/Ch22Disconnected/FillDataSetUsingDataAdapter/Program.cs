6f40087081639fc0905715b0b0eea44561b2444e
Wei Ding <ding_web@yahoo.co.jp>
2020-11-20 21:03:00 -0500

    read data

--- CS60DN46/Ch22Disconnected/FillDataSetUsingDataAdapter/Program.cs
+++ CS60DN46/Ch22Disconnected/FillDataSetUsingDataAdapter/Program.cs
@@ -12,7 +12,26 @@ namespace FillDataSetUsingDataAdapter {
 	class Program {
 		static void Main() {
 			WriteLine("***** Fun with Data Adapter *****");
+			FetchData();
 			ResetColor();
+		}
+
+		private static void FetchData() {
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> Fetch data from database");
+
+			string connectionString = "Integrated Security=SSPI;Initial Catalog=AutoLot;Data Source=(local)";
+			DataSet ds = new DataSet("AutoLot");
+
+			SqlDataAdapter adapter = new SqlDataAdapter("Select * From Inventory", connectionString);
+
+			WriteLine($"The number of rows is {adapter.Fill(ds, "Inventory")}.");
+
+			PrintDataSet(ds);
+		}
+
+		private static void PrintDataSet(DataSet d) {
+
 		}
 	}
 }
