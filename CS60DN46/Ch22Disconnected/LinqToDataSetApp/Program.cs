fd694a3476df91197eda6e34cc24b21ed211219c
Wei Ding <ding_web@yahoo.co.jp>
2020-11-21 23:53:57 -0500

    print datatable

--- CS60DN46/Ch22Disconnected/LinqToDataSetApp/Program.cs
+++ CS60DN46/Ch22Disconnected/LinqToDataSetApp/Program.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoLotDAL.DataSet;
+using AutoLotDAL.DataSet.AutoLotDataSetTableAdapters;
+using System.Data;
+using static System.Console;
 
 namespace LinqToDataSetApp {
 	class Program {
-		static void Main(string[] args) {
+		static void Main() {
+			WriteLine("***** Linq over DataSet *****");
+			UseLinq();
+			ResetColor();
+		}
+
+		private static void UseLinq() {
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> Linq-Complatible DataTable");
+
+			AutoLotDataSet dal = new AutoLotDataSet();
+			InventoryTableAdapter adapter = new InventoryTableAdapter();
+			AutoLotDataSet.InventoryDataTable data = adapter.GetData();
+
+			PrintAllCarIDs(data);
+		}
+
+		private static void PrintAllCarIDs(DataTable data) {
+			EnumerableRowCollection enumData = data.AsEnumerable();
+			foreach (DataRow r in enumData)
+				WriteLine($"Car ID = {r["CarID"]}");
 		}
 	}
 }
