ee22c39ab07f14b0946eeafdfb85fede1d419742
Wei Ding <ding_web@yahoo.co.jp>
2020-11-22 00:43:36 -0500

    list black cars using linq

--- CS60DN46/Ch22Disconnected/LinqToDataSetApp/Program.cs
+++ CS60DN46/Ch22Disconnected/LinqToDataSetApp/Program.cs
@@ -12,11 +12,11 @@ namespace LinqToDataSetApp {
 	class Program {
 		static void Main() {
 			WriteLine("***** Linq over DataSet *****");
-			UseLinq();
+			UseEnumerable();
 			ResetColor();
 		}
 
-		private static void UseLinq() {
+		private static void UseEnumerable() {
 			ForegroundColor = ConsoleColor.Yellow;
 			WriteLine("=> Linq-Complatible DataTable");
 
@@ -25,11 +25,21 @@ namespace LinqToDataSetApp {
 			AutoLotDataSet.InventoryDataTable data = adapter.GetData();
 
 			PrintAllCarIDs(data);
+			ShowBalckCars(data);
+		}
+
+		private static void ShowBalckCars(DataTable data) {
+			ForegroundColor = ConsoleColor.Cyan;
+
+			var cars = from car in data.AsEnumerable() where (string)car["Color"] == "Black" select new { ID = (int)car["CarID"], Make = (string)car["Make"] };
+			WriteLine($"Here are the black cars we have in stock (size {cars.Count()}) :");
+			foreach (var item in cars)
+				WriteLine($"CarID = {item.ID} is {item.Make}");
 		}
 
 		private static void PrintAllCarIDs(DataTable data) {
 			EnumerableRowCollection enumData = data.AsEnumerable();
-			Write($"Car IDs:");
+			Write($"All Cars with IDs:");
 			foreach (DataRow r in enumData)
 				Write($" {r["CarID"]}");
 			WriteLine();
