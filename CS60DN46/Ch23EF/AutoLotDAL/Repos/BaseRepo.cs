589891f1c4c64971050b6716a80f941ed3fb1ab9
Wei Ding <ding_web@yahoo.co.jp>
2020-11-27 02:31:24 -0500

    saving record

--- CS60DN46/Ch23EF/AutoLotDAL/Repos/BaseRepo.cs
+++ CS60DN46/Ch23EF/AutoLotDAL/Repos/BaseRepo.cs
@@ -13,24 +13,6 @@ namespace AutoLotDAL.Repos {
 		public AutoLotEntities Context { get; } = new AutoLotEntities();
 		protected DbSet<T> Table;
 
-		internal int SaveChanges() {
-			try {
-				return Context.SaveChanges();
-			}
-			catch(DbUpdateConcurrencyException ex) {
-				throw;
-			}
-			catch(DbUpdateException ex) {
-				throw;
-			}
-			catch(CommitFailedException ex) {
-				throw;
-			}
-			catch(Exception ex) {
-				throw;
-			}
-		}
-
 		public int Add(T entity) {
 			Table.Add(entity);
 			return SaveChanges();
@@ -46,6 +28,43 @@ namespace AutoLotDAL.Repos {
 		public Task<int> AddRangeAsync(IList<T> entities) {
 			Table.AddRange(entities);
 			return SaveChangesAsync();
+		}
+
+		public int Save(T entity) {
+			Context.Entry(entity).State = EntityState.Modified;
+			return SaveChanges();
+		}
+		public Task<int> SaveAsync(T entity) {
+			Context.Entry(entity).State = EntityState.Modified;
+			return SaveChangesAsync();
+		}
+
+		public T GetOne(int? id) => Table.Find(id);
+		public Task<T> GetOneAsync(int? id) => Table.FindAsync(id);
+		public List<T> GetAll() => Table.ToList();
+		public Task<List<T>> GetAllAsync() => Table.ToListAsync();
+
+		public List<T> ExecuteQuery(string sql) => Table.SqlQuery(sql).ToList();
+		public Task<List<T>> ExecuteQueryAsync(string sql) => Table.SqlQuery(sql).ToListAsync();
+		public List<T> ExecuteQuery(string sql, object[] sqlParametersObjects) => Table.SqlQuery(sql, sqlParametersObjects).ToList();
+		public Task<List<T>> ExecuteQueryAsync(string sql, object[] sqlParametersObjects) => Table.SqlQuery(sql, sqlParametersObjects).ToListAsync();
+
+		internal int SaveChanges() {
+			try {
+				return Context.SaveChanges();
+			}
+			catch (DbUpdateConcurrencyException ex) {
+				throw;
+			}
+			catch (DbUpdateException ex) {
+				throw;
+			}
+			catch (CommitFailedException ex) {
+				throw;
+			}
+			catch (Exception ex) {
+				throw;
+			}
 		}
 
 		internal async Task<int> SaveChangesAsync() {
@@ -65,15 +84,20 @@ namespace AutoLotDAL.Repos {
 				throw;
 			}
 		}
+	}
 
-		public T GetOne(int? id) => Table.Find(id);
-		public Task<T> GetOneAsync(int? id) => Table.FindAsync(id);
-		public List<T> GetAll() => Table.ToList();
-		public Task<List<T>> GetAllAsync() => Table.ToListAsync();
+	public abstract class BaseRepo : IDisposable {
+		protected AutoLotEntities Context { get; } = new AutoLotEntities();
 
-		public List<T> ExecuteQuery(string sql) => Table.SqlQuery(sql).ToList();
-		public Task<List<T>> ExecuteQueryAsync(string sql) => Table.SqlQuery(sql).ToListAsync();
-		public List<T> ExecuteQuery(string sql, object[] sqlParametersObjects) => Table.SqlQuery(sql, sqlParametersObjects).ToList();
-		public Task<List<T>> ExecuteQueryAsync(string sql, object[] sqlParametersObjects) => Table.SqlQuery(sql, sqlParametersObjects).ToListAsync();
+		bool disposed = false;
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		protected virtual void Dispose(bool disposing) {
+			if (disposed) return;
+			if (disposing) Context.Dispose();
+			disposed = true;
+		}
 	}
 }
