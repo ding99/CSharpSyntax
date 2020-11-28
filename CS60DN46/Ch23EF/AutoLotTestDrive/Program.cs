cace30e0337a9b46b40b64ff77fcde0f6e8b2d74
Wei Ding <ding_web@yahoo.co.jp>
2020-11-28 00:10:26 -0500

    clean up

--- CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
+++ CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
@@ -2,9 +2,9 @@
 using AutoLotDAL.Models;
 using AutoLotDAL.Repos;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
-using System.Collections.Generic;
 using System.Linq;
 using static System.Console;
 
