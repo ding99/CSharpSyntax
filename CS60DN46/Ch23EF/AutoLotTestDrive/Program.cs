af01dfd329427efb66041ccad6f8f19f2210fb90
Wei Ding <ding_web@yahoo.co.jp>
2020-11-27 19:27:49 -0500

    add test project

---
+++ CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using AutoLotDAL.EF;
+using AutoLotDAL.Repos;
+using AutoLotDAL.Models;
+using static System.Console;
+
+namespace AutoLotTestDrive {
+	class Program {
+		static void Main() {
+			Database.SetInitializer(new DataInitializer());
+			WriteLine("***** Fun with ADO.NET EF Code First *****");
+
+
+			ResetColor();
+		}
+
+		private static void PrintAllInventory() {
+			using (var repo = new InventoryRepo())
+				foreach (Inventory c in repo.GetAll())
+					WriteLine(c);
+		}
+	}
+}
