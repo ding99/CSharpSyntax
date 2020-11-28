ebf01c292a652b71af3105f2895adc853384e37b
Wei Ding <ding_web@yahoo.co.jp>
2020-11-27 19:44:49 -0500

    print all

--- CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
+++ CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using AutoLotDAL.Models;
+using AutoLotDAL.EF;
+using AutoLotDAL.Repos;
+using static System.Console;
 
 namespace AutoLotTestDrive {
 	class Program {
 		static void Main() {
+			WriteLine("***** Fun with ADO.NET EF Code First *****");
+			Database.SetInitializer(new DataInitializer());
+
+			PrintAllInventory();
+
+			ResetColor();
+		}
+
+		private static void PrintAllInventory() {
+			ForegroundColor = ConsoleColor.Yellow;
+			WriteLine("=> Print All Inventory");
+
+			using (var repo = new InventoryRepo())
+				foreach (Inventory c in repo.GetAll())
+					WriteLine(c);
 		}
 	}
 }
