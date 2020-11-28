7de85c4556d4090ad7c79d7e74eb0fc756f84bdd
Wei Ding <ding_web@yahoo.co.jp>
2020-11-27 19:57:09 -0500

    add new cars

--- CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
+++ CS60DN46/Ch23EF/AutoLotTestDrive/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
+﻿using AutoLotDAL.EF;
 using AutoLotDAL.Models;
-using AutoLotDAL.EF;
 using AutoLotDAL.Repos;
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace AutoLotTestDrive {
@@ -16,8 +13,32 @@ namespace AutoLotTestDrive {
 			Database.SetInitializer(new DataInitializer());
 
 			PrintAllInventory();
+			AddNewCar();
 
 			ResetColor();
+		}
+
+		private static void AddNewCar() {
+			ForegroundColor = ConsoleColor.Cyan;
+			WriteLine("=> Adding New Inventory");
+
+			var car1 = new Inventory { Make="Yugo", Color = "Brown", PetName = "Brownie" };
+			var car2 = new Inventory { Make = "SmartCar", Color = "Brown", PetName = "Shorty" };
+
+			AddNewRecord(car1);
+			AddNewRecords(new List<Inventory> { car1, car2 });
+
+			PrintAllInventory();
+		}
+
+		private static void AddNewRecord(Inventory car) {
+			using (var repo = new InventoryRepo())
+				repo.Add(car);
+		}
+
+		private static void AddNewRecords(IList<Inventory> cars) {
+			using (var repo = new InventoryRepo())
+				repo.AddRange(cars);
 		}
 
 		private static void PrintAllInventory() {
