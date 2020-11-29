2988defa886f919d52bbdc44315c7594832dac16
Wei Ding <ding_web@yahoo.co.jp>
2020-11-29 05:41:52 -0500

    VC example

---
+++ CS60DN46/Ch24LinqToXml/XmlLook/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+
+Public Module Module1
+    Sub Main()
+        Console.WriteLine("Hi XML")
+    End Sub
+
+    'Public Class XmlLiteralExample
+    '    Public Sub MakeXmlFileUsingLiterals()
+    '        ' Notice that we can inline XML data to an XElement
+    '        Dim doc As XElement =
+    '            <Inventory>
+    '                <Car ID="1000">
+    '                    <PetName>Jimbo</PetName>
+    '                    <Color>Red</Color>
+    '                    <Make>Ford</Make>
+    '                </Car>
+    '            </Inventory>
+    '        ' Save to file
+    '        doc.Save("InventoryVBStyle.xml")
+    '    End Sub
+    'End Class
+End Module
+
+
