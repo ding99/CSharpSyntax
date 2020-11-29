9dd09defb15c871e8c6a5bda2782be0a708df1d6
Wei Ding <ding_web@yahoo.co.jp>
2020-11-29 05:04:07 -0500

    use DOM

--- CS60DN46/Ch24LinqToXml/LinqToXmlFirstLook/Program.cs
+++ CS60DN46/Ch24LinqToXml/LinqToXmlFirstLook/Program.cs
@@ -10,14 +10,37 @@ namespace LinqToXmlFirstLook {
 		static void Main() {
 			Console.WriteLine("***** Linq to XML First Look *****");
 
-			UseDOM();
+			BuildXmlDocWithDOM();
 
 			Console.ResetColor();
 		}
 
-		private static void UseDOM() {
+		private static void BuildXmlDocWithDOM() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("=> Build XML Doc with DOM");
+
+			XmlDocument doc = new XmlDocument();
+
+			XmlElement inventory = doc.CreateElement("Inventory");
+
+			XmlElement car = doc.CreateElement("Car");
+			car.SetAttribute("ID", "1000");
+
+			XmlElement name = doc.CreateElement("PerName");
+			name.InnerText = "Jimbo";
+			XmlElement color = doc.CreateElement("Color");
+			color.InnerText = "Red";
+			XmlElement make = doc.CreateElement("Make");
+			make.InnerText = "Ford";
+
+			car.AppendChild(name);
+			car.AppendChild(color);
+			car.AppendChild(make);
+
+			inventory.AppendChild(car);
+
+			doc.AppendChild(inventory);
+			doc.Save("Inventory.xml");
 		}
 	}
 }
