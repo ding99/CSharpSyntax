0b01d1f9f7ff1a4c03b5b2f81e67ab61cb669600
Wei Ding <ding_web@yahoo.co.jp>
2020-11-28 22:34:57 -0500

    start interceptor class

---
+++ CS60DN46/Ch23EF/AutoLotDAL/Interception/ConsoleWriterInterceptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+using static System.Console;
+
+namespace AutoLotDAL.Interception {
+	public class ConsoleWriterInterceptor : IDbCommandInterceptor {
+		public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext) {
+
+		}
+		public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext) {
+
+		}
+
+		public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext) {
+
+		}
+		public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext) {
+
+		}
+
+		public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) {
+
+		}
+		public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) {
+
+		}
+	}
+}
