714ad94b52c3ce0363227ff52b1481ec2e725256
Wei Ding <ding_web@yahoo.co.jp>
2020-11-29 04:53:13 -0500

    look project

---
+++ CS60DN46/Ch24LinqToXml/LinqToXmlFirstLook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LinqToXmlFirstLook {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Linq to XML First Look *****");
+
+			UseDOM();
+
+			Console.ResetColor();
+		}
+
+		private static void UseDOM() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("=> Build XML Doc with DOM");
+		}
+	}
+}
