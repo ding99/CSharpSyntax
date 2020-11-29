6bb87ffd4611414e0866a73e04ed16a2123f76c2
Wei Ding <ding_web@yahoo.co.jp>
2020-11-28 22:42:59 -0500

    register interceptor

--- CS60DN46/Ch23EF/AutoLotDAL/EF/AutoLotEntities.cs
+++ CS60DN46/Ch23EF/AutoLotDAL/EF/AutoLotEntities.cs
@@ -2,13 +2,18 @@ using System;
 using System.Data.Entity;
 using System.Linq;
 using AutoLotDAL.Models;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Infrastructure.Interception;
+using AutoLotDAL.Interception;
 
 namespace AutoLotDAL.EF {
 	public class AutoLotEntities : DbContext {
 		// connection string: 'AutoLotEntities'.
 		// target: 'AutoLotDAL.EF.AutoLotEntities' database on your LocalDb instance. 
 		// If you wish to target a different database and/or database provider, modify the 'AutoLotEntities' connection string in the configuration file.
-		public AutoLotEntities() : base("name=AutoLotConnection") {}
+		public AutoLotEntities() : base("name=AutoLotConnection") {
+			DbInterception.Add(new ConsoleWriterInterceptor());
+		}
 
 		public virtual DbSet<CreditRisk> CreditRisks { get; set; }
 		public virtual DbSet<Customer> Customers { get; set; }
