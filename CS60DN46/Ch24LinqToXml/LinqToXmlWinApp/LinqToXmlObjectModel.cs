85454ed2d1c20e9d858434f9c9123e3b4ea5e039
Wei Ding <ding_web@yahoo.co.jp>
2020-11-30 22:33:56 -0500

    load xml file

---
+++ CS60DN46/Ch24LinqToXml/LinqToXmlWinApp/LinqToXmlObjectModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LinqToXmlWinApp {
+	public class LinqToXmlObjectModel {
+		public static XDocument GetXmlInventory() {
+			try {
+				XDocument inventory = XDocument.Load("Inventory.xml");
+				return inventory;
+			}
+			catch(System.IO.FileNotFoundException ex) {
+				MessageBox.Show(ex.Message);
+				return null;
+			}
+		}
+	}
+}
