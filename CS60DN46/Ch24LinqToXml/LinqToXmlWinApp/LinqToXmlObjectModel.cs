794ff474b8f9190b5676d070c4576fc0283e430f
Wei Ding <ding_web@yahoo.co.jp>
2020-11-30 22:48:35 -0500

    add new car, and search

--- CS60DN46/Ch24LinqToXml/LinqToXmlWinApp/LinqToXmlObjectModel.cs
+++ CS60DN46/Ch24LinqToXml/LinqToXmlWinApp/LinqToXmlObjectModel.cs
@@ -8,6 +8,7 @@ using System.Xml.Linq;
 
 namespace LinqToXmlWinApp {
 	public class LinqToXmlObjectModel {
+		string 
 		public static XDocument GetXmlInventory() {
 			try {
 				XDocument inventory = XDocument.Load("Inventory.xml");
@@ -17,6 +18,31 @@ namespace LinqToXmlWinApp {
 				MessageBox.Show(ex.Message);
 				return null;
 			}
+		}
+
+		public static void InsertNewElement(string make, string color, string petName) {
+			string file = "Inventory.xml";
+			XDocument inventoryDoc = XDocument.Load(file);
+
+			Random r = new Random();
+
+			XElement newElement = new XElement("Car", new XAttribute("ID", r.Next(50000)), new XElement("Color", color), new XElement("Make", make), new XElement("PetName", petName));
+
+			inventoryDoc.Descendants("Inventory").First().Add(newElement);
+
+			inventoryDoc.Save(file);
+		}
+
+		public static void LookUpColorsForMake(string make) {
+			XDocument inventoryDoc = XDocument.Load("Inventory.xml");
+
+			var makeInfo = from car in inventoryDoc.Descendants("Car") where (string)car.Element("Make") == make select car.Element("Color").Value;
+
+			string data = string.Empty;
+			foreach (var item in makeInfo.Distinct())
+				data += string.Format("- {0}\n", item);
+
+			MessageBox.Show(data, string.Format("{0} colors:", make))
 		}
 	}
 }
