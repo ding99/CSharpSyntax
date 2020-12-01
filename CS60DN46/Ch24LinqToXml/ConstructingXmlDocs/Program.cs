c2bb30ed53f70e0d57db120246822a0e175704b5
Wei Ding <ding_web@yahoo.co.jp>
2020-11-30 20:01:25 -0500

    root document and element

--- CS60DN46/Ch24LinqToXml/ConstructingXmlDocs/Program.cs
+++ CS60DN46/Ch24LinqToXml/ConstructingXmlDocs/Program.cs
@@ -10,12 +10,37 @@ namespace ConstructingXmlDocs {
 		static void Main() {
 			Console.WriteLine("***** Constructing Xml Docs *****");
 			CreateFullXDocument();
+			CreateRootAndChildren();
 			Console.ResetColor();
+		}
+
+		private static void CreateRootAndChildren() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("Root and Children");
+
+			XDocument inventoryDoc =
+				new XDocument(
+					new XElement("Inventory",
+						new XComment("Current Inventory of cars!"),
+						new XElement("Car", new XAttribute("ID", "1"),
+							new XElement("Color", "Green"),
+							new XElement("Make", "BWM"),
+							new XElement("PetName", "Stan")
+						),
+						new XElement("Car", new XAttribute("ID", "2"),
+							new XElement("Color", "Pink"),
+							new XElement("Make", "Yugo"),
+							new XElement("PetName", "Melvin")
+						)
+					)
+				);
+
+			inventoryDoc.Save("RootInventory.xml");
 		}
 
 		private static void CreateFullXDocument() {
 			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine("Use XElement");
+			Console.WriteLine("Full XDocument");
 
 			XDocument inventoryDoc =
 				new XDocument(
