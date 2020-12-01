c892f83955fbde2f036698cf3e003d6e95215894
Wei Ding <ding_web@yahoo.co.jp>
2020-11-30 22:19:43 -0500

    winform for linq to xml

---
+++ CS60DN46/Ch24LinqToXml/LinqToXmlWinApp/MainForm.Designer.cs
@@ -0,0 +1,490 @@
+﻿
+namespace LinqToXmlWinApp {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtInventory = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtMake = new System.Windows.Forms.TextBox();
+			this.txtColor = new System.Windows.Forms.TextBox();
+			this.txtPetName = new System.Windows.Forms.TextBox();
+			this.btnAddNewItem = new System.Windows.Forms.Button();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.label14 = new System.Windows.Forms.Label();
+			this.txtMakeToLookUp = new System.Windows.Forms.TextBox();
+			this.btnLookUpColors = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.groupBox5.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 5);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(88, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Current Inventory";
+			// 
+			// txtInventory
+			// 
+			this.txtInventory.Location = new System.Drawing.Point(12, 25);
+			this.txtInventory.Multiline = true;
+			this.txtInventory.Name = "txtInventory";
+			this.txtInventory.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.txtInventory.Size = new System.Drawing.Size(248, 274);
+			this.txtInventory.TabIndex = 1;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.groupBox3);
+			this.groupBox1.Controls.Add(this.groupBox2);
+			this.groupBox1.Controls.Add(this.btnAddNewItem);
+			this.groupBox1.Controls.Add(this.txtPetName);
+			this.groupBox1.Controls.Add(this.txtColor);
+			this.groupBox1.Controls.Add(this.txtMake);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Location = new System.Drawing.Point(266, 9);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(281, 181);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Add Inventory Item";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 33);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(34, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Make";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(16, 69);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(31, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Color";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(16, 108);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(54, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Pet Name";
+			// 
+			// txtMake
+			// 
+			this.txtMake.Location = new System.Drawing.Point(76, 30);
+			this.txtMake.Name = "txtMake";
+			this.txtMake.Size = new System.Drawing.Size(181, 20);
+			this.txtMake.TabIndex = 4;
+			// 
+			// txtColor
+			// 
+			this.txtColor.Location = new System.Drawing.Point(76, 66);
+			this.txtColor.Name = "txtColor";
+			this.txtColor.Size = new System.Drawing.Size(181, 20);
+			this.txtColor.TabIndex = 5;
+			// 
+			// txtPetName
+			// 
+			this.txtPetName.Location = new System.Drawing.Point(76, 101);
+			this.txtPetName.Name = "txtPetName";
+			this.txtPetName.Size = new System.Drawing.Size(181, 20);
+			this.txtPetName.TabIndex = 6;
+			// 
+			// btnAddNewItem
+			// 
+			this.btnAddNewItem.Location = new System.Drawing.Point(160, 140);
+			this.btnAddNewItem.Name = "btnAddNewItem";
+			this.btnAddNewItem.Size = new System.Drawing.Size(97, 23);
+			this.btnAddNewItem.TabIndex = 7;
+			this.btnAddNewItem.Text = "Add";
+			this.btnAddNewItem.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.textBox3);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.label7);
+			this.groupBox2.Location = new System.Drawing.Point(2, 195);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(293, 181);
+			this.groupBox2.TabIndex = 8;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Add Inventory Item";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(182, 140);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(76, 97);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(181, 20);
+			this.textBox1.TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(76, 66);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(181, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(76, 30);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(181, 20);
+			this.textBox3.TabIndex = 4;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(16, 100);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(54, 13);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "Pet Name";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(19, 69);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(31, 13);
+			this.label6.TabIndex = 2;
+			this.label6.Text = "Color";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(16, 33);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(34, 13);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "Make";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.groupBox4);
+			this.groupBox3.Controls.Add(this.button3);
+			this.groupBox3.Controls.Add(this.textBox7);
+			this.groupBox3.Controls.Add(this.textBox8);
+			this.groupBox3.Controls.Add(this.textBox9);
+			this.groupBox3.Controls.Add(this.label11);
+			this.groupBox3.Controls.Add(this.label12);
+			this.groupBox3.Controls.Add(this.label13);
+			this.groupBox3.Location = new System.Drawing.Point(3, 195);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(286, 151);
+			this.groupBox3.TabIndex = 9;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Add Inventory Item";
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.button2);
+			this.groupBox4.Controls.Add(this.textBox4);
+			this.groupBox4.Controls.Add(this.textBox5);
+			this.groupBox4.Controls.Add(this.textBox6);
+			this.groupBox4.Controls.Add(this.label8);
+			this.groupBox4.Controls.Add(this.label9);
+			this.groupBox4.Controls.Add(this.label10);
+			this.groupBox4.Location = new System.Drawing.Point(2, 195);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(293, 181);
+			this.groupBox4.TabIndex = 8;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Add Inventory Item";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(182, 140);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Add";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(76, 97);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(181, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(76, 66);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(181, 20);
+			this.textBox5.TabIndex = 5;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(76, 30);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(181, 20);
+			this.textBox6.TabIndex = 4;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(16, 100);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(54, 13);
+			this.label8.TabIndex = 3;
+			this.label8.Text = "Pet Name";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(19, 69);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(31, 13);
+			this.label9.TabIndex = 2;
+			this.label9.Text = "Color";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(16, 33);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(34, 13);
+			this.label10.TabIndex = 1;
+			this.label10.Text = "Make";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(182, 140);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Add";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(76, 97);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(181, 20);
+			this.textBox7.TabIndex = 6;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(76, 66);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(181, 20);
+			this.textBox8.TabIndex = 5;
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(76, 30);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(181, 20);
+			this.textBox9.TabIndex = 4;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(16, 100);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(54, 13);
+			this.label11.TabIndex = 3;
+			this.label11.Text = "Pet Name";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(19, 69);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(31, 13);
+			this.label12.TabIndex = 2;
+			this.label12.Text = "Color";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(16, 33);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(34, 13);
+			this.label13.TabIndex = 1;
+			this.label13.Text = "Make";
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Controls.Add(this.btnLookUpColors);
+			this.groupBox5.Controls.Add(this.txtMakeToLookUp);
+			this.groupBox5.Controls.Add(this.label14);
+			this.groupBox5.Location = new System.Drawing.Point(267, 203);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(280, 96);
+			this.groupBox5.TabIndex = 3;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Look up Colors for Make";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(9, 34);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(90, 13);
+			this.label14.TabIndex = 0;
+			this.label14.Text = "Make to Look Up";
+			// 
+			// txtMakeToLookUp
+			// 
+			this.txtMakeToLookUp.Location = new System.Drawing.Point(105, 29);
+			this.txtMakeToLookUp.Name = "txtMakeToLookUp";
+			this.txtMakeToLookUp.Size = new System.Drawing.Size(156, 20);
+			this.txtMakeToLookUp.TabIndex = 1;
+			// 
+			// btnLookUpColors
+			// 
+			this.btnLookUpColors.Location = new System.Drawing.Point(160, 59);
+			this.btnLookUpColors.Name = "btnLookUpColors";
+			this.btnLookUpColors.Size = new System.Drawing.Size(97, 23);
+			this.btnLookUpColors.TabIndex = 2;
+			this.btnLookUpColors.Text = "Look Up Colors";
+			this.btnLookUpColors.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(560, 330);
+			this.Controls.Add(this.groupBox5);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.txtInventory);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Fun With LINQ to XML";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
+			this.groupBox5.ResumeLayout(false);
+			this.groupBox5.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtInventory;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button btnAddNewItem;
+		private System.Windows.Forms.TextBox txtPetName;
+		private System.Windows.Forms.TextBox txtColor;
+		private System.Windows.Forms.TextBox txtMake;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox5;
+		private System.Windows.Forms.Button btnLookUpColors;
+		private System.Windows.Forms.TextBox txtMakeToLookUp;
+		private System.Windows.Forms.Label label14;
+	}
+}
+
