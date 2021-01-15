c01fe180923fe4c67ce145bde0e8ce2811cb1431
Wei Ding <ding_web@yahoo.co.jp>
2021-01-15 00:54:05 -0500

    remove unused namespace

--- CS60DN46/Ch25WCF/MagicEightBallServiceLib/IEightBall.cs
+++ CS60DN46/Ch25WCF/MagicEightBallServiceLib/IEightBall.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ServiceModel;
+﻿using System.ServiceModel;
 
 namespace MagicEightBallServiceLib {
 	[ServiceContract]
 	public interface IEightBall {
+		//Ask a question, receive an answer!
 		[OperationContract]
 		string ObtainAnswerToQuestion(string userQuestion);
 	}
