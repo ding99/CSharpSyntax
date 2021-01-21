e954a103e8057e8c3a4b529d08543515a7ba085c
Wei Ding <ding_web@yahoo.co.jp>
2021-01-21 00:54:28 -0500

    isolate baseaddress in config

--- CS60DN46/Ch25WCF/MagicEightBallServiceHost/Program.cs
+++ CS60DN46/Ch25WCF/MagicEightBallServiceHost/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MagicEightBallServiceLib;
+using System;
 using System.ServiceModel;
-using MagicEightBallServiceLib;
 
 namespace MagicEightBallServiceHost {
 	class Program {
