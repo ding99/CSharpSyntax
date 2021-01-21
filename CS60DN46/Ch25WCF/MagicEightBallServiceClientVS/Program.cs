e90bcf61f40c7f1493c14e12a5f631779e66bf13
Wei Ding <ding_web@yahoo.co.jp>
2021-01-21 03:11:42 -0500

    use visual studio

---
+++ CS60DN46/Ch25WCF/MagicEightBallServiceClientVS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MagicEightBallServiceClientVS.ServiceReference1;
+
+namespace MagicEightBallServiceClientVS {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Ask the Magic 8 Ball (VS) *****");
+
+			using (EightBallClient ball = new EightBallClient()) {
+				Console.WriteLine("Your question: ");
+				string question = Console.ReadLine();
+				string answer = ball.ObtainAnswerToQuestion(question);
+				Console.WriteLine($"Question : {question}");
+				Console.WriteLine($"Answer   : {answer}");
+			}
+		}
+	}
+}
