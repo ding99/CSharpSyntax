1196850dc4c4853775a80760e2c4bf8c732cd8c9
Wei Ding <ding_web@yahoo.co.jp>
2021-01-21 02:15:42 -0500

    wcf client

---
+++ CS60DN46/Ch25WCF/MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicEightBallServiceClient {
+	class Program {
+		static void Main() {
+			Console.WriteLine("***** Magic Egith Ball Service Client *****");
+		}
+	}
+}
