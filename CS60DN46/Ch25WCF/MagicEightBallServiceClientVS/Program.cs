c6d1164d988ef1e32693e59260fc0107cd2d4d3b
Wei Ding <ding_web@yahoo.co.jp>
2021-01-21 03:13:25 -0500

    remove unuse namespaces

--- CS60DN46/Ch25WCF/MagicEightBallServiceClientVS/Program.cs
+++ CS60DN46/Ch25WCF/MagicEightBallServiceClientVS/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ServiceModel;
-using MagicEightBallServiceClientVS.ServiceReference1;
+﻿using MagicEightBallServiceClientVS.ServiceReference1;
+using System;
 
 namespace MagicEightBallServiceClientVS {
 	class Program {
