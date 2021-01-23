42a6b3b6326c02802b047883d9776010d338ff3a
Wei Ding <ding_web@yahoo.co.jp>
2021-01-23 14:08:47 -0500

    optimize math lib

--- CS60DN46/Ch25WCF/MathServiceLib/MathService.cs
+++ CS60DN46/Ch25WCF/MathServiceLib/MathService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-
-namespace MathServiceLib {
+﻿namespace MathServiceLib {
 
 	public class MathService : IBasicMath {
 		public int Add(int x, int y) {
