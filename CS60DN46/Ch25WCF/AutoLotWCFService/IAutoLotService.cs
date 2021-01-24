07fa6be44ccc67522d8c2f77cae533bd3a8fa326
Wei Ding <ding_web@yahoo.co.jp>
2021-01-23 22:48:48 -0500

    implement interface

--- CS60DN46/Ch25WCF/AutoLotWCFService/IAutoLotService.cs
+++ CS60DN46/Ch25WCF/AutoLotWCFService/IAutoLotService.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using AutoLotDAL;
 
 namespace AutoLotWCFService {
 	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
@@ -12,31 +13,30 @@ namespace AutoLotWCFService {
 	public interface IAutoLotService {
 
 		[OperationContract]
-		string GetData(int value);
+		void InsertCar(int id, string make, string color, string petname);
+
+		[OperationContract(Name = "InsertCarWithDetails")]
+		void InsertCar(InventoryRecord car);
 
 		[OperationContract]
-		CompositeType GetDataUsingDataContract(CompositeType composite);
-
-		// TODO: Add your service operations here
+		InventoryRecord[] GetInventory();
 	}
 
 
 	// Use a data contract as illustrated in the sample below to add composite types to service operations.
 	[DataContract]
-	public class CompositeType {
-		bool boolValue = true;
-		string stringValue = "Hello ";
+	public class InventoryRecord {
 
 		[DataMember]
-		public bool BoolValue {
-			get { return boolValue; }
-			set { boolValue = value; }
-		}
+		public int ID;
 
 		[DataMember]
-		public string StringValue {
-			get { return stringValue; }
-			set { stringValue = value; }
-		}
+		public string Make;
+
+		[DataMember]
+		public string Color;
+
+		[DataMember]
+		public string PetName;
 	}
 }
