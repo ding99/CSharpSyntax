c364b7d865e41127ad594f3a816b9e9cddfe9252
Wei Ding <ding_web@yahoo.co.jp>
2021-01-23 23:29:42 -0500

    remove unuse namespaces

--- CS60DN46/Ch25WCF/AutoLotWCFService/AutoLotService.svc.cs
+++ CS60DN46/Ch25WCF/AutoLotWCFService/AutoLotService.svc.cs
@@ -1,12 +1,6 @@
-﻿using System;
+﻿using AutoLotDAL.ConnectedLayer;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 using System.Data;
-using AutoLotDAL.ConnectedLayer;
 
 namespace AutoLotWCFService {
 	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
