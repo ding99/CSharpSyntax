3dc6acd4b7ef8c96eb0d4a6cae21505bc7eb761e
Wei Ding <ding_web@yahoo.co.jp>
2021-01-30 20:08:07 -0500

    action

--- CS60DN46/Ch31WebForms/BlankApp/Program.cs
+++ CS60DN46/Ch31WebForms/BlankApp/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlankApp {
 	class Program {
-		static void Main(string[] args) {
+		static void Main() {
+			Console.WriteLine("This is a project for examing web pages.");
 		}
 	}
 }
