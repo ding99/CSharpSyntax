f01ff1210501e37a93d5b9cc613f14c7dbff385b
Wei Ding <ding_web@yahoo.co.jp>
2021-01-31 04:49:46 -0500

    add ref

--- CS60DN46/Ch31WebForms/AutoLotDAL/EF/AutoLotEntities.cs
+++ CS60DN46/Ch31WebForms/AutoLotDAL/EF/AutoLotEntities.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Infrastructure.Interception;
+using System.Web;
 
 namespace AutoLotDAL.EF {
 	public class AutoLotEntities : DbContext {
@@ -12,7 +13,7 @@ namespace AutoLotDAL.EF {
 		// target: 'AutoLotDAL.EF.AutoLotEntities' database on your LocalDb instance. 
 		// If you wish to target a different database and/or database provider, modify the 'AutoLotEntities' connection string in the configuration file.
 
-		static readonly DatabaseLogger dbLogger = new DatabaseLogger("sqllog.txt", true);
+		static readonly DatabaseLogger dbLogger = new DatabaseLogger($"{HttpRuntime.AppDomainAppPath}/sqllog.txt", true);
 
 		public AutoLotEntities() : base("name=AutoLotConnection") {
 			//DbInterception.Add(new ConsoleWriterInterceptor());
