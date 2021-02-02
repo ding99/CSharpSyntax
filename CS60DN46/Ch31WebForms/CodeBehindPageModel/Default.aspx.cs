41405d9d7074abd00ee60649617b584c44fa6349
Wei Ding <ding_web@yahoo.co.jp>
2021-02-02 01:58:37 -0500

    remove unused namespaces

--- CS60DN46/Ch31WebForms/CodeBehindPageModel/Default.aspx.cs
+++ CS60DN46/Ch31WebForms/CodeBehindPageModel/Default.aspx.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using AutoLotDAL.Models;
+﻿using AutoLotDAL.Models;
 using AutoLotDAL.Repos;
+using System;
+using System.Collections.Generic;
 
 namespace CodeBehindPageModel {
 	public partial class Default : System.Web.UI.Page {
