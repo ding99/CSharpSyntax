514cf2352f8d411f690f7d3b8cfba036ccddc226
Wei Ding <ding_web@yahoo.co.jp>
2021-02-04 23:36:05 -0500

    button handler

--- CS60DN46/Ch31WebForms/FunWithPageMembers/Default.aspx.cs
+++ CS60DN46/Ch31WebForms/FunWithPageMembers/Default.aspx.cs
@@ -9,4 +9,15 @@ public partial class _Default : System.Web.UI.Page {
 	protected void Page_Load(object sender, EventArgs e) {
 
 	}
+
+	protected void btnGetBrowserStats_Click(object sender, EventArgs e) {
+		string theInfo = "";
+		theInfo += $"<li>Is the client AOL? {Request.Browser.AOL}</li>";
+		theInfo += $"<li>Does the client support ActiveX? {Request.Browser.ActiveXControls}</li>";
+		theInfo += $"<li>Is the client a Beta? {Request.Browser.Beta}</li>";
+		theInfo += $"<li>Does the client support Java Applets? {Request.Browser.JavaApplets}</li>";
+		theInfo += $"<li>Does the client support Cookies? {Request.Browser.Cookies}</li>";
+		theInfo += $"<li>Does the client support VBScript? {Request.Browser.VBScript}</li>";
+		lblOutput.Text = theInfo;
+	}
 }
