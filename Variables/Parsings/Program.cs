6130f5f86832d7906fffab14eb0b8bf44978043b
Wei Ding <ding_web@yahoo.co.jp>
2021-02-07 23:30:44 -0500

    optimize

--- Variables/Parsings/Program.cs
+++ Variables/Parsings/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Parsings {
 	class Program {
 		static void Main() {
-			Console.WriteLine("Please enter an input :");
 			int a = 0;
+
+			Console.Write("Please enter an input value :");
 			try {
 				a = Convert.ToInt32(Console.ReadLine());
 				//"size=45000"
@@ -16,6 +13,7 @@ namespace Parsings {
 			catch(Exception e) {
 				Console.WriteLine($"Error: {e.Message}");
 			}
+
 			Console.WriteLine($"Input value is : {a}");
 			Console.WriteLine($"The output is : {1}");
 		}
