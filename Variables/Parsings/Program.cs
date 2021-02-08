78bb6371411997e207a348045acbe71855b915ea
Wei Ding <ding_web@yahoo.co.jp>
2021-02-07 23:29:33 -0500

    parsing project

---
+++ Variables/Parsings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsings {
+	class Program {
+		static void Main() {
+			Console.WriteLine("Please enter an input :");
+			int a = 0;
+			try {
+				a = Convert.ToInt32(Console.ReadLine());
+				//"size=45000"
+			}
+			catch(Exception e) {
+				Console.WriteLine($"Error: {e.Message}");
+			}
+			Console.WriteLine($"Input value is : {a}");
+			Console.WriteLine($"The output is : {1}");
+		}
+	}
+}
