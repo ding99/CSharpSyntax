beb546ce54b5e0d85b53c32d013c3b7b51c2e5d5
Wei Ding <ding_web@yahoo.co.jp>
2021-02-22 19:18:27 -0500

    fix form value issue

--- CS60DN46/Ch32WebControls/DynamicCtrls/Default.aspx.cs
+++ CS60DN46/Ch32WebControls/DynamicCtrls/Default.aspx.cs
@@ -18,14 +18,14 @@ public partial class _Default : System.Web.UI.Page {
 
 	protected void btnAddWidgets_Click(object sender, System.EventArgs e) {
 		for(int i = 0; i < 3; i++)
-			myPanel.Controls.Add(new TextBox { ID = $"newTextBox{i}", Text = $"TextBox{i}" });
+			myPanel.Controls.Add(new TextBox { ID = $"newTextBox{i}", Text = $"Text{i}" });
 		ListControlsInPanel();
 	}
 
 	protected void btnGetTextData_Click(object sender, System.EventArgs e) {
-		StringBuilder b = new StringBuilder();
+		StringBuilder b = new StringBuilder($"Request Forms ({Request.Form.Count})<br/>");
 		for(int i = 0; i < Request.Form.Count; i++) {
-			b.Append($"<li>Request.Form[i]</li><br/>");
+			b.Append($"<li>{Request.Form[i]}</li><br/>");
 		}
 		lblTextBoxData.Text = b.ToString();
 	}
