e2d905d8254ef6ac59effacd9babeaf99ab4c692
Wei Ding <ding_web@yahoo.co.jp>
2021-02-24 02:27:56 -0500

    update inventory content page

--- CS60DN46/Ch32WebControls/AspNetCarsSite/Inventory.aspx.cs
+++ CS60DN46/Ch32WebControls/AspNetCarsSite/Inventory.aspx.cs
@@ -5,8 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using AutoLotDAL.Repos;
+using AutoLotDAL.Models;
+
 public partial class Default2 : System.Web.UI.Page {
 	protected void Page_Load(object sender, EventArgs e) {
 
 	}
+
+	public IEnumerable<Inventory> GetData() => new InventoryRepo().GetAll();
 }
