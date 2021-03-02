70a2e54938261a94c74d234673550ecd63b45dff
Wei Ding <ding_web@yahoo.co.jp>
2021-03-01 20:41:38 -0500

    session state project

---
+++ CS60DN46/Ch33State/SessionState/App_Code/UserShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserShoppingCart
+/// </summary>
+public class UserShoppingCart {
+	public UserShoppingCart() {
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
