f687981aa31717423383c210c221c1fea136cca8
Wei Ding <ding_web@yahoo.co.jp>
2021-03-02 09:22:54 -0500

    variable vs session

--- CS60DN46/Ch33State/SimpleStateExample/Default.aspx.cs
+++ CS60DN46/Ch33State/SimpleStateExample/Default.aspx.cs
@@ -3,19 +3,19 @@
 public partial class _Default : System.Web.UI.Page {
 
 	// State data
-	//private string userFavoriteCar = "Yugo";
+	private string userFavoriteCar = "Yugo";
 
 	protected void Page_Load(object sender, EventArgs e) {
 	}
 
 	protected void btnSetCar_Click(object sender, EventArgs e) {
-		//userFavoriteCar = txtFavCar.Text;
+		userFavoriteCar = txtFavCar.Text;
 		//lblFavCar.Text = userFavoriteCar;
 		Session["UserFavCar"] = txtFavCar.Text;
 	}
 
 	protected void btnGetCar_Click(object sender, EventArgs e) {
 		//lblFavCar.Text = userFavoriteCar;
-		lblFavCar.Text = (string)Session["UserFavCar"];
+		lblFavCar.Text = $"In variable: [{userFavoriteCar}];  In Session [{(string)Session["UserFavCar"]}]";
 	}
 }
