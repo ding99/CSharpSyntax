0c13b3f1c39315177ab61e74c955d44a0e1ac147
Wei Ding <ding_web@yahoo.co.jp>
2021-03-01 22:03:38 -0500

    global file

--- CS60DN46/Ch33State/SessionState/App_Code/UserShoppingCart.cs
+++ CS60DN46/Ch33State/SessionState/App_Code/UserShoppingCart.cs
@@ -7,9 +7,10 @@ using System.Web;
 /// Summary description for UserShoppingCart
 /// </summary>
 public class UserShoppingCart {
-	public UserShoppingCart() {
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+	public string DesiredCar { get; set; }
+	public string DesiredCarColor { get; set; }
+	public float DownPayment { get; set; }
+	public bool IsLeasing { get; set; }
+	public DateTime DateOfPickUp { get; set; }
+	public override string ToString() => $"Car: {DesiredCar}<br>Color: {DesiredCarColor}<br> Down: { DownPayment}" + $"<br>Lease: {IsLeasing}<br>Pick-up Date: {DateOfPickUp.ToShortDateString()}";
 }
