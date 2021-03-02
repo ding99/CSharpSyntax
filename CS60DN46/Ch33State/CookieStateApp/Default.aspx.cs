83ac51e08b16ddb994fc38de2bb4a59211cbe90f
Wei Ding <ding_web@yahoo.co.jp>
2021-03-02 02:22:12 -0500

    create cookie

---
+++ CS60DN46/Ch33State/CookieStateApp/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+	protected void Page_Load(object sender, EventArgs e) {
+
+	}
+
+	protected void btnWrite_Click(object sender, EventArgs e) {
+		HttpCookie theCookie = new HttpCookie(txtName.Text, txtValue.Text);
+		//theCookie.Expires = DateTime.Now.AddMonths(3); // persistent cookie
+		Response.Cookies.Add(theCookie);
+	}
+}
