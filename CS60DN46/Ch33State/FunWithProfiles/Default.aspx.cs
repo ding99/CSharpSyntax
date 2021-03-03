cbbedfea490542c259f0f0a07fc998917399c149
Wei Ding <ding_web@yahoo.co.jp>
2021-03-03 02:50:26 -0500

    handler

--- CS60DN46/Ch33State/FunWithProfiles/Default.aspx.cs
+++ CS60DN46/Ch33State/FunWithProfiles/Default.aspx.cs
@@ -5,8 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page {
+public partial class _Default : Page {
 	protected void Page_Load(object sender, EventArgs e) {
+		GetUserAddress();
+	}
 
+	protected void btnSubmit_Click(object sender, EventArgs e) {
+		Profile.StreetAddress = txtStreet.Text;
+		Profile.City = txtCity.Text;
+		Profile.State = txtState.Text;
+
+		GetUserAddress();
+	}
+
+	private void GetUserAddress() {
+		lblUserData.Text = $"You live here: {Profile.StreetAddress}, {Profile.City}, {Profile.State}";
 	}
 }
