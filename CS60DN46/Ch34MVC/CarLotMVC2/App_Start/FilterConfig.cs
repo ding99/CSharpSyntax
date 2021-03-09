23837bbd1664216d4a921bf8ffb569ff5d3308f9
Wei Ding <ding_web@yahoo.co.jp>
2021-03-08 23:45:28 -0500

    change namespace name

--- CS60DN46/Ch34MVC/CarLotMVC2/App_Start/FilterConfig.cs
+++ CS60DN46/Ch34MVC/CarLotMVC2/App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace CarLotMVC2 {
+namespace CarLotMVC {
 	public class FilterConfig {
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
 			filters.Add(new HandleErrorAttribute());
