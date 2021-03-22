cafc03c789986b00d878d13cb994041a0d15a12a
Wei Ding <ding_web@yahoo.co.jp>
2021-03-21 23:52:44 -0400

    correct metadata location

---
+++ CS60DN46/Ch34MVC/AutoLotDAL/Models/MetaData/InventoryMetaData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotDAL.Models.MetaData {
+	public class InventoryMetaData {
+		[Display(Name="Pet Name")]
+		public string PetName;
+	}
+}
