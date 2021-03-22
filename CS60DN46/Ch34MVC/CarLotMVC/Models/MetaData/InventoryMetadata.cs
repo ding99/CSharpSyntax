d1bd4a29b0f98559bf5adca4b5597d860bed9ef3
Wei Ding <ding_web@yahoo.co.jp>
2021-03-21 23:49:37 -0400

    start metadata class

---
+++ CS60DN46/Ch34MVC/CarLotMVC/Models/MetaData/InventoryMetadata.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarLotMVC.Models.MetaData {
+	public class InventoryMetadata {
+		[Display(Name="Pet Name")]
+		public string PetName;
+	}
+}
