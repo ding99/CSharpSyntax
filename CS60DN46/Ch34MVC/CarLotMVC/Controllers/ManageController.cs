01898b85707c0882fb66a22a8e2278f3a2d348af
Wei Ding <ding_web@yahoo.co.jp>
2021-03-24 23:45:55 -0400

    remove unused using

--- CS60DN46/Ch34MVC/CarLotMVC/Controllers/ManageController.cs
+++ CS60DN46/Ch34MVC/CarLotMVC/Controllers/ManageController.cs
@@ -1,16 +1,14 @@
-﻿using System;
+﻿using CarLotMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using CarLotMVC.Models;
 
-namespace CarLotMVC.Controllers
-{
-    [Authorize]
+namespace CarLotMVC.Controllers {
+	[Authorize]
     public class ManageController : Controller
     {
         private ApplicationSignInManager _signInManager;
