a330b838fef910624b24ee8bcbd4def82c53f390
Wei Ding <ding_web@yahoo.co.jp>
2021-03-29 13:03:50 -0400

    recreate mvc project

--- CS60DN46/Ch34MVC/MVC/CarLotMVC/Models/IdentityModels.cs
+++ CS60DN46/Ch34MVC/MVC/CarLotMVC/Models/IdentityModels.cs
@@ -29,7 +29,5 @@ namespace CarLotMVC.Models
         {
             return new ApplicationDbContext();
         }
-
-		public System.Data.Entity.DbSet<AutoLotDAL.Models.Inventory> Inventories { get; set; }
-	}
+    }
 }
