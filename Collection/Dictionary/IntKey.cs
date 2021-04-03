c3120e586671f98e4e1f6e099369bec8ac3840b2
Wei Ding <ding_web@yahoo.co.jp>
2021-04-03 16:55:43 -0400

    linq method

--- Collection/Dictionary/IntKey.cs
+++ Collection/Dictionary/IntKey.cs
@@ -8,30 +8,43 @@ namespace Dictionary {
 			Console.ForegroundColor = ConsoleColor.Cyan;
 		}
 
-		private void UseDictionary(int[] input) {
+		private void ByDictionary(int[] input) {
 			Console.WriteLine("-- Method 1: Dictionary");
 
 			Dictionary<int, int> pair = new Dictionary<int, int>();
-			foreach(int i in input) {
+			foreach(int i in input)
 				if (pair.ContainsKey(i))
 					pair[i]++;
 				else pair.Add(i, 1);
-			}
 
 			int max = 0, pos = -1;
-			foreach(var a in pair) {
+			foreach(var a in pair)
 				if (a.Value > max) { max = a.Value; pos = a.Key; }
-			}
 
 			Console.WriteLine($"max(loop): {max} at value {pos}");
-
 			Console.WriteLine($"max(linq): {(from p in pair select p.Value).Max()}");
 		}
 
-		private void UseLinq(int[] input) {
-			Console.WriteLine("-- Method 2: Linq");
+		private void ByLinqQuery(int[] input) {
+			Console.WriteLine("-- Method 2: Linq Query");
 
 			var ndata = (from p in input.ToList() group p by p into g select new { key = g.Key, count = g.Count() });
+
+			Console.Write($"ndata ({ndata.Count()}):");
+			foreach (var a in ndata) Console.Write($" <{a.key}>-{a.count}");
+			Console.WriteLine();
+
+			Console.WriteLine($"max(linq): {ndata.Max(x => x.count)}");
+		}
+		private void ByLinqMethod(int[] input) {
+			Console.WriteLine("-- Method 3: Linq Method");
+
+			var ndata = input.ToList().GroupBy(x => x).Select(g => new { key = g.Key, count = g.Count() });
+
+			Console.Write($"ndata ({ndata.Count()}):");
+			foreach (var a in ndata) Console.Write($" <{a.key}>-{a.count}");
+			Console.WriteLine();
+
 			Console.WriteLine($"max(linq): {ndata.Max(x => x.count)}");
 		}
 
@@ -39,8 +52,8 @@ namespace Dictionary {
 			Console.WriteLine($"line1: [{line1}]");
 			Console.WriteLine($"line2: [{line2}]");
 
-			int count;
-			if (!Int32.TryParse(line1, out count)) {
+			//int count;
+			if (!Int32.TryParse(line1, out int count)) {
 				Console.WriteLine("input line1 should be a number.");
 				return;
 			}
@@ -52,15 +65,15 @@ namespace Dictionary {
 			}
 
 			int[] org = new int[count];
-			for(int i = 0; i < count; i++) {
+			for(int i = 0; i < count; i++)
 				if(!Int32.TryParse(ns[i], out org[i])) {
 					Console.WriteLine($"Term #{i + 1} in line2 should be a number");
 					return;
 				}
-			}
 
-			UseDictionary(org);
-			UseLinq(org);
+			ByDictionary(org);
+			ByLinqQuery(org);
+			ByLinqMethod(org);
 		}
 
 		public void Start() {
