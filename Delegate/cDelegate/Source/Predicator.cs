fd2a803dae5f00d4a5a1457cf20cc83ebe4df225
Wei Ding <ding_web@yahoo.co.jp>
2021-04-28 20:10:53 -0400

    all uppers

---
+++ Delegate/cDelegate/Source/Predicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CDelegate {
+	public class Predicator {
+		public Predicator() {
+			Console.ForegroundColor = ConsoleColor.DarkCyan;
+		}
+
+		public void Start() {
+			Console.WriteLine("-- Start Predicate examination");
+
+			Uppercases();
+		}
+
+		private void Uppercases() {
+			isUp("Hello Predicator!");
+			isUp("HELLO PREDICATOR!");
+		}
+
+		private void isUp(string str) {
+			Predicate<string> isUpper = s => s.Equals(s.ToUpper());
+			Console.WriteLine($"[{str}] all uppercases? -> {isUpper(str)}");
+		}
+	}
+}
