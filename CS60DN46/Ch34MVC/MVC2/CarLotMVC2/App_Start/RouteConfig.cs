9a073eb689d799b2bfa997f76144c0d47d3dbba3
Wei Ding <ding_web@yahoo.co.jp>
2021-05-12 22:16:56 -0400

    about route

--- CS60DN46/Ch34MVC/MVC2/CarLotMVC2/App_Start/RouteConfig.cs
+++ CS60DN46/Ch34MVC/MVC2/CarLotMVC2/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace CarLotMVC2 {
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapRoute("Contact","Contact/{*pathinfo}", new { controller = "Home", action = "Contact" });
+			routes.MapRoute("About", "About/{*pathinfo}", new { controller = "Home", action = "About" });
 
 			routes.MapRoute(
 				name: "Default",
