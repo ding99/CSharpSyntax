3cc753ea7a28a170659582ac40c9fccd785f5070
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 13:50:20 -0400

    annotation

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Order.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Order.cs
@@ -8,10 +8,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models {
 	public partial class Order {
+		[Key, Required, DatabaseGenerated(DatabaseGeneratedOption.Identity]
 		public int OrderId { get; set; }
+
+		[Required]
 		public int CustId { get; set; }
+
+		[Required]
 		public int CarId { get; set; }
+
+		[ForeignKey("CustId")]
 		public virtual Customer Customer { get; set; }
+
+		[ForeignKey("CarId")]
 		public virtual Inventory Car { get; set; }
 	}
 }
