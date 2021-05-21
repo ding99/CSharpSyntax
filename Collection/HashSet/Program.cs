a8827297025344f88e8f6d848b852e893d09e48d
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 11:49:48 -0400

    create entry of examination

---
+++ Collection/HashSet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet {
+	class Program {
+		static void Main() {
+
+			ExamineHashSet exam = new ExamineHashSet();
+			exam.Start();
+		}
+	}
+}
