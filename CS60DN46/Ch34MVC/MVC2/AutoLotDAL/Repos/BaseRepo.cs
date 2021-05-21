b6b2c46ad1c524abad3aa4e43f3729021d1809b0
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 15:00:17 -0400

    base save methods

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Repos/BaseRepo.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Repos/BaseRepo.cs
@@ -1,7 +1,49 @@
-﻿using AutoLotDAL.EF;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoLotDAL.EF;
 
 namespace AutoLotDAL.Repos {
 	public abstract class BaseRepo<T> where T: class, new() {
 		public AutoLotEntities Context { get; } = new AutoLotEntities();
+
+		internal int SaveChanges() {
+			try {
+				return Context.SaveChanges();
+			}
+			catch(DbUpdateConcurrencyException ex) {
+				throw;
+			}
+			catch (DbUpdateException ex) {
+				throw;
+			}
+			catch (CommitFailedException ex) {
+				throw;
+			}
+			catch (Exception ex) {
+				throw;
+			}
+		}
+
+		internal async Task<int> SaveChangesAsync() {
+			try {
+				return await Context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException ex) {
+				throw;
+			}
+			catch (DbUpdateException ex) {
+				throw;
+			}
+			catch (CommitFailedException ex) {
+				throw;
+			}
+			catch (Exception ex) {
+				throw;
+			}
+		}
 	}
 }
