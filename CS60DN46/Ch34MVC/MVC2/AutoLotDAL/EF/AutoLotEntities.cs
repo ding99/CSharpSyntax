a0240bfc0ee0d0c2261fa2a7cad89862710cdc7f
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 14:32:29 -0400

    remove unused comment

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/EF/AutoLotEntities.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/EF/AutoLotEntities.cs
@@ -20,15 +20,5 @@ namespace AutoLotDAL.EF {
 		public virtual DbSet<Inventory> Inventory { get; set; }
 		public virtual DbSet<Order> Orders { get; set; }
 
-		// Add a DbSet for each entity type that you want to include in your model. For more information 
-		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-		// public virtual DbSet<MyEntity> MyEntities { get; set; }
 	}
-
-	//public class MyEntity
-	//{
-	//    public int Id { get; set; }
-	//    public string Name { get; set; }
-	//}
 }
