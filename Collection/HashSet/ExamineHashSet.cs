84d4602eb65473ec097430dc4720801715e5806f
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 13:12:13 -0400

    two sets

--- Collection/HashSet/ExamineHashSet.cs
+++ Collection/HashSet/ExamineHashSet.cs
@@ -7,7 +7,33 @@ using System.Threading.Tasks;
 namespace HashSet {
 	class ExamineHashSet {
 		public void Start() {
-			Console.WriteLine("-- Start Hash Set Examination");
+			HashString();
+			HashInt();
+		}
+
+		private void HashString() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("-- HashSet of String: Languages");
+
+			HashSet<string> languages = new HashSet<string>();
+			languages.Add("C");
+			languages.Add("C++");
+			languages.Add("C#");
+			languages.Add("Java");
+			languages.Add("Ruby");
+			languages.Add("Perl");
+
+			foreach(var a in languages)
+				Console.WriteLine(a);
+		}
+
+		private void HashInt() {
+			Console.ForegroundColor = ConsoleColor.Cyan;
+
+			Console.WriteLine("-- HashSet of Integer");
+			HashSet<int> ints = new HashSet<int> { 10, 100, 100, 10000, 100000 };
+			foreach(var i in ints)
+				Console.WriteLine(i);
 		}
 	}
 }
--- Collection/HashSet/Program.cs
+++ Collection/HashSet/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HashSet {
+﻿namespace HashSet {
 	class Program {
 		static void Main() {
+			System.Console.WriteLine("== Start HashSet Examination");
 
 			ExamineHashSet exam = new ExamineHashSet();
 			exam.Start();
