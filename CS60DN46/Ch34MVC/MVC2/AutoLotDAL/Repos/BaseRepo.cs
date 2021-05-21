c0a5ecd1f533ee39bd642b8d05b7c11872f54e9e
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 18:36:29 -0400

    four methods

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Repos/BaseRepo.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Repos/BaseRepo.cs
@@ -9,6 +9,12 @@ using AutoLotDAL.EF;
 namespace AutoLotDAL.Repos {
 	public abstract class BaseRepo<T> where T: class, new() {
 		public AutoLotEntities Context { get; } = new AutoLotEntities();
+		protected DbSet<T> Table;
+
+		public T GetOne(int? id) => Table.Find(id);
+		public Task<T> GetOneAsync(int? id) => Table.FindAsync(id);
+		public List<T> GetAll() => Table.ToList();
+		public Task<List<T>> GetAllAsync() => Table.ToListAsync();
 
 		internal int SaveChanges() {
 			try {
@@ -27,7 +33,6 @@ namespace AutoLotDAL.Repos {
 				throw;
 			}
 		}
-
 		internal async Task<int> SaveChangesAsync() {
 			try {
 				return await Context.SaveChangesAsync();
