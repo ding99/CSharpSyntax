0b8613ed85fce77250064554500aa80c8d8ce5dd
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 13:36:50 -0400

    not-mapped property

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Partials/InventoryPartial.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Partials/InventoryPartial.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AutoLotDAL.Models.Partials {
-	class InventoryPartial {
+namespace AutoLotDAL.Models {
+	public partial class Inventory {
+		public override string ToString() {
+			return $"{this.PetName ?? "**No Name**"} is a {this.Color} {this.Make} with ID {this.CarId}.";
+		}
+
+		[NotMapped]
+		public string MakeColor => $"{Make} + ({Color})";
 	}
 }
