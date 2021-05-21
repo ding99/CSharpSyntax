76022e72dac985d3dd64ca01f1b2aaa04382cadc
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 14:53:34 -0400

    base repo

---
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Repos/BaseRepo.cs
@@ -0,0 +1,7 @@
+﻿using AutoLotDAL.EF;
+
+namespace AutoLotDAL.Repos {
+	public abstract class BaseRepo<T> where T: class, new() {
+		public AutoLotEntities Context { get; } = new AutoLotEntities();
+	}
+}
