43bdb655c0d0a38a1bafa9e3ea4ddf35aae5ec07
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 13:47:36 -0400

    order class

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Order.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/Models/Order.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models {
-	public class Order {
+	public partial class Order {
+		public int OrderId { get; set; }
+		public int CustId { get; set; }
+		public int CarId { get; set; }
+		public virtual Customer Customer { get; set; }
+		public virtual Inventory Car { get; set; }
 	}
 }
