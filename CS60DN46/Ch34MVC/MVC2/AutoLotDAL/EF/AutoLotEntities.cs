869a179ed1d767be8d6f2a249970d5e7572b5a37
Wei Ding <ding_web@yahoo.co.jp>
2021-05-22 00:31:48 -0400

    more test functions

--- CS60DN46/Ch34MVC/MVC2/AutoLotDAL/EF/AutoLotEntities.cs
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/EF/AutoLotEntities.cs
@@ -5,12 +5,6 @@ using AutoLotDAL.Models;
 
 namespace AutoLotDAL.EF {
 	public class AutoLotEntities : DbContext {
-		// Your context has been configured to use a 'AutoLotEntities' connection string from your application's 
-		// configuration file (App.config or Web.config). By default, this connection string targets the 
-		// 'AutoLotDAL.EF.AutoLotEntities' database on your LocalDb instance. 
-		// 
-		// If you wish to target a different database and/or database provider, modify the 'AutoLotEntities' 
-		// connection string in the application configuration file.
 		public AutoLotEntities()
 			: base("name=AutoLotConnection") {
 		}
