53bc7c8911f032aa8871205a00f0b8f30413c381
Wei Ding <ding_web@yahoo.co.jp>
2021-05-21 20:45:14 -0400

    create class

---
+++ CS60DN46/Ch34MVC/MVC2/AutoLotDAL/EF/DataInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.EF {
+	public class DataInitializer : DropCreateDatabaseAlways<AutoLotEntities> {
+		protected override void Seed(AutoLotEntities context) {
+			
+		}
+	}
+}
