2f448db6c9da7c11b84b86fead1dd163ad6df0dd
Wei Ding <ding_web@yahoo.co.jp>
2021-05-22 04:14:35 -0400

    add controller

---
+++ CS60DN46/Ch34MVC/WebAPI/CarLotWebAPI/Controllers/InventoryController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AutoLotDAL.EF;
+using AutoLotDAL.Models;
+
+namespace CarLotWebAPI.Controllers
+{
+    public class InventoryController : ApiController
+    {
+        private AutoLotEntities db = new AutoLotEntities();
+
+        // GET: api/Inventory
+        public IQueryable<Inventory> GetInventory()
+        {
+            return db.Inventory;
+        }
+
+        // GET: api/Inventory/5
+        [ResponseType(typeof(Inventory))]
+        public async Task<IHttpActionResult> GetInventory(int id)
+        {
+            Inventory inventory = await db.Inventory.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventory);
+        }
+
+        // PUT: api/Inventory/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutInventory(int id, Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != inventory.CarId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(inventory).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InventoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Inventory
+        [ResponseType(typeof(Inventory))]
+        public async Task<IHttpActionResult> PostInventory(Inventory inventory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Inventory.Add(inventory);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = inventory.CarId }, inventory);
+        }
+
+        // DELETE: api/Inventory/5
+        [ResponseType(typeof(Inventory))]
+        public async Task<IHttpActionResult> DeleteInventory(int id)
+        {
+            Inventory inventory = await db.Inventory.FindAsync(id);
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            db.Inventory.Remove(inventory);
+            await db.SaveChangesAsync();
+
+            return Ok(inventory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool InventoryExists(int id)
+        {
+            return db.Inventory.Count(e => e.CarId == id) > 0;
+        }
+    }
+}
