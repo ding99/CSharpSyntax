4b1f7ddf1fc7cd69148e0d3a0f2be1e7fd962bea
Wei Ding <ding_web@yahoo.co.jp>
2021-08-31 10:05:21 -0400

    create Partial class

---
+++ Variables/Parsings/Partial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Parsings {
+	public class Partial {
+		public Partial() { Console.ForegroundColor = ConsoleColor.Yellow; }
+
+		public void Start() {
+			string source = string.Empty;
+			int target = 0;
+
+			Console.Write("Please enter an input value :");
+			try {
+				target = Convert.ToInt32(source = Console.ReadLine());
+				//size=45000, 4500, ...
+			}
+			catch (Exception e) {
+				Console.WriteLine($"Error: {e.Message}");
+			}
+
+			Console.WriteLine($"Input value is : {source}");
+			Console.WriteLine($"The output is : {target}");
+		}
+	}
+}
