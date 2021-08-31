d394a1e64977da211c303499722da9103426e1d4
Wei Ding <ding_web@yahoo.co.jp>
2021-08-31 10:20:36 -0400

    clean up

--- Variables/Parsings/Partial.cs
+++ Variables/Parsings/Partial.cs
@@ -7,20 +7,6 @@ namespace Parsings {
 		public void Start() {
 			Parsing("2345");
 			Parsing("size=2345");
-			//string source = string.Empty;
-			//int target = 0;
-
-			//Console.Write("Please enter an input value :");
-			//try {
-			//	target = Convert.ToInt32(source = Console.ReadLine());
-			//	//size=45000, 4500, ...
-			//}
-			//catch (Exception e) {
-			//	Console.WriteLine($"Error: {e.Message}");
-			//}
-
-			//Console.WriteLine($"Input value is : {source}");
-			//Console.WriteLine($"The output is : {target}");
 		}
 
 		private void Parsing(string source) {
