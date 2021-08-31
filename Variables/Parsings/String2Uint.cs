24088b9699c33a1d95335e9652fa041726c2289d
Wei Ding <ding_web@yahoo.co.jp>
2021-08-31 10:34:26 -0400

    format

--- Variables/Parsings/String2Uint.cs
+++ Variables/Parsings/String2Uint.cs
@@ -10,13 +10,30 @@ namespace Parsings {
 
 		public void Start() {
 			Parsing("ffml");
-			Parsing("scc ");
+			Parsing("vtt ");
+			Parsing("webvtt");
 		}
 
 		private void Parsing(string source) {
-			uint target1 = ((uint)source[0] << 24) + ((uint)source[1] << 16) + ((uint)source[2] << 8) + source[3];
+			Console.Write($"Source [{source}]");
 
-			Console.WriteLine($"source [{source}], target1 [{target1}]");
+			try {
+				uint target1 = ((uint)source[0] << 24) + ((uint)source[1] << 16) + ((uint)source[2] << 8) + source[3];
+				Console.Write($" Target1 [{target1.ToString("x")}]");
+			}
+			catch(Exception e) {
+				Console.Write($" [{e.Message}]");
+			}
+
+			try {
+				uint target2 = uint.Parse(source.Substring(0, 4), System.Globalization.NumberStyles.HexNumber);
+				Console.Write($" Target2 [{target2.ToString("x")}]");
+			}
+			catch (Exception e) {
+				Console.Write($" [{e.Message}]");
+			}
+
+			Console.WriteLine();
 		}
 	}
 }
