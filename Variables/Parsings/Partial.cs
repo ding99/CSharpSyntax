17aae33ba4dce33e386015124f689fe9f906af71
Wei Ding <ding_web@yahoo.co.jp>
2021-08-31 10:18:12 -0400

    automatic input

--- Variables/Parsings/Partial.cs
+++ Variables/Parsings/Partial.cs
@@ -5,20 +5,34 @@ namespace Parsings {
 		public Partial() { Console.ForegroundColor = ConsoleColor.Yellow; }
 
 		public void Start() {
-			string source = string.Empty;
-			int target = 0;
+			Parsing("2345");
+			Parsing("size=2345");
+			//string source = string.Empty;
+			//int target = 0;
 
-			Console.Write("Please enter an input value :");
+			//Console.Write("Please enter an input value :");
+			//try {
+			//	target = Convert.ToInt32(source = Console.ReadLine());
+			//	//size=45000, 4500, ...
+			//}
+			//catch (Exception e) {
+			//	Console.WriteLine($"Error: {e.Message}");
+			//}
+
+			//Console.WriteLine($"Input value is : {source}");
+			//Console.WriteLine($"The output is : {target}");
+		}
+
+		private void Parsing(string source) {
+			Console.Write($"Source [{source}], ");
+
 			try {
-				target = Convert.ToInt32(source = Console.ReadLine());
-				//size=45000, 4500, ...
+				int target = Convert.ToInt32(source);
+				Console.WriteLine($"Parsed [{target}]");
 			}
 			catch (Exception e) {
-				Console.WriteLine($"Error: {e.Message}");
+				Console.WriteLine($"Error: [{e.Message}]");
 			}
-
-			Console.WriteLine($"Input value is : {source}");
-			Console.WriteLine($"The output is : {target}");
 		}
 	}
 }
