76cc6c0d403d7924166f1611b8af13003de92bed
Wei Ding <ding_web@yahoo.co.jp>
2021-09-03 11:45:19 -0400

    create project

---
+++ Variables/IntPtr/Invoke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace IntPtr {
+	public class Invoke {
+		public Invoke() { Console.ForegroundColor = ConsoleColor.Yellow; }
+
+		public void Start() {
+			Console.WriteLine("invoke");
+		}
+	}
+}
