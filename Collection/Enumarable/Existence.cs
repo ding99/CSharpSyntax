bd5d0518bfe7e7ddadacf2e7c4725d3982c85f75
Wei Ding <ding_web@yahoo.co.jp>
2021-09-16 09:53:06 -0400

    add enumerable project

---
+++ Collection/Enumarable/Existence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumarable {
+	public class Existence {
+		public Existence() {
+			Console.ForegroundColor = ConsoleColor.Yellow;
+		}
+
+		public void ExistAny() {
+			ExistOne(new List<string>());
+		}
+
+		public void ExistOne<T>(IList<T> one) {
+			Console.Write($"IsNull [{one == null}]");
+			if(one != null) {
+				Console.Write($", Any() [{one.Any()}]");
+				Console.Write($", Count [{one.Count}]");
+				Console.Write($", Count() [{one.Count()}]");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Collection/Enumarable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumarable {
+	class Program {
+		static void Main() {
+
+			Console.WriteLine("== Start");
+
+			Existence e = new Existence();
+			e.ExistAny();
+
+			Console.ResetColor();
+			Console.WriteLine("== End");
+		}
+	}
+}
