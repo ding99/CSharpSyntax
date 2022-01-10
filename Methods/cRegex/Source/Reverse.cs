cf9e057c916bbb1246f5b7d6dc2f9b8630c8cfe7
Wei Ding <ding_web@yahoo.co.jp>
2022-01-10 03:45:25 -0800

    reverse 0

---
+++ Methods/cRegex/Source/Reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CRegex
+{
+    class Reverse
+    {
+        public Reverse ()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        private void One(string s)
+        {
+            string key = "0*$";
+            Match m = new Regex(key).Match (s);
+            Console.WriteLine ($"key [{key}] string[{s}] match ({m.Success}) value [{m.Value}] [{m.Index}]");
+        }
+
+        public void Start ()
+        {
+            this.One ("11223344500000");
+            this.One ("00011100000");
+            this.One ("aabbcc00000");
+            this.One ("1234560");
+        }
+    }
+}
